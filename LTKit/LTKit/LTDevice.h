15985b211c0365584eac9e70120fe95a047a15b7
Yaron Inger <yaron@lightricks.com>
2013-12-01 15:08:02 +0200

    Add LTDevice’s common properties with tests.

--- LTKit/LTKit/LTDevice.h
+++ LTKit/LTKit/LTDevice.h
@@ -1,20 +1,157 @@
 // Copyright (c) 2013 Lightricks. All rights reserved.
-// Created by Amit Goldstein.
+// Created by Yaron Inger.
 
-#import <Foundation/Foundation.h>
+typedef NS_ENUM(NSUInteger, LTDeviceType) {
+  // iPhone.
+  LTDeviceTypeIPhone1 = 0,
+  LTDeviceTypeIPhone3G,
+  LTDeviceTypeIPhone3GS,
+  LTDeviceTypeIPhone4,
+  LTDeviceTypeIPhone4S,
+  LTDeviceTypeIPhone5,
+  LTDeviceTypeIPhone5C,
+  LTDeviceTypeIPhone5S,
+
+  // iPod.
+  LTDeviceTypeIPod1G,
+  LTDeviceTypeIPod2G,
+  LTDeviceTypeIPod3G,
+  LTDeviceTypeIPod4G,
+  LTDeviceTypeIPod5G,
+
+  // iPad.
+  LTDeviceTypeIPad1G,
+  LTDeviceTypeIPad2G,
+  LTDeviceTypeIPad3G,
+  LTDeviceTypeIPad4G,
+  LTDeviceTypeIPadAir,
+
+  // iPad mini.
+  LTDeviceTypeIPadMini1G,
+  LTDeviceTypeIPadMini2G,
+
+  // Apple TV.
+  LTDeviceTypeAppleTV2,
+  LTDeviceTypeAppleTV3,
+
+  // Simulator
+  LTDeviceTypeSimulatorIPhone,
+  LTDeviceTypeSimulatorIPad,
+
+  // Unknowns.
+  LTDeviceTypeUnknownIPhone,
+  LTDeviceTypeUnknownIPod,
+  LTDeviceTypeUnknownIPad,
+  LTDeviceTypeUnknownAppleTV,
+  LTDeviceTypeUnknownDevice
+};
 
 @interface LTDevice : NSObject
 
-// The current device the app is running on.
-+ (LTDevice *)currentDevice;
+/// Default initializer. Will be called when the singleton creates its instance.
+- (id)init;
 
-// Maximal texture size that can be used on the device's GPU.
-@property (readonly, nonatomic) NSUInteger maxTextureSize;
+/// The current device the app is running on.
++ (instancetype)currentDevice;
 
-// YES if writing to textures with half-float precision is supported.
-@property (readonly, nonatomic) BOOL canUseHalfFloatTextures;
+#pragma mark -
+#pragma mark Device idioms
+#pragma mark -
 
-// YES if writing to textures with half-float precision is supported.
+/// Runs the block if currently running with a pad idiom. Note that an app might run on an iPad as
+/// an iPhone app in 'simulation mode'. In such a case, the block won't execute.
+- (void)iPad:(LTVoidBlock)block;
+
+/// Runs the block if currently running with a phone idiom. Note that an app might run on an iPad as
+/// an iPhone app in 'simulation mode'. In such a case, the block will execute.
+- (void)iPhone:(LTVoidBlock)block;
+
+/// Runs the first block if running with a phone idiom, or the second one if running with a pad
+/// idiom.
+- (void)iPhone:(LTVoidBlock)iPhoneBlock iPad:(LTVoidBlock)iPadBlock;
+
+/// \c YES if currently running with a pad user interface idiom.
+@property (readonly, nonatomic) BOOL isPadIdiom;
+
+/// \c YES if currently running with a phone user interface idiom.
+@property (readonly, nonatomic) BOOL isPhoneIdiom;
+
+#pragma mark -
+#pragma mark Platform info
+#pragma mark -
+
+/// Name of the platform, such as 'iPhone1,2'.
+@property (readonly, nonatomic) NSString *platformName;
+
+/// Type of the device the software currently runs on.
+@property (readonly, nonatomic) LTDeviceType deviceType;
+
+/// Device type the software currently runs on as string.
+@property (readonly, nonatomic) NSString *deviceTypeString;
+
+/// \c YES if the device is an iPhone and has tall 4-inch screen (such as the iPhone 5).
+@property (readonly, nonatomic) BOOL has4InchScreen;
+
+// The number of screen points per inch on the device.
+@property (readonly, nonatomic) CGFloat pointsPerInch;
+
+/// The typical size (in points) of a finger on the device. This depends on the device's screen
+/// points per inch.
+@property (readonly, nonatomic) CGFloat fingerSizeInPoints;
+
+#pragma mark -
+#pragma mark Localization
+#pragma mark -
+
+/// Preferred language for the current device. This is the current iOS interface language, the one
+/// appearing on top of the list in Settings-General-International-Language.
+///
+/// @return the canonicalized IETF BCP 47 representation of the preferred language, or \c nil if the
+/// language is not available.
+@property (readonly, nonatomic) NSString *preferredLanguage;
+
+/// Language the app is currently using. In case a localization of the preferred language is not
+/// available, the best available language will be selected (according to the order of the languages
+/// in Settings-General-International-Language).
+///
+/// @return the canonicalized IETF BCP 47 representation of the preferred language, or \c nil if the
+/// language is not available.
+///
+/// @note the current app language is determined by the main bundle only, meaning it assumes that
+/// localizations to the main bundle apply to other bundles as well.
+@property (readonly, nonatomic) NSString *currentAppLanguage;
+
+#pragma mark -
+#pragma mark GPU
+#pragma mark -
+
+/// Maximal texture size that can be used on the device's GPU.
+@property (readonly, nonatomic) GLint maxTextureSize;
+
+/// Maximal number of texture units that can be used on the device GPU.
+@property (readonly, nonatomic) GLint maxTextureUnits;
+
+/// \c YES if writing to half-float textures is supported.
 @property (readonly, nonatomic) BOOL canRenderToHalfFloatTextures;
+
+/// \c YES if writing to float textures is supported.
+@property (readonly, nonatomic) BOOL canRenderToFloatTextures;
+
+@end
+
+#pragma mark -
+#pragma mark For testing
+#pragma mark -
+
+@interface LTDevice (ForTesting)
+
+/// Designated initializer: creates object for testing.
+///
+/// @param device \c UIDevice instance for device querying.
+/// @param screen \c UIScreen instance for screen querying.
+/// @param platformName name of the platform taken from \c uname.
+/// @param mainBundle main bundle of the app.
+- (id)initWithUIDevice:(UIDevice *)device UIScreen:(UIScreen *)screen
+          platformName:(NSString *)platformName mainBundle:(NSBundle *)mainBundle;
 
 @end
---
+++ LTKit/LTKitTests/LTDeviceSpec.mm
@@ -0,0 +1,174 @@
+// Copyright (c) 2013 Lightricks. All rights reserved.
+// Created by Yaron Inger.
+
+#import "LTDevice.h"
+
+SpecBegin(LTDevice)
+
+context(@"device idioms", ^{
+  context(@"ipad devices", ^{
+    __block LTDevice *device;
+
+    beforeEach(^{
+      UIDevice *uiDevice = mock([UIDevice class]);
+
+      [given(uiDevice.userInterfaceIdiom) willReturnInteger:UIUserInterfaceIdiomPad];
+
+      device = [[LTDevice alloc] initWithUIDevice:uiDevice
+                                         UIScreen:[UIScreen mainScreen]
+                                     platformName:@"iPad1"
+                                       mainBundle:[NSBundle mainBundle]];
+    });
+
+    afterEach(^{
+      device = nil;
+    });
+
+    it(@"should return correct idiom", ^{
+      expect(device.isPadIdiom).to.beTruthy();
+      expect(device.isPhoneIdiom).to.beFalsy();
+    });
+
+    it(@"should execute ipad block", ^{
+      __block BOOL executed = NO;
+
+      [device iPad:^{
+        executed = YES;
+      }];
+
+      expect(executed).to.beTruthy();
+    });
+
+    it(@"should execute ipad block and not iphone", ^{
+      __block BOOL iPadExecuted = NO;
+      __block BOOL iPhoneExecuted = NO;
+
+      [device iPhone:^{
+        iPhoneExecuted = YES;
+      } iPad:^{
+        iPadExecuted = YES;
+      }];
+
+      expect(iPhoneExecuted).to.beFalsy();
+      expect(iPadExecuted).to.beTruthy();
+    });
+
+    it(@"should not execute iphone block", ^{
+      __block BOOL executed = NO;
+
+      [device iPhone:^{
+        executed = YES;
+      }];
+
+      expect(executed).to.beFalsy();
+    });
+  });
+
+  context(@"iphone devices", ^{
+    __block LTDevice *device;
+
+    beforeEach(^{
+      UIDevice *uiDevice = mock([UIDevice class]);
+
+      [given(uiDevice.userInterfaceIdiom) willReturnInteger:UIUserInterfaceIdiomPhone];
+
+      device = [[LTDevice alloc] initWithUIDevice:uiDevice
+                                         UIScreen:[UIScreen mainScreen]
+                                     platformName:@"iPhone1"
+                                       mainBundle:[NSBundle mainBundle]];
+    });
+
+    afterEach(^{
+      device = nil;
+    });
+
+    it(@"should return correct idiom", ^{
+      expect(device.isPadIdiom).to.beFalsy();
+      expect(device.isPhoneIdiom).to.beTruthy();
+    });
+
+    it(@"should execute iphone block", ^{
+      __block BOOL executed = NO;
+
+      [device iPhone:^{
+        executed = YES;
+      }];
+
+      expect(executed).to.beTruthy();
+    });
+
+    it(@"should execute iphone block and not ipad", ^{
+      __block BOOL iPadExecuted = NO;
+      __block BOOL iPhoneExecuted = NO;
+
+      [device iPhone:^{
+        iPhoneExecuted = YES;
+      } iPad:^{
+        iPadExecuted = YES;
+      }];
+
+      expect(iPhoneExecuted).to.beTruthy();
+      expect(iPadExecuted).to.beFalsy();
+    });
+
+    it(@"should not execute ipad block", ^{
+      __block BOOL executed = NO;
+
+      [device iPad:^{
+        executed = YES;
+      }];
+
+      expect(executed).to.beFalsy();
+    });
+  });
+});
+
+context(@"platform info", ^{
+  __block LTDevice *device;
+
+  NSString *platformName = @"iPad1";
+
+  beforeEach(^{
+    device = [[LTDevice alloc] initWithUIDevice:[UIDevice currentDevice]
+                                       UIScreen:[UIScreen mainScreen]
+                                   platformName:platformName
+                                     mainBundle:[NSBundle mainBundle]];
+  });
+
+  afterEach(^{
+    device = nil;
+  });
+
+  it(@"should provide correct platform name", ^{
+    expect(device.platformName).to.equal(platformName);
+  });
+
+  it(@"should return correct device type", ^{
+    expect(device.deviceType).to.equal(LTDeviceTypeIPad1G);
+  });
+
+  it(@"should return correct device type string", ^{
+    expect(device.deviceTypeString).to.equal(@"LTDeviceTypeIPad1G");
+  });
+
+  pending(@"should return correct screen type", ^{
+    // TODO: (yaron) return a struct from UIScreen.bounds when OCMockito will support it.
+  });
+});
+
+context(@"localization", ^{
+  it(@"should return current app language", ^{
+    NSBundle *mainBundle = mock([NSBundle class]);
+
+    [given(mainBundle.preferredLocalizations) willReturn:@[@"en", @"fr-ca"]];
+
+    LTDevice *device = [[LTDevice alloc] initWithUIDevice:[UIDevice currentDevice]
+                                                 UIScreen:[UIScreen mainScreen]
+                                             platformName:nil
+                                               mainBundle:mainBundle];
+
+    expect(device.currentAppLanguage).to.equal(@"en");
+  });
+});
+
+SpecEnd
