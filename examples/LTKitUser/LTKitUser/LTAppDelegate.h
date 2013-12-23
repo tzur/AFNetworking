21ac7fca4aa09198dbb16088b6475072ab3e671b
Yaron Inger <yaron@lightricks.com>
2013-12-23 14:18:58 +0200

    Add LTKitUser, an example LTKit-supported project.

---
+++ examples/LTKitUser/LTKitUser/LTAppDelegate.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2013 Lightricks. All rights reserved.
+// Created by Yaron Inger.
+
+#import <UIKit/UIKit.h>
+
+@interface LTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
