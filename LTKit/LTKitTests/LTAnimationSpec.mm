cebd6b02518a193237e8aa9df04596671f046c34
Amit Goldstein <amit@lightricks.com>
2013-12-31 16:48:28 +0200

    Update tests to match a timeout of 0.1 seconds or less.

--- LTKit/LTKitTests/LTAnimationSpec.mm
+++ LTKit/LTKitTests/LTAnimationSpec.mm
@@ -9,47 +9,49 @@
 
 @end
 
+static const CGFloat kTargetFps = 60;
+static const CGFloat kTimeout = 5.0 / kTargetFps;
+static const double kUSecInSec = 1000000;
+
 SpecBegin(LTAnimation)
 
 beforeEach(^{
+  Expecta.asynchronousTestTimeout = kTimeout;
   [LTAnimation reset];
 });
 
 context(@"animations", ^{
-  const NSUInteger kTargetFps = 60;
-
   it(@"running a single animation", ^{
     __block NSUInteger counter = 0;
-    __block CFTimeInterval totalTime;
+    
     [LTAnimation animationWithBlock:^BOOL(CFTimeInterval __unused timeSinceLastFrame,
-                                          CFTimeInterval totalAnimationTime) {
-      totalTime = totalAnimationTime;
-      return (++counter < kTargetFps);
+                                          CFTimeInterval __unused totalAnimationTime) {
+      ++counter;
+      return YES;
     }];
-    expect(counter).will.equal(kTargetFps);
-    expect(1.0 - totalTime).will.beCloseToWithin(0, 0.05);
-    expect([LTAnimation isAnyAnimationRunning]).will.beFalsy();
+    expect(counter).will.equal(kTimeout * kTargetFps);
   });
   
   it(@"running two concurrent animations", ^{
     __block NSUInteger counterCombined = 0;
     __block NSUInteger counter1 = 0;
     __block NSUInteger counter2 = 0;
+    const NSUInteger targetFrames = kTimeout * kTargetFps;
     
     [LTAnimation animationWithBlock:^BOOL(CFTimeInterval __unused timeSinceLastFrame,
                                           CFTimeInterval __unused totalAnimationTime) {
       ++counter1;
-      return (++counterCombined < 2 * kTargetFps);
+      return (++counterCombined < 2 * targetFrames);
     }];
     [LTAnimation animationWithBlock:^BOOL(CFTimeInterval __unused timeSinceLastFrame,
                                           CFTimeInterval __unused totalAnimationTime) {
       ++counter2;
-      return (++counterCombined < 2 * kTargetFps);
+      return (++counterCombined < 2 * targetFrames);
     }];
     
-    expect(counter1).will.equal(kTargetFps);
-    expect(counter2).will.equal(kTargetFps);
-    expect(counterCombined).will.equal(2 * kTargetFps);
+    expect(counter1).will.equal(targetFrames);
+    expect(counter2).will.equal(targetFrames);
+    expect(counterCombined).will.equal(2 * targetFrames);
   });
   
   it(@"stopping a running animation", ^{
@@ -63,7 +65,7 @@ context(@"animations", ^{
     
     [animation stopAnimation];
     expect([LTAnimation isAnyAnimationRunning]).will.beFalsy();
-    usleep(100000);
+    usleep(kTimeout * kUSecInSec);
     expect(didExecute).to.beFalsy();
   });
   
@@ -82,7 +84,7 @@ context(@"animations", ^{
     }];
 
     expect([LTAnimation isAnyAnimationRunning]).will.beFalsy();
-    usleep(100000);
+    usleep(kTimeout * kUSecInSec);
     expect(counter).to.equal(1);
   });
 });
@@ -92,7 +94,7 @@ context(@"properties", ^{
     LTAnimation *animation = [LTAnimation animationWithBlock:
                               ^BOOL(CFTimeInterval __unused timeSinceLastFrame,
                                     CFTimeInterval __unused totalAnimationTime) {
-      return totalAnimationTime < 0.5 * [Expecta asynchronousTestTimeout];
+      return totalAnimationTime < 0.5 * kTimeout;
     }];
     expect(animation.isAnimating).to.beTruthy();
     expect(animation.isAnimating).will.beFalsy();
