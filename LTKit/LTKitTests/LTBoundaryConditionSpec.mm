691a55a385fa6c8466a0453caa5a32c5e3269b08
Yaron Inger <yaron@lightricks.com>
2014-01-07 18:21:22 +0200

    Change cv::Vec2i to cv::Size2i.

--- LTKit/LTKitTests/LTBoundaryConditionSpec.mm
+++ LTKit/LTKitTests/LTBoundaryConditionSpec.mm
@@ -44,21 +44,21 @@ context(@"two-dimensions boundary condition", ^{
     // Start.
     GLKVector2 start = [LTSymmetricBoundaryCondition
                         boundaryConditionForPoint:GLKVector2Make(0, 0)
-                        withSignalSize:cv::Vec2i(3, 5)];
+                        withSignalSize:cv::Size2i(3, 5)];
     expect(start.x).to.beCloseTo(0);
     expect(start.y).to.beCloseTo(0);
 
     // Middle.
     GLKVector2 middle = [LTSymmetricBoundaryCondition
                          boundaryConditionForPoint:GLKVector2Make(1.5, 3.5)
-                         withSignalSize:cv::Vec2i(3, 5)];
+                         withSignalSize:cv::Size2i(3, 5)];
     expect(middle.x).to.beCloseTo(1.5);
     expect(middle.y).to.beCloseTo(3.5);
 
     // End.
     GLKVector2 end = [LTSymmetricBoundaryCondition
                       boundaryConditionForPoint:GLKVector2Make(2.0, 4.0)
-                      withSignalSize:cv::Vec2i(3, 5)];
+                      withSignalSize:cv::Size2i(3, 5)];
     expect(end.x).to.beCloseTo(2.0);
     expect(end.y).to.beCloseTo(4.0);
   });
@@ -67,28 +67,28 @@ context(@"two-dimensions boundary condition", ^{
     // Left.
     GLKVector2 left = [LTSymmetricBoundaryCondition
                        boundaryConditionForPoint:GLKVector2Make(-0.5, -0.75)
-                       withSignalSize:cv::Vec2i(3, 5)];
+                       withSignalSize:cv::Size2i(3, 5)];
     expect(left.x).to.beCloseTo(0.5);
     expect(left.y).to.beCloseTo(0.75);
 
     // Right.
     GLKVector2 right = [LTSymmetricBoundaryCondition
                         boundaryConditionForPoint:GLKVector2Make(4.5, 6)
-                        withSignalSize:cv::Vec2i(3, 5)];
+                        withSignalSize:cv::Size2i(3, 5)];
     expect(right.x).to.beCloseTo(0.5);
     expect(right.y).to.beCloseTo(2.0);
 
     // Exactly one hop.
     GLKVector2 exact = [LTSymmetricBoundaryCondition
                         boundaryConditionForPoint:GLKVector2Make(4.0, 8.0)
-                        withSignalSize:cv::Vec2i(3, 5)];
+                        withSignalSize:cv::Size2i(3, 5)];
     expect(exact.x).to.beCloseTo(0);
     expect(exact.y).to.beCloseTo(0);
 
     // More than one hop.
     GLKVector2 hop = [LTSymmetricBoundaryCondition
                       boundaryConditionForPoint:GLKVector2Make(4.5, 9.0)
-                      withSignalSize:cv::Vec2i(3, 5)];
+                      withSignalSize:cv::Size2i(3, 5)];
     expect(hop.x).to.beCloseTo(0.5);
     expect(hop.y).to.beCloseTo(1);
   });
