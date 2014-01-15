d116c72c95feb9b63b1f7388f11a4217c29ae853
Zeev Farbman <zeev@lightricks.com>
2014-01-15 15:32:51 +0200

    Add convenience methods to LTGLKitExtensions.

--- LTKit/LTKit/LTGLKitExtensions.h
+++ LTKit/LTKit/LTGLKitExtensions.h
@@ -22,4 +22,24 @@ inline GLKVector4 operator*(const float &lhs, const GLKVector4 &rhs) {
   return GLKVector4MultiplyScalar(rhs, lhs);
 }
 
+/// Divide a vector by a scalar value.
+inline GLKVector3 operator/(const GLKVector3 &lhs, const float &rhs) {
+  return GLKVector3DivideScalar(lhs, rhs);
+}
+/// Divide a vector by a scalar value.
+inline GLKVector4 operator/(const GLKVector4 &lhs, const float &rhs)  {
+  return GLKVector4DivideScalar(lhs, rhs);
+}
+
+namespace std {
+  /// Find a sum of the components.
+  CG_INLINE float sum(const GLKVector3 &v) {
+    return v.x + v.y + v.z;
+  }
+  /// Find a sum of the components.
+  CG_INLINE float sum(const GLKVector4 &v) {
+    return v.x + v.y + v.z + v.w;
+  }
+}
+
 #endif
