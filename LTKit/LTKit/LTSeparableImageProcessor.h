142170c1b0c18158bcbabb5382cf148ec2cce197
Zeev Farbman <zeev@lightricks.com>
2014-01-22 10:30:48 +0200

    Small tweak.

--- LTKit/LTKit/LTSeparableImageProcessor.h
+++ LTKit/LTKit/LTSeparableImageProcessor.h
@@ -3,14 +3,14 @@
 
 #import "LTIterativeImageProcessor.h"
 
-/// Separable filter that alternates between horizontal and vertical filtering of the image. This
-/// processor allows to sample
+/// Separable filter that alternates between horizontal and vertical filtering of the image.
 @interface LTSeparableImageProcessor : LTIterativeImageProcessor
 
 /// Initializes a new separable filter processor with a single source texture texture, auxiliary
 /// textures and a varying number of output textures.
-/// @attention program should include texelOffset uniform, which allow to traverse the image pixel
-/// grid in the fragment shader.
+///
+/// @attention program should include \c texelOffset uniform, which allow to traverse the image
+/// pixel grid in the fragment shader.
 - (instancetype)initWithProgram:(LTProgram *)program sourceTexture:(LTTexture *)sourceTexture
               auxiliaryTextures:(NSDictionary *)auxiliaryTextures outputs:(NSArray *)outputs;
 
