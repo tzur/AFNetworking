c18fe2fc12e3dc4dd4e21a97ea3d53d7a5579706
Yaron Inger <yaron@lightricks.com>
2014-02-18 17:16:56 +0200

    Update docs about power of two inputs.

--- LTKit/LTKit/LTFFTProcessor.h
+++ LTKit/LTKit/LTFFTProcessor.h
@@ -39,13 +39,18 @@ typedef struct {
 /// default when executing an inverse transform, the normalization is being taken care as an
 /// additional step. Therefore, it is possible to avoid normalization and  gain a possible
 /// performance boost.
+///
+/// @note because of implementation details, the inputs and outputs to this processor must have
+/// dimensions which are a power of two.
 @interface LTFFTProcessor : NSObject <LTImageProcessor>
 
 /// Initializes with a real input (so the imaginary part is considered all zeroed) and a complex
-/// output struct.
+/// output struct. \c realInput and \c output must be of the same size and their dimensions must be
+/// a power of two.
 - (instancetype)initWithRealInput:(const cv::Mat1f &)realInput output:(LTSplitComplexMat *)output;
 
-/// Initializes with a complex input and a complex output structs.
+/// Initializes with a complex input and a complex output structs. \c input and \c output must be of
+/// the same size and their dimensions must be a power of two.
 - (instancetype)initWithInput:(LTSplitComplexMat)input output:(LTSplitComplexMat *)output;
 
 /// Direction of the Fourier transform to execute. Default value is LTFFTTransformDirectionForward.
