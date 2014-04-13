66913dca3e2342e9d8c51d2adfb6946f9a91f2d4
Yaron Inger <yaron@lightricks.com>
2014-04-13 10:55:11 +0300

    Add LTTimer.

---
+++ LTKit/LTKit/LTTimer.h
@@ -0,0 +1,40 @@
+// Copyright (c) 2014 Lightricks. All rights reserved.
+// Created by Yaron Inger.
+
+/// Class used for timing blocks of code using a high-precision timer which is independent on a
+/// runloop (such as \c NSTimer).
+@interface LTTimer : NSObject
+
+/// Returns the time took to execute the given block.
++ (CFTimeInterval)timeForBlock:(LTVoidBlock)block;
+
+/// Starts the timer. If the timer is already started, the call is ignored.
+- (void)start;
+
+/// Returns the time elapsed since the timer was started or the last split, if available.
+- (CFTimeInterval)split;
+
+/// Stops the timer and returns the time elapsed, in seconds, since the timer was started. If the
+/// timer is already stopped, this method will return \c 0.
+- (CFTimeInterval)stop;
+
+/// Returns \c YES if the timer is currently running.
+@property (nonatomic) BOOL isRunning;
+
+@end
+
+/// Protocol for object that returns the current time in seconds.
+@protocol LTTimeProvider <NSObject>
+
+/// Returns the current time in seconds. The time is relative to a fixed time point, which can be
+/// different than the Epoch (for example, the device uptime in seconds).
+- (CFTimeInterval)currentTime;
+
+@end
+
+@interface LTTimer (ForTesting)
+
+/// Initializes a new timer with the given time provider.
+- (instancetype)initWithTimeProvider:(id<LTTimeProvider>)timeProvider;
+
+@end
---
+++ LTKit/LTKit/LTTimer.m
@@ -0,0 +1,89 @@
+// Copyright (c) 2014 Lightricks. All rights reserved.
+// Created by Yaron Inger.
+
+#import "LTTimer.h"
+
+@interface LTDefaultTimeProvider : NSObject <LTTimeProvider>
+@end
+
+@implementation LTDefaultTimeProvider
+
+- (CFTimeInterval)currentTime {
+  return CACurrentMediaTime();
+}
+
+@end
+
+@interface LTTimer ()
+
+/// Time provider to use when calculating time intervals.
+@property (nonatomic) id<LTTimeProvider> timeProvider;
+
+/// Time the timer started.
+@property (nonatomic) CFTimeInterval startTime;
+
+/// Time the timer was last split.
+@property (nonatomic) CFTimeInterval splitTime;
+
+@end
+
+@implementation LTTimer
+
+- (id)init {
+  return [self initWithTimeProvider:[[LTDefaultTimeProvider alloc] init]];
+}
+
++ (CFTimeInterval)timeForBlock:(LTVoidBlock)block {
+  LTParameterAssert(block);
+
+  LTTimer *timer = [[LTTimer alloc] init];
+  [timer start];
+  block();
+  return [timer stop];
+}
+
+- (void)start {
+  if (self.isRunning) {
+    return;
+  }
+
+  self.startTime = [self.timeProvider currentTime];
+  self.splitTime = self.startTime;
+  self.isRunning = YES;
+}
+
+- (CFTimeInterval)split {
+  CFTimeInterval currentTime = [self.timeProvider currentTime];
+  CFTimeInterval timeElapsed = currentTime - self.splitTime;
+
+  self.splitTime = currentTime;
+
+  return timeElapsed;
+}
+
+- (CFTimeInterval)stop {
+  if (!self.isRunning) {
+    return 0;
+  }
+
+  CFTimeInterval timeElapsed = [self.timeProvider currentTime] - self.startTime;
+
+  self.startTime = 0;
+  self.splitTime = 0;
+  self.isRunning = NO;
+
+  return timeElapsed;
+}
+
+@end
+
+@implementation LTTimer (ForTesting)
+
+- (instancetype)initWithTimeProvider:(id<LTTimeProvider>)timeProvider {
+  if (self = [super init]) {
+    self.timeProvider = timeProvider;
+  }
+  return self;
+}
+
+@end
