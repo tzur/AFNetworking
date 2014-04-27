2744649e13149df5902109d624822335a5ff8a88
Yaron Inger <yaron@lightricks.com>
2014-04-27 14:42:01 +0300

    Add LTRecomposeProcessor.

---
+++ LTKit/LTKit/LTRecomposeProcessor.mm
@@ -0,0 +1,258 @@
+// Copyright (c) 2014 Lightricks. All rights reserved.
+// Created by Yaron Inger.
+
+#import "LTRecomposeProcessor.h"
+
+#import <objc/runtime.h>
+
+#import "LTCGExtensions.h"
+#import "LTFbo.h"
+#import "LTInverseTransformSampler.h"
+#import "LTMultiRectDrawer.h"
+#import "LTProgram.h"
+#import "LTRotatedRect.h"
+#import "LTShaderStorage+LTPassthroughShaderFsh.h"
+#import "LTShaderStorage+LTPassthroughShaderVsh.h"
+#import "LTTexture+Factory.h"
+
+@interface LTRecomposeProcessor ()
+
+/// Drawer used to draw multiple rects (pixel rows) into the target.
+@property (strong, nonatomic) LTMultiRectDrawer *drawer;
+
+/// Framebuffer referencing the output texture.
+@property (strong, nonatomic) LTFbo *fbo;
+
+/// Input image.
+@property (strong, nonatomic) LTTexture *input;
+
+/// Mask image.
+@property (strong, nonatomic) LTTexture *mask;
+
+/// Output image.
+@property (strong, nonatomic) LTTexture *output;
+
+/// Source rects (pixel lines) in input texture coordinates.
+@property (strong, nonatomic) NSArray *sourceRects;
+
+/// Target rects (pixel lines) in output texture coordinates.
+@property (strong, nonatomic) NSArray *targetRects;
+
+/// Ordered (first to last) indices of lines (vertical or horizontal, depending on \c
+/// decimationDimensionSize) to remove.
+@property (strong, nonatomic) NSArray *decimationOrder;
+
+/// Size of the input image of the dimension the processor decimates.
+@property (readonly, nonatomic) CGFloat decimationDimensionSize;
+
+@end
+
+@implementation LTRecomposeProcessor
+
+#pragma mark -
+#pragma mark Initialization
+#pragma mark -
+
+- (instancetype)initWithInput:(LTTexture *)input mask:(LTTexture *)mask output:(LTTexture *)output {
+  LTParameterAssert(input.size == output.size, @"Input size must be equal to output size");
+  LTParameterAssert(input.size == mask.size, @"Input size must be equal to mask size");
+
+  if (self = [super init]) {
+    self.drawer = [[LTMultiRectDrawer alloc] initWithProgram:[self createProgram]
+                                               sourceTexture:input];
+    self.fbo = [[LTFbo alloc] initWithTexture:output];
+
+    self.input = input;
+    self.mask = mask;
+    self.output = output;
+
+    self.samplerFactory = [[LTInverseTransformSamplerFactory alloc] init];
+    self.decimationDimension = [self defaultDecimationDimension];
+  }
+  return self;
+}
+
+- (LTProgram *)createProgram {
+  return [[LTProgram alloc] initWithVertexSource:[LTPassthroughShaderVsh source]
+                                  fragmentSource:[LTPassthroughShaderFsh source]];
+}
+
+- (LTRecomposeDecimationDimension)defaultDecimationDimension {
+  return self.input.size.width > self.input.size.height ?
+      LTRecomposeDecimationDimensionHorizontal : LTRecomposeDecimationDimensionVertical;
+}
+
+#pragma mark -
+#pragma mark Mask
+#pragma mark -
+
+- (void)setMaskUpdated {
+  self.decimationOrder = nil;
+}
+
+- (NSMutableArray *)calculateMaskFrequencies {
+  __block cv::Mat1f sum;
+
+  // Sum the mask to get a 1D signal.
+  [self.mask mappedImageForReading:^(const cv::Mat &mapped, BOOL) {
+    cv::reduce(mapped, sum, self.decimationDimension, CV_REDUCE_SUM, CV_32F);
+  }];
+
+  static const double kBlurSize = 21;
+  static const float kEpsilon = 1;
+
+  // Smooth the summed mask.
+  cv::Mat1f smoothedSum(sum.size());
+  cv::GaussianBlur(sum, smoothedSum, cv::Size(0, 0), kBlurSize);
+
+  // Get maximal value.
+  float maxValue = *std::max_element(smoothedSum.begin(), smoothedSum.end());
+
+  // Inverse values so a white mask will cause less throws than black, and make sure that each line
+  // has a minimal small frequency so it will be able to be selected when sampling.
+  smoothedSum = maxValue - smoothedSum + kEpsilon;
+
+  NSMutableArray *frequencies = [NSMutableArray array];
+  for (float value : smoothedSum) {
+    [frequencies addObject:@(value)];
+  }
+
+  return frequencies;
+}
+
+#pragma mark -
+#pragma mark Processing
+#pragma mark -
+
+- (id<LTImageProcessorOutput>)process {
+  [self updateDecimationOrderIfNeeded];
+  [self updateRectsForLinesToDecimate];
+
+  [self.drawer drawRotatedRects:self.targetRects inFramebuffer:self.fbo
+               fromRotatedRects:self.sourceRects];
+
+  return [[LTSingleTextureOutput alloc] initWithTexture:self.output];
+}
+
+- (void)updateDecimationOrderIfNeeded {
+  if (!self.decimationOrder) {
+    NSMutableArray *frequencies = [self calculateMaskFrequencies];
+    self.decimationOrder = [self calculateDecimationOrderUsingFrequencies:frequencies];
+  }
+}
+
+- (CGFloat)decimationDimensionSize {
+  switch (self.decimationDimension) {
+    case LTRecomposeDecimationDimensionHorizontal:
+      return self.input.size.width;
+    case LTRecomposeDecimationDimensionVertical:
+      return self.input.size.height;
+  }
+}
+
+- (NSArray *)calculateDecimationOrderUsingFrequencies:(NSMutableArray *)frequencies {
+  NSMutableArray *decimationOrder = [NSMutableArray array];
+
+  // Sample from the distribution with no repetitions.
+  while (decimationOrder.count < self.decimationDimensionSize) {
+    id<LTDistributionSampler> sampler = [self.samplerFactory samplerWithFrequencies:frequencies];
+    NSUInteger samplesRequired = self.decimationDimensionSize - decimationOrder.count;
+
+    // NSOrderedSet is used here instead of NSSet to preserve the sampling order, so
+    // high-probability lines for decimation will be decimated first (since they will probably be
+    // sampled first). This also aids testing since the order of samples is not shuffled when using
+    // NSSet's hashing.
+    NSOrderedSet *samples = [NSOrderedSet orderedSetWithArray:[sampler sample:samplesRequired]];
+    [decimationOrder addObjectsFromArray:[samples array]];
+    for (NSNumber *index in samples) {
+      frequencies[[index unsignedIntegerValue]] = @0;
+    }
+  }
+
+  return [decimationOrder copy];
+}
+
+- (void)updateRectsForLinesToDecimate {
+  NSSet *linesToThrow = [NSSet setWithArray:[self.decimationOrder
+                                             subarrayWithRange:NSMakeRange(0,
+                                                                           self.linesToDecimate)]];
+
+  NSMutableArray *imageCoordinates = [NSMutableArray array];
+  for (NSUInteger i = 0; i < self.decimationDimensionSize; ++i) {
+    if ([linesToThrow containsObject:@(i)]) {
+      continue;
+    }
+    [imageCoordinates addObject:@(i)];
+  }
+
+  NSMutableArray *sourceRects = [NSMutableArray array];
+  NSMutableArray *targetRects = [NSMutableArray array];
+  [imageCoordinates enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *) {
+    [sourceRects addObject:[self sourceRectForIndex:[obj unsignedIntegerValue]]];
+    [targetRects addObject:[self targetRectForIndex:idx]];
+  }];
+
+  self.sourceRects = [sourceRects copy];
+  self.targetRects = [targetRects copy];
+}
+
+- (LTRotatedRect *)sourceRectForIndex:(NSUInteger)index {
+  switch (self.decimationDimension) {
+    case LTRecomposeDecimationDimensionHorizontal:
+      return [LTRotatedRect rect:CGRectMake(index, 0, 1, self.input.size.height)];
+    case LTRecomposeDecimationDimensionVertical:
+      return [LTRotatedRect rect:CGRectMake(0, index, self.input.size.width, 1)];
+  }
+}
+
+- (LTRotatedRect *)targetRectForIndex:(NSUInteger)index {
+  switch (self.decimationDimension) {
+    case LTRecomposeDecimationDimensionHorizontal:
+      return [LTRotatedRect rect:CGRectMake(index, 0, 1, self.input.size.height)];
+    case LTRecomposeDecimationDimensionVertical:
+      return [LTRotatedRect rect:CGRectMake(0, index, self.input.size.width, 1)];
+  }
+}
+
+#pragma mark -
+#pragma mark Properties
+#pragma mark -
+
+- (void)setDecimationDimension:(LTRecomposeDecimationDimension)decimationDimension {
+  _decimationDimension = decimationDimension;
+  self.linesToDecimate = std::min(self.linesToDecimate,
+                                  (NSUInteger)self.decimationDimensionSize);
+  self.decimationOrder = nil;
+}
+
+- (void)setLinesToDecimate:(NSUInteger)linesToDecimate {
+  LTParameterAssert(linesToDecimate <= self.decimationDimensionSize);
+  _linesToDecimate = linesToDecimate;
+}
+
+#pragma mark -
+#pragma mark Model values
+#pragma mark -
+
+- (id)objectForKeyedSubscript:(NSString __unused *)key {
+  LTMethodNotImplemented();
+}
+
+- (void)setObject:(id __unused)obj forKeyedSubscript:(NSString __unused *)key {
+  LTMethodNotImplemented();
+}
+
+@end
+
+@implementation LTRecomposeProcessor (ForTesting)
+
+- (void)setSamplerFactory:(id<LTDistributionSamplerFactory>)samplerFactory {
+  objc_setAssociatedObject(self, @selector(samplerFactory), samplerFactory,
+                           OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+- (id<LTDistributionSamplerFactory>)samplerFactory {
+  return objc_getAssociatedObject(self, @selector(samplerFactory));
+}
+
+@end
---
+++ LTKit/LTKitTests/LTRecomposeProcessorSpec.mm
@@ -0,0 +1,208 @@
+// Copyright (c) 2014 Lightricks. All rights reserved.
+// Created by Yaron Inger.
+
+#import "LTRecomposeProcessor.h"
+
+#import "LTInverseTransformSampler.h"
+#import "LTOpenCVExtensions.h"
+#import "LTTexture+Factory.h"
+
+typedef std::pair<NSUInteger, float> LTIndexedFloat;
+
+@interface LTDegenerateSampler : NSObject <LTDistributionSampler> {
+  std::vector<LTIndexedFloat> _pairs;
+}
+@end
+
+@implementation LTDegenerateSampler
+
+- (instancetype)initWithFrequencies:(NSArray *)frequencies {
+  if (self = [super init]) {
+    [frequencies enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *) {
+      _pairs.push_back(std::make_pair(idx, [obj floatValue]));
+    }];
+    std::stable_sort(_pairs.begin(), _pairs.end(), [](LTIndexedFloat a, LTIndexedFloat b) {
+      return a.second > b.second;
+    });
+  }
+  return self;
+}
+
+- (NSArray *)sample:(NSUInteger)times {
+  NSMutableArray *values = [NSMutableArray array];
+  for (NSUInteger i = 0; i < times; ++i) {
+    [values addObject:@(_pairs[i % _pairs.size()].first)];
+  }
+  return [values copy];
+}
+
+@end
+
+@interface LTDegenerateSamplerFactory : NSObject <LTDistributionSamplerFactory>
+@end
+
+@implementation LTDegenerateSamplerFactory
+
+- (id<LTDistributionSampler>)samplerWithFrequencies:(NSArray *)frequencies {
+  return [[LTDegenerateSampler alloc] initWithFrequencies:frequencies];
+}
+
+@end
+
+SpecBegin(LTRecomposeProcessor)
+
+beforeEach(^{
+  LTGLContext *context = [[LTGLContext alloc] init];
+  [LTGLContext setCurrentContext:context];
+});
+
+afterEach(^{
+  [LTGLContext setCurrentContext:nil];
+});
+
+context(@"initialization", ^{
+  it(@"should not initialize if input size is different than mask size", ^{
+    LTTexture *input = [LTTexture byteRGBATextureWithSize:CGSizeMake(4, 4)];
+    LTTexture *mask = [LTTexture byteRGBATextureWithSize:CGSizeMake(8, 8)];
+    LTTexture *output = [LTTexture textureWithPropertiesOf:input];
+
+    expect(^{
+      LTRecomposeProcessor __unused *processor = [[LTRecomposeProcessor alloc]
+                                                  initWithInput:input mask:mask output:output];
+    }).to.raise(NSInvalidArgumentException);
+  });
+
+  it(@"should not initialize if input size is different than output size", ^{
+    LTTexture *input = [LTTexture byteRGBATextureWithSize:CGSizeMake(4, 4)];
+    LTTexture *mask = [LTTexture textureWithPropertiesOf:input];
+    LTTexture *output = [LTTexture byteRGBATextureWithSize:CGSizeMake(8, 8)];
+
+    expect(^{
+      LTRecomposeProcessor __unused *processor = [[LTRecomposeProcessor alloc]
+                                                  initWithInput:input mask:mask output:output];
+    }).to.raise(NSInvalidArgumentException);
+  });
+});
+
+context(@"processing", ^{
+  __block LTRecomposeProcessor *processor;
+  __block cv::Mat4b image;
+  __block LTTexture *mask;
+
+  beforeEach(^{
+    image = cv::Mat4b(4, 4);
+    image(cv::Rect(0, 0, 1, 4)) = cv::Vec4b(255, 0, 0, 255);
+    image(cv::Rect(1, 0, 1, 4)) = cv::Vec4b(0, 255, 0, 255);
+    image(cv::Rect(2, 0, 1, 4)) = cv::Vec4b(0, 0, 255, 255);
+    image(cv::Rect(3, 0, 1, 4)) = cv::Vec4b(0, 0, 0, 255);
+
+    LTTexture *input = [LTTexture textureWithImage:image];
+    mask = [LTTexture textureWithSize:input.size precision:LTTexturePrecisionByte
+                               format:LTTextureFormatRed allocateMemory:YES];
+    LTTexture *output = [LTTexture textureWithPropertiesOf:input];
+
+    [mask clearWithColor:GLKVector4Make(0, 0, 0, 1)];
+    [output clearWithColor:GLKVector4Make(0, 0, 0, 0)];
+
+    processor = [[LTRecomposeProcessor alloc] initWithInput:input mask:mask output:output];
+    processor.samplerFactory = [[LTDegenerateSamplerFactory alloc] init];
+    processor.linesToDecimate = 2;
+  });
+
+  afterEach(^{
+    mask = nil;
+    processor = nil;
+  });
+
+  it(@"should decimate horizontally", ^{
+    processor.decimationDimension = LTRecomposeDecimationDimensionHorizontal;
+    LTSingleTextureOutput *result = [processor process];
+
+    // Take cols 2,3.
+    cv::Mat4b expected(4, 4, cv::Vec4b(0, 0, 0, 0));
+    image(cv::Rect(2, 0, 1, 4)).copyTo(expected(cv::Rect(0, 0, 1, 4)));
+    image(cv::Rect(3, 0, 1, 4)).copyTo(expected(cv::Rect(1, 0, 1, 4)));
+
+    expect($([result.texture image])).to.equalMat($(expected));
+  });
+
+  it(@"should decimate vertically", ^{
+    processor.decimationDimension = LTRecomposeDecimationDimensionVertical;
+    LTSingleTextureOutput *result = [processor process];
+
+    // Take cols 2,3.
+    cv::Mat4b expected(4, 4, cv::Vec4b(0, 0, 0, 0));
+    image(cv::Rect(0, 2, 4, 1)).copyTo(expected(cv::Rect(0, 0, 4, 1)));
+    image(cv::Rect(0, 3, 4, 1)).copyTo(expected(cv::Rect(0, 1, 4, 1)));
+
+    expect($([result.texture image])).to.equalMat($(expected));
+  });
+
+  it(@"should decimate according to mask", ^{
+    [mask mappedImageForWriting:^(cv::Mat *mapped, BOOL) {
+      (*mapped)(cv::Rect(1, 0, 1, 4)).setTo(255);
+    }];
+    [processor setMaskUpdated];
+
+    processor.decimationDimension = LTRecomposeDecimationDimensionHorizontal;
+    LTSingleTextureOutput *result = [processor process];
+
+    // Take cols 1,3, since a mask is blocking column 1 from disappearing.
+    cv::Mat4b expected(4, 4, cv::Vec4b(0, 0, 0, 0));
+    image(cv::Rect(1, 0, 1, 4)).copyTo(expected(cv::Rect(0, 0, 1, 4)));
+    image(cv::Rect(3, 0, 1, 4)).copyTo(expected(cv::Rect(1, 0, 1, 4)));
+
+    expect($([result.texture image])).to.equalMat($(expected));
+  });
+});
+
+context(@"properties", ^{
+  it(@"should limit lines to decimate range", ^{
+    LTTexture *input = [LTTexture textureWithImage:cv::Mat4b(2, 4)];
+    LTTexture *mask = [LTTexture textureWithPropertiesOf:input];
+    LTTexture *output = [LTTexture textureWithPropertiesOf:input];
+
+    LTRecomposeProcessor *processor = [[LTRecomposeProcessor alloc] initWithInput:input
+                                                                             mask:mask
+                                                                           output:output];
+
+    expect(^{
+      processor.linesToDecimate = 4;
+    }).toNot.raiseAny();
+
+    expect(^{
+      processor.linesToDecimate = 5;
+    }).to.raise(NSInvalidArgumentException);
+
+    processor.decimationDimension = LTRecomposeDecimationDimensionVertical;
+    expect(^{
+      processor.linesToDecimate = 3;
+    }).to.raise(NSInvalidArgumentException);
+  });
+
+  it(@"should have correct default decimationDimension value for wide images", ^{
+    LTTexture *input = [LTTexture textureWithImage:cv::Mat4b(2, 4)];
+    LTTexture *mask = [LTTexture textureWithPropertiesOf:input];
+    LTTexture *output = [LTTexture textureWithPropertiesOf:input];
+
+    LTRecomposeProcessor *processor = [[LTRecomposeProcessor alloc] initWithInput:input
+                                                                             mask:mask
+                                                                           output:output];
+
+    expect(processor.decimationDimension).to.equal(LTRecomposeDecimationDimensionHorizontal);
+  });
+
+  it(@"should have correct default decimationDimension value for tall images", ^{
+    LTTexture *input = [LTTexture textureWithImage:cv::Mat4b(4, 2)];
+    LTTexture *mask = [LTTexture textureWithPropertiesOf:input];
+    LTTexture *output = [LTTexture textureWithPropertiesOf:input];
+
+    LTRecomposeProcessor *processor = [[LTRecomposeProcessor alloc] initWithInput:input
+                                                                             mask:mask
+                                                                           output:output];
+
+    expect(processor.decimationDimension).to.equal(LTRecomposeDecimationDimensionVertical);
+  });
+});
+
+SpecEnd
