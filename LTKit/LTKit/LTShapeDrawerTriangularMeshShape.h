8e33798c1e5872529126526c439d62b2b53ab8e5
Amit Goldstein <amit@lightricks.com>
2014-05-15 12:44:52 +0300

    Add LTShapeDrawerTriangularMeshShape class.

---
+++ LTKit/LTKit/LTShapeDrawerTriangularMeshShape.h
@@ -0,0 +1,17 @@
+// Copyright (c) 2014 Lightricks. All rights reserved.
+// Created by Amit Goldstein.
+
+#import "LTCGExtensions.h"
+#import "LTCommonDrawableShape.h"
+#import "LTDrawableShape.h"
+
+/// @class LTShapeDrawerTriangularMeshShape
+///
+/// A class used for drawing filled 2D triangular meshes. This is usually used when drawing custom
+/// shapes like arrows, rectangles, etc.
+@interface LTShapeDrawerTriangularMeshShape : LTCommonDrawableShape <LTDrawableShape>
+
+/// Fills the given triangle with shadows on the edges defined in the given mask.
+- (void)fillTriangle:(CGTriangle)triangle withShadowOnEdges:(CGTriangleEdgeMask)edgeMask;
+
+@end
