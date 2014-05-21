8b47c4fb76cc38dc33c978e076794436220182b5
Yaron Inger <yaron@lightricks.com>
2014-05-21 14:31:28 +0300

    Add NSError+LTKit.

---
+++ LTKit/LTKit/NSError+LTKit.h
@@ -0,0 +1,5 @@
+// Copyright (c) 2014 Lightricks. All rights reserved.
+// Created by Yaron Inger.
+
+/// Error domain for LTKit.
+extern NSString * const kLTKitErrorDomain;
---
+++ LTKit/LTKit/NSError+LTKit.m
@@ -0,0 +1,6 @@
+// Copyright (c) 2014 Lightricks. All rights reserved.
+// Created by Yaron Inger.
+
+#import "NSError+LTKit.h"
+
+NSString * const kLTKitErrorDomain = @"com.lightricks.LTKit";
