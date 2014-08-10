aadb36e04f42cd56bab83e4286b102cfa9610126
Yaron Inger <yaron@lightricks.com>
2014-08-10 13:59:07 +0300

    LTGPUImage: add processWithPlacement: to allow code reuse.
    This will allow subclasses to define their placement processing while
    reusing LTProcessingStrategy calls defined in the base class.

--- LTKit/LTKit/LTGPUImageProcessor+Protected.h
+++ LTKit/LTKit/LTGPUImageProcessor+Protected.h
@@ -3,6 +3,13 @@
 
 #import "LTGPUImageProcessor.h"
 
+@protocol LTProcessingDrawer, LTProcessingStrategy;
+
+@class LTNextIterationPlacement;
+
+/// Block for processing a source texture to a target framebuffer defined in the given \c placement.
+typedef void (^LTProcessWithPlacementBlock)(LTNextIterationPlacement *placement);
+
 @interface LTGPUImageProcessor (Protected)
 
 /// Sets a processor's program's uniform \c key to the given \c value.
@@ -14,6 +21,11 @@
 /// Adds or updates a single auxiliary texture to the auxiliary textures dictionary. The texture
 /// will be retained by the processor.
 - (void)setAuxiliaryTexture:(LTTexture *)texture withName:(NSString *)name;
+
+/// Executes the processor's strategy and calls the given \c block with a placement configuration.
+/// The block should process the source texture to the target framebuffer. For multi-pass
+/// strategies, the block may be called multiple times with different placements.
+- (void)processWithPlacement:(LTProcessWithPlacementBlock)block;
 
 /// Dictionary of \c NSString to \c LTTexture of axiliary textures to use to assist processing.
 @property (strong, nonatomic) NSDictionary *auxiliaryTextures;
