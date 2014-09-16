cf203a7931c27775904c5843cc5176f988da7e35
Amit Goldstein <amit@lightricks.com>
2014-09-16 11:41:22 +0300

    LTOneShotImageProcessor: refactor into LTOneShotBaseImageProcessor.
    Split LTOneShotImageProcessor to LTOneShotBaseImageProcessor that has
    the LTScreenProcessing and LTPartialProcessing capabilities and
    implementation, and the LTOneShotImageProcessor which is its subclass
    with an LTRectDrawer as its drawer.

--- LTKit/LTKit/LTOneShotImageProcessor.h
+++ LTKit/LTKit/LTOneShotImageProcessor.h
@@ -1,15 +1,9 @@
 // Copyright (c) 2014 Lightricks. All rights reserved.
-// Created by Yaron Inger.
+// Created by Amit Goldstein.
 
-#import "LTGPUImageProcessor.h"
+#import "LTOneShotBaseImageProcessor.h"
 
-#import "LTPartialProcessing.h"
-#import "LTScreenProcessing.h"
-
-/// @class LTOneShotImageProcessor
-///
-/// Processes a single image input with a single processing iteration, and returns a single output.
-@interface LTOneShotImageProcessor : LTGPUImageProcessor <LTPartialProcessing, LTScreenProcessing>
+@interface LTOneShotImageProcessor : LTOneShotBaseImageProcessor
 
 /// Initializes with vertex and fragment shaders sources, a single input texture and a single output
 /// texture.
@@ -17,24 +11,13 @@
                       fragmentSource:(NSString *)fragmentSource
                                input:(LTTexture *)input andOutput:(LTTexture *)output;
 
-/// Initializes with vertex and fragment shaders sources, a source texture (which defines the
-/// coordinate system for processing), additional input auxiliary textures and an output texture.
+/// Designater initializer: Initializes with vertex and fragment shaders sources, a source texture
+/// (which defines the coordinate system for processing), additional input auxiliary textures and an
+/// output texture.
 - (instancetype)initWithVertexSource:(NSString *)vertexSource
                       fragmentSource:(NSString *)fragmentSource
                        sourceTexture:(LTTexture *)sourceTexture
                    auxiliaryTextures:(NSDictionary *)auxiliaryTextures
                            andOutput:(LTTexture *)output;
-
-/// Size of the input texture or the source texture, if there are auxiliary textures available.
-@property (readonly, nonatomic) CGSize inputSize;
-
-/// Size of the output texture.
-@property (readonly, nonatomic) CGSize outputSize;
-
-/// Output texture of the processor.
-@property (readonly, nonatomic) LTTexture *outputTexture;
-
-/// Input texture of the processor.
-@property (readonly, nonatomic) LTTexture *inputTexture;
 
 @end
