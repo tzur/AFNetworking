a497c066b5e6cecfa25ad94cff8a7f677811a472
Amit Shabtay <ashabtay@lightricks.com>
2015-04-20 11:02:35 +0300

    LTCircularPatchProcessor: initial implementation.

--- LTKit/LTKit/LTKit.h
+++ LTKit/LTKit/LTKit.h
@@ -53,6 +53,7 @@
 #import <LTKit/LTBilateralFilterProcessor.h>
 #import <LTKit/LTBoxFilterProcessor.h>
 #import <LTKit/LTBWProcessor.h>
+#import <LTKit/LTCircularPatchProcessor.h>
 #import <LTKit/LTClarityProcessor.h>
 #import <LTKit/LTColorConversionProcessor.h>
 #import <LTKit/LTColorGradient.h>
