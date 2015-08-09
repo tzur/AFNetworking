ceeeb771545ac3282f2eeefad3b7863b0be0baac
Yaron Inger <yaron@lightricks.com>
2015-08-09 17:25:17 +0300

    Photons.h: initial commit.
    Required for dynamic framework.

---
+++ Photons/Photons.h
@@ -0,0 +1,29 @@
+// Copyright (c) 2015 Lightricks. All rights reserved.
+// Created by Yaron Inger.
+
+#import <Foundation/Foundation.h>
+#import <ReactiveCocoa/ReactiveCocoa.h>
+
+/// Project version number for Photons.
+FOUNDATION_EXPORT double PhotonsVersionNumber;
+
+/// Project version string for Photons.
+FOUNDATION_EXPORT const unsigned char PhotonsVersionString[];
+
+/// Base.
+#import <Photons/PTNAlbum.h>
+#import <Photons/PTNAlbumChangeset.h>
+#import <Photons/PTNAlbumChangesetMove.h>
+#import <Photons/PTNAssetManager.h>
+#import <Photons/PTNCollection.h>
+#import <Photons/PTNImageFetchOptions.h>
+#import <Photons/PTNObject.h>
+#import <Photons/PTNProgress.h>
+
+/// PhotoKit.
+#import <Photons/NSURL+PhotoKit.h>
+#import <Photons/PTNPhotoKitAlbum.h>
+#import <Photons/PTNPhotoKitAlbumType.h>
+#import <Photons/PTNPhotoKitAssetManager.h>
+#import <Photons/PTNPhotoKitFetcher.h>
+#import <Photons/PTNPhotoKitObserver.h>
