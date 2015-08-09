92bfacdc43a60f80d7da978d5bc83108b32af0ed
Yaron Inger <yaron@lightricks.com>
2015-08-09 17:22:02 +0300

    NSError+Photons: add more error types.
    These methods will be converted to methods that create an error given
    code and user info soon.

--- Photons/NSError+Photons.m
+++ Photons/NSError+Photons.m
@@ -6,20 +6,59 @@
 NS_ASSUME_NONNULL_BEGIN
 
 NSString * kPTNErrorDomain = @"com.lightricks.Photons";
+
 NSString * kPTNErrorRequestURLKey = @"URL";
+NSString * kPTNErrorAssociatedObjectKey = @"AssociatedObject";
 
 @implementation NSError (Photons)
 
-+ (NSError *)ptn_invalidURL:(NSURL *)url {
++ (instancetype)ptn_invalidURL:(NSURL *)url {
   return [NSError errorWithDomain:kPTNErrorDomain
                              code:PTNErrorCodeInvalidURL
                          userInfo:@{kPTNErrorRequestURLKey: url ?: [NSNull null]}];
 }
 
-+ (NSError *)ptn_albumNotFound:(NSURL *)url {
++ (instancetype)ptn_albumNotFound:(NSURL *)url {
   return [NSError errorWithDomain:kPTNErrorDomain
                              code:PTNErrorCodeAlbumNotFound
                          userInfo:@{kPTNErrorRequestURLKey: url ?: [NSNull null]}];
+}
+
++ (instancetype)ptn_assetNotFound:(NSURL *)url {
+  return [NSError errorWithDomain:kPTNErrorDomain
+                             code:PTNErrorCodeAssetNotFound
+                         userInfo:@{kPTNErrorRequestURLKey: url ?: [NSNull null]}];
+}
+
++ (instancetype)ptn_keyAssetsNotFound:(NSURL *)url {
+  return [NSError errorWithDomain:kPTNErrorDomain
+                             code:PTNErrorCodeKeyAssetsNotFound
+                         userInfo:@{kPTNErrorRequestURLKey: url ?: [NSNull null]}];
+}
+
++ (instancetype)ptn_assetLoadingFailed:(NSURL *)url {
+  return [NSError errorWithDomain:kPTNErrorDomain
+                             code:PTNErrorCodeAssetLoadingFailed
+                         userInfo:@{kPTNErrorRequestURLKey: url ?: [NSNull null]}];
+}
+
++ (instancetype)ptn_assetLoadingFailed:(NSURL *)url underlyingError:(NSError *)error {
+  return [NSError errorWithDomain:kPTNErrorDomain
+                             code:PTNErrorCodeAssetLoadingFailed
+                         userInfo:@{kPTNErrorRequestURLKey: url ?: [NSNull null],
+                                    NSUnderlyingErrorKey: error ?: [NSNull null]}];
+}
+
++ (instancetype)ptn_invalidAssetType:(NSURL *)url {
+  return [NSError errorWithDomain:kPTNErrorDomain
+                             code:PTNErrorCodeInvalidAssetType
+                         userInfo:@{kPTNErrorRequestURLKey: url ?: [NSNull null]}];
+}
+
++ (instancetype)ptn_invalidObject:(id<NSObject>)object {
+  return [NSError errorWithDomain:kPTNErrorDomain
+                             code:PTNErrorCodeInvalidObject
+                         userInfo:@{kPTNErrorAssociatedObjectKey: object ?: [NSNull null]}];
 }
 
 @end
