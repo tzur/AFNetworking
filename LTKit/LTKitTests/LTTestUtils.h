089f5fc94b1833a8f992a862f030e37763765fa0
Yaron Inger <yaron@lightricks.com>
2015-08-20 15:13:35 +0300

    LTTestUtils: remove irrelevant methods.

--- LTKit/LTKitTests/LTTestUtils.h
+++ LTKit/LTKitTests/LTTestUtils.h
@@ -3,8 +3,6 @@
 
 #import <numeric>
 
-#import "LTBlendMode.h"
-
 /// Returns an absolute path to the given file name. The path depends on the specific spec currently
 /// running, so there's no concern for files overwriting each other. If no argument is given, the
 /// directory itself is returned.
@@ -29,84 +27,6 @@ void dit(NSString *name, id block);
 
 /// Returns \c YES if currently running application tests (and not logic tests).
 BOOL LTRunningApplicationTests();
-
-/// Returns \c YES if the two given matrices are equal. Matrices are equal if their \c size,
-/// \c depth, \c channels and actual data are all equal.
-BOOL LTCompareMat(const cv::Mat &expected, const cv::Mat &actual,
-                  cv::Point *firstMismatch = nullptr);
-
-/// Returns \c YES if the two given matrices are equal, up to the given \c range, which is set
-/// across all channels. Matrices are equal if their \c size, \c depth, \c channels and actual data
-/// are all equal.
-BOOL LTFuzzyCompareMat(const cv::Mat &expected, const cv::Mat &actual, double range = 1,
-                       cv::Point *firstMismatch = nullptr);
-
-/// Returns \c YES if the given \c actual matrix cells are all equal to the given \c expected
-/// scalar.
-BOOL LTCompareMatWithValue(const cv::Scalar &expected, const cv::Mat &actual,
-                           cv::Point *firstMismatch = nullptr);
-
-/// Returns \c YES if the given \c actual matrix cells are all equal, up to the given \c range, to
-/// the given \c expected scalar.
-BOOL LTFuzzyCompareMatWithValue(const cv::Scalar &expected, const cv::Mat &actual,
-                                double range = 1, cv::Point *firstMismatch = nullptr);
-
-/// Returns a string representation of the \c mat cell value at the given \c position.
-NSString *LTMatValueAsString(const cv::Mat &mat, cv::Point position);
-
-/// Returns a string representation of the \c scalar value.
-NSString *LTScalarAsString(const cv::Scalar &scalar);
-
-/// Blends the given colors according to the given \c LTBlendMode. The \c premultiplied flag
-/// indicates whether the input is given with premultipled alpha or not, and the output will match.
-cv::Vec4b LTBlend(const cv::Vec4b &oldColor, const cv::Vec4b &newColor, bool premultiplied = YES,
-                  LTBlendMode mode = LTBlendModeNormal);
-
-/// Converts a \c CGRect to OpenCV's \c cv::Rect.
-cv::Rect LTCVRectWithCGRect(CGRect rect);
-
-/// Converts a \c cv::Vec4b to \c LTVector4.
-LTVector4 LTCVVec4bToLTVector4(cv::Vec4b value);
-
-/// Converts a \c cv::Vec4hf to \c LTVector4.
-LTVector4 LTCVVec4hfToLTVector4(cv::Vec4hf value);
-
-/// Converts a \c LTVector4 to \c cv::Vec4b.
-cv::Vec4b LTLTVector4ToVec4b(LTVector4 value);
-
-/// Rotates (clockwise) the given mat by the given angle (in radians) around its center.
-/// Uses nearest neighbor interpolation.
-cv::Mat LTRotateMat(const cv::Mat input, CGFloat angle);
-
-/// Returns a matrix of the given \c size containing delta at \c position.
-cv::Mat4b LTCreateDeltaMat(CGSize size, CGPoint position);
-
-/// Returns a matrix of the given \c size containing delta at the middle.
-cv::Mat4b LTCreateDeltaMat(CGSize size);
-
-/// Returns a new UIImage with the given size.
-UIImage *LTCreateUIImage(CGSize size);
-
-/// Loads an image to \c cv::Mat. The name of the image can differ between simulator and device.
-/// Loads from the bundle that contains the given class. Throws exception if the image cannot be
-/// found or loaded.
-cv::Mat LTLoadDeviceDependentMat(Class classInBundle, NSString *simulatorName,
-                                 NSString *deviceName);
-
-/// Returns the mean value of all elements in the given container.
-template <typename Container>
-double LTMean(const Container &container) {
-  return container.size() > 0 ?
-      std::accumulate(container.begin(), container.end(), 0.0) / (double)container.size() : 0.0;
-}
-
-/// Returns the variance of all elements in the given container.
-template <typename Container>
-double LTVariance(const Container &container) {
-  double mean = LTMean(container);
-  double squareSum = std::inner_product(container.begin(), container.end(), container.begin(), 0.0);
-  return squareSum / container.size() - mean * mean;
-}
 
 #define _LTTemporaryPath(...) \
   metamacro_if_eq(1, metamacro_argcount(__VA_ARGS__)) \
