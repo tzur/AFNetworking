3793328608cf8cb37ee92c59501fb92228d1d35a
Yaron Inger <yaron@lightricks.com>
2015-10-05 20:26:51 +0300

    LTTestModule: bind UIDevice and NSLocale.

--- LTEngine/LTEngineTests/LTTestModule.m
+++ LTEngine/LTEngineTests/LTTestModule.m
@@ -13,7 +13,10 @@ static const NSUInteger kTestingSeed = 1234;
   [super configure];
 
   [self bind:[UIApplication sharedApplication] toClass:[UIApplication class]];
+  [self bind:[UIDevice currentDevice] toClass:[UIDevice class]];
   [self bind:[UIScreen mainScreen] toClass:[UIScreen class]];
+  [self bind:[NSLocale currentLocale] toClass:[NSLocale class]];
+
   [self bind:nil toClass:[NSFileManager class]];
   [self bindBlock:^id(JSObjectionInjector __unused *context) {
     return [[LTRandom alloc] initWithSeed:kTestingSeed];
