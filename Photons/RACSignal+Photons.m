6177c8ab369c3bb7d04c71dff113d9594a57b63f
Barak Yoresh <barak@lightricks.com>
2015-11-29 18:19:09 +0200

    RacSignal+Photons: Fix ptn_replayLastLazily operator.

--- Photons/RACSignal+Photons.m
+++ Photons/RACSignal+Photons.m
@@ -10,12 +10,46 @@ NS_ASSUME_NONNULL_BEGIN
 - (instancetype)ptn_replayLastLazily {
   RACMulticastConnection *connection = [self multicast:[RACReplaySubject
                                                         replaySubjectWithCapacity:1]];
-  return [[RACSignal
-      defer:^{
-        [connection connect];
-        return connection.signal;
-      }]
-      setNameWithFormat:@"[%@] -ptn_replayLastLazily", self.name];
+  // A new signal is created here instead of defer since under specific race conditions a value
+  // can be missed and lost entirely when using defer.
+  //
+  // The defer operator creates and returns a signal that subscribes to the original signal in its
+  // subscribe block.
+  // \c RACReplaySubject holds an internal intermmediate subject used to pass values after
+  // subscription and to replay values upon late subscription. All the while itself being subscribed
+  // to the original signal.
+  //
+  // Since \c RACReplaySubject first connects (subscribes up to the original signal) and only
+  // afterwards subscribes subscribers to itself, quick on-subscription operation related values can
+  // be received by it before it adds subscribers to itself.
+  // In such a case the values won't be sent directly to its subscribers. Luckily the recording of
+  // values does occur, so subscribers receive the values by replay upon their subscription.
+  //
+  // The \c replayLazily operator has an infinite replay capacity, thus this phenomena has no vital
+  // effect - the subscribers receive the values, not knowing that they're actually echoes of the
+  // past.
+  //
+  // This operator on the other hand is vulnerable to such an effect. Receiving the replayed values
+  // instead of passed-through originals has no immediate consequences, but rather its limited
+  // capacity is the true culprit. If multiple values are passed in during that crucial purgatory
+  // time, when the \c replaySignal is subscribed above but yet to have subscribers of its own, it
+  // records only the last value received, discarding earlier values completely.
+  //
+  // To overcome this we create a new signal that first subscribes its own subscribers to itself and
+  // only afterwards subscribes itself the the underlying signal.
+  // Defer with \c RACMulticastConnection can actually achieve this with \c autoconnect but it
+  // permanently stops subscribing when it reaches 0 subscribers.
+  return [[RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+    RACCompoundDisposable *compoundDisposable = [RACCompoundDisposable compoundDisposable];
+
+    RACDisposable *signalDisposable = [connection.signal subscribe:subscriber];
+    [compoundDisposable addDisposable:signalDisposable];
+    
+    RACDisposable *connectionDisposable = [connection connect];
+    [compoundDisposable addDisposable:connectionDisposable];
+    
+    return compoundDisposable;
+  }] setNameWithFormat:@"[%@] -ptn_replayLastLazily", self.name];
 }
 
 @end
--- PhotonsTests/RACSignal+PhotonsSpec.m
+++ PhotonsTests/RACSignal+PhotonsSpec.m
@@ -5,25 +5,64 @@
 
 SpecBegin(RACSignal_Photons)
 
-__block RACSubject *subject;
-__block RACSignal *lastLazily;
+context(@"ptn_replayLastLazily", ^{
+  __block RACSubject *subject;
+  __block RACSignal *lastLazily;
+  
+  beforeEach(^{
+    subject = [RACSubject subject];
+    lastLazily = [[subject ptn_replayLastLazily] startCountingSubscriptions];
+  });
+  
+  it(@"should not subscribe to signal", ^{
+    expect(lastLazily).to.beSubscribedTo(0);
+  });
+  
+  it(@"should pass through all values sent after subscription", ^{
+    LLSignalTestRecorder *recorder = [lastLazily testRecorder];
+    
+    [subject sendNext:@1];
+    [subject sendNext:@2];
+    [subject sendNext:@3];
 
-beforeEach(^{
-  subject = [RACReplaySubject subject];
-  lastLazily = [[subject ptn_replayLastLazily] startCountingSubscriptions];
-});
+    expect(recorder.values).to.equal(@[@1, @2, @3]);
+  });
+  
+  it(@"should replay only the last value sent to late subscribers", ^{
+    [lastLazily subscribeNext:^(id __unused x) {}];
 
-it(@"should not subscribe to signal", ^{
-  expect(lastLazily).to.beSubscribedTo(0);
-});
+    [subject sendNext:@1];
+    [subject sendNext:@2];
+    [subject sendNext:@3];
+    LLSignalTestRecorder *recorder = [lastLazily testRecorder];
+    
+    expect(recorder.values).to.equal(@[@3]);
+  });
+  
+  it(@"should replay last value and pass through all future values sent to late subscribers", ^{
+    [lastLazily subscribeNext:^(id __unused x) {}];
 
-it(@"should replay a single value", ^{
-  [subject sendNext:@1];
-  [subject sendNext:@2];
-  [subject sendNext:@3];
 
-  expect(lastLazily).to.sendValues(@[@3]);
-  expect(lastLazily).to.beSubscribedTo(1);
+    [subject sendNext:@1];
+    [subject sendNext:@2];
+    LLSignalTestRecorder *recorder = [lastLazily testRecorder];
+    [subject sendNext:@3];
+    
+    expect(recorder.values).to.equal(@[@2, @3]);
+  });
+  
+  it(@"should continue to operate regularly after reaching zero subscribers", ^{
+    RACDisposable *earlySubscriber = [lastLazily subscribeNext:^(id __unused x) {}];
+    
+    [subject sendNext:@1];
+    [subject sendNext:@2];
+    [subject sendNext:@3];
+    
+    expect(lastLazily).to.beSubscribedTo(1);
+    [earlySubscriber dispose];
+    LLSignalTestRecorder *recorder = [lastLazily testRecorder];
+    expect(recorder.values).to.equal(@[@3]);
+  });
 });
 
 SpecEnd
