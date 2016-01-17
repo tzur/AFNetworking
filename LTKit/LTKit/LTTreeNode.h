56377fed434f1d839e655029500ecf13cd8ec964
Rouven Strauss <rouven@lightricks.com>
2016-01-17 08:27:13 +0200

    LTTreeNode & LTMutableTreeNode: Initial commit.
    This commit adds the LTTreeNode and LTMutableTreeNode classes for
    (im)mutable value objects constituting the nodes of a tree.

---
+++ LTKit/LTKit/LTTreeNode.h
@@ -0,0 +1,96 @@
+// Copyright (c) 2016 Lightricks. All rights reserved.
+// Created by Rouven Strauss.
+
+NS_ASSUME_NONNULL_BEGIN
+
+@class LTTreeNode;
+
+/// Different orders by which tree nodes can be traversed.
+typedef NS_ENUM(NSUInteger) {
+  LTTreeTraversalOrderPreOrder,
+  LTTreeTraversalOrderPostOrder
+} LTTreeTraversalOrder;
+
+/// Object that can be held by an \c LTTreeNode.
+typedef id<NSCopying, NSObject> LTTreeNodeObject;
+
+/// Block used for traversal of tree nodes.
+typedef void (^LTTreeTraversalBlock)(LTTreeNode *node, BOOL * stop);
+
+/// Immutable value object constituting the node of a tree. The node wraps a given
+/// \c id<NSCopying, NSObject> object and maintains immutable references to its children vertices.
+///
+/// @important if objects of this class are abused to create a graph with cycles, any functionality
+/// described by the API is undefined.
+@interface LTTreeNode<ObjectType : LTTreeNodeObject> : NSObject <NSCopying, NSMutableCopying>
+
+- (instancetype)init NS_UNAVAILABLE;
+
+/// Initializes with the given \c object and \c children.
+- (instancetype)initWithObject:(ObjectType)object
+                      children:(NSArray<LTTreeNode<ObjectType> *> *)children;
+
+/// Executes the given \c block for each node of the tree, in the given \c traversalOrder. If
+/// \c stop is set to \c YES, the execution of the \c block terminates immediately.
+///
+/// @important the given \c stop argument is an out-only argument. It must only be modified within
+/// the given \c block.
+///
+/// Time complexity: \c O(n), where \c is the number of vertices in this instance.
+- (void)enumerateObjectsWithTraversalOrder:(LTTreeTraversalOrder)traversalOrder
+                                usingBlock:(LTTreeTraversalBlock)block;
+
+/// Returns \c YES if the given \c node is of class \c LTTreeNode with an equal \c object and equal
+/// \c children.
+///
+/// @important calling this method might be expensive if the number of \c children is large.
+///
+/// Time complexity: \c O(n * m), where \c n equals the number of descendants of the receiver and
+/// \c m equals the time complexity of testing equality of the \c object objects of the two involved
+/// vertices.
+- (BOOL)isEqual:(id)node;
+
+/// Returns a number that can be used as a table address in a hash table structure.
+///
+/// @important calling this method might be expensive if the number of \c children is large.
+///
+/// Time complexity: \c O(n * m), where \c n equals the number of descendants of the receiver and
+/// \c m equals the time complexity of retrieving the hash value of the held \c object.
+- (NSUInteger)hash;
+
+/// Returns an immutable deep copy of the receiver. More explicitly, returns an
+/// \c LTTreeNode<ObjectType> object which is a recursive immutable copy of the receiver.
+///
+/// Time complexity: \c O(n), where \c n is the number of descendants of this node.
+- (LTTreeNode<ObjectType> *)deepCopy;
+
+/// Returns a \c LTMutableTreeNode<ObjectType> object that is a copy of the receiver.
+- (id)mutableCopyWithZone:(nullable NSZone *)zone;
+
+/// Returns a mutable deep copy of the receiver. More explicitly, returns an
+/// \c LTTreeNode<ObjectType> object which is a recursive mutable copy of the receiver.
+///
+/// Time complexity: \c O(n), where \c n is the number of descendants of this node.
+- (id)mutableDeepCopy;
+
+/// Object wrapped by this node.
+@property (readonly, nonatomic) ObjectType object;
+
+/// Ordered collection of children of this node.
+@property (readonly, nonatomic) NSArray<LTTreeNode<ObjectType> *> *children;
+
+@end
+
+@interface LTMutableTreeNode<ObjectType : LTTreeNodeObject> : LTTreeNode
+
+- (instancetype)init NS_UNAVAILABLE;
+
+/// Mutable object wrapped by this node.
+@property (strong, nonatomic) ObjectType object;
+
+/// Mutable ordered collection of children of this node.
+@property (readonly, nonatomic) NSMutableArray<LTTreeNode<ObjectType> *> *children;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ LTKit/LTKit/LTTreeNode.mm
@@ -0,0 +1,186 @@
+// Copyright (c) 2016 Lightricks. All rights reserved.
+// Created by Rouven Strauss.
+
+#import "LTTreeNode.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LTTreeNode ()
+
+/// Object wrapped by this instance.
+@property (strong, readwrite, nonatomic) LTTreeNodeObject object;
+
+/// Ordered collection of children of this instance.
+@property (strong, readwrite, nonatomic) NSArray<LTTreeNode<LTTreeNodeObject> *> *children;
+
+@end
+
+@implementation LTTreeNode
+
+#pragma mark -
+#pragma mark Initialization
+#pragma mark -
+
+- (instancetype)initWithObject:(LTTreeNodeObject)object
+                      children:(NSArray<LTTreeNode<LTTreeNodeObject> *> *)children {
+  if (self = [super init]) {
+    self.object = [object copyWithZone:nil];
+    self.children = [children copy];
+  }
+  return self;
+}
+
+#pragma mark -
+#pragma mark Public Interface
+#pragma mark -
+
+- (void)enumerateObjectsWithTraversalOrder:(LTTreeTraversalOrder)traversalOrder
+                                usingBlock:(LTTreeTraversalBlock)block {
+  __block BOOL stop = NO;
+  [self enumerateObjectsWithTraversalOrder:traversalOrder usingBlock:block stop:&stop node:self];
+}
+
+- (void)enumerateObjectsWithTraversalOrder:(LTTreeTraversalOrder)traversalOrder
+                                usingBlock:(LTTreeTraversalBlock)block
+                                      stop:(BOOL *)stop node:(nullable LTTreeNode *)node {
+  if (!node) {
+    return;
+  }
+
+  if (traversalOrder == LTTreeTraversalOrderPreOrder) {
+    block(node, stop);
+    if (*stop) {
+      return;
+    }
+  }
+
+  for (LTTreeNode *child in node.children) {
+    [self enumerateObjectsWithTraversalOrder:traversalOrder usingBlock:block stop:stop node:child];
+    if (*stop) {
+      return;
+    }
+  }
+
+  if (traversalOrder == LTTreeTraversalOrderPostOrder) {
+    // No need to check the \c stop variable here since it has been done as last command in the
+    // previous loop.
+    block(node, stop);
+  }
+}
+
+#pragma mark -
+#pragma mark NSObject
+#pragma mark -
+
+- (BOOL)isEqual:(LTTreeNode *)node {
+  if (node == self) {
+    return YES;
+  }
+
+  if (![node isKindOfClass:[LTTreeNode class]]) {
+    return NO;
+  }
+
+  return [self.object isEqual:node.object] && [node.children isEqualToArray:self.children];
+}
+
+- (NSUInteger)hash {
+  NSUInteger hash = self.object.hash;
+
+  for (LTTreeNode *child in self.children) {
+    hash ^= child.hash;
+  }
+
+  return hash;
+}
+
+- (NSString *)description {
+  return [NSString stringWithFormat:@"<%@: %p, object: %@, children count: %lu>",
+          self.class, self, self.object, (unsigned long)self.children.count];
+}
+
+#pragma mark -
+#pragma mark NSCopying
+#pragma mark -
+
+- (id)copyWithZone:(nullable NSZone __unused *)zone {
+  return self;
+}
+
+#pragma mark -
+#pragma mark NSMutableCopying
+#pragma mark -
+
+- (LTMutableTreeNode<LTTreeNodeObject> *)mutableCopyWithZone:(nullable NSZone __unused *)zone {
+  return [[LTMutableTreeNode alloc] initWithObject:self.object
+                                            children:[self.children mutableCopy]];
+}
+
+#pragma mark -
+#pragma mark Deep Copying
+#pragma mark -
+
+- (LTTreeNode<LTTreeNodeObject> *)deepCopy {
+  NSMutableArray *children =
+      self.children ? [NSMutableArray arrayWithCapacity:self.children.count] : nil;
+
+  BOOL createdNewNodeInstance = NO;
+
+  for (LTTreeNode *child in self.children) {
+    LTTreeNode *copyOfChild = [child deepCopy];
+    [children addObject:copyOfChild];
+    createdNewNodeInstance |= (copyOfChild != child);
+  }
+
+  if (!createdNewNodeInstance) {
+    return [self copy];
+  }
+
+  return [[LTTreeNode alloc] initWithObject:self.object children:[children copy]];
+}
+
+#pragma mark -
+#pragma mark Mutable Deep Copying
+#pragma mark -
+
+- (LTMutableTreeNode<LTTreeNodeObject> *)mutableDeepCopy {
+  NSMutableArray *children =
+      self.children ? [NSMutableArray arrayWithCapacity:self.children.count] : nil;
+
+  for (LTTreeNode *child in self.children) {
+    [children addObject:[child mutableDeepCopy]];
+  }
+
+  return [[LTMutableTreeNode alloc] initWithObject:self.object children:[children copy]];
+}
+
+@end
+
+@implementation LTMutableTreeNode
+
+@dynamic object;
+@dynamic children;
+
+#pragma mark -
+#pragma mark Initialization
+#pragma mark -
+
+- (instancetype)initWithObject:(LTTreeNodeObject)object
+                      children:(NSArray<LTTreeNode<LTTreeNodeObject> *> *)children {
+  if (self = [super initWithObject:object children:@[]]) {
+    self.children = [children mutableCopy];
+  }
+  return self;
+}
+
+#pragma mark -
+#pragma mark NSCopying
+#pragma mark -
+
+- (id)copyWithZone:(nullable NSZone __unused *)zone {
+  return [[LTTreeNode alloc] initWithObject:self.object children:[self.children copy]];
+}
+
+@end
+
+NS_ASSUME_NONNULL_END
