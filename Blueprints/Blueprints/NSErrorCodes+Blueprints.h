b674b29aa9b48b71c231e244c75e55c5a5f95140
Yaron Inger <yaron@lightricks.com>
2016-01-19 17:16:51 +0200

    NSErrorCodes+Blueprints: initial commit.

---
+++ Blueprints/Blueprints/NSErrorCodes+Blueprints.h
@@ -0,0 +1,20 @@
+// Copyright (c) 2016 Lightricks. All rights reserved.
+// Created by Yaron Inger.
+
+#import <LTKit/NSErrorCodes+LTKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+/// Product ID.
+NS_ENUM(NSInteger) {
+  /// Product ID of Blueprints.
+  BlueprintsErrorCodeProductID = 4
+};
+
+/// All error codes available in Blueprints.
+LTErrorCodesDeclare(BlueprintsErrorCodeProductID,
+  /// Caused when a node has not been found.
+  BLUErrorCodeNodeNotFound
+);
+
+NS_ASSUME_NONNULL_END
---
+++ Blueprints/Blueprints/NSErrorCodes+Blueprints.mm
@@ -0,0 +1,14 @@
+// Copyright (c) 2016 Lightricks. All rights reserved.
+// Created by Yaron Inger.
+
+#import "NSErrorCodes+Blueprints.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+/// All error codes available in Blueprints.
+LTErrorCodesImplement(BlueprintsErrorCodeProductID,
+  /// Caused when a node has not been found.
+  BLUErrorCodeNodeNotFound
+);
+
+NS_ASSUME_NONNULL_END
