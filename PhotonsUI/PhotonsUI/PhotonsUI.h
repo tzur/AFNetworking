f82ac498c9fda084dfe0629a8e98e00c4ad528e1
Barak Yoresh <barak@lightricks.com>
2016-02-01 11:34:04 +0200

    PhotonsUI: Initial commit.

---
+++ PhotonsUI/PhotonsUI/PhotonsUI.h
@@ -0,0 +1,8 @@
+// Copyright (c) 2015 Lightricks. All rights reserved.
+// Created by Barak Yoresh.
+
+/// Project version number for PhotonsUI.
+FOUNDATION_EXPORT double PhotonsUIVersionNumber;
+
+/// Project version string for PhotonsUI.
+FOUNDATION_EXPORT const unsigned char PhotonsUIVersionString[];
