0690296d9062abb4e03f4d78949a173d1e64e999
Yaron Inger <yaron@lightricks.com>
2016-04-13 10:55:51 +0300

    UIColor+Utilities: port from Enlight.

---
+++ Wireframes/Wireframes/UIColor+Utilities.h
@@ -0,0 +1,24 @@
+// Copyright (c) 2016 Lightricks. All rights reserved.
+// Created by Yaron Inger.
+
+NS_ASSUME_NONNULL_BEGIN
+
+/// Various utilities for \c UIColor conversion and creation.
+@interface UIColor (Utilities)
+
+/// Returns an instance of \c UIColor associated with the given hex string. The string can be in one
+/// of the following formats, where the hash sign is optional: {#RGB, #ARGB, #RRGGBB, #AARRGGBB}.
+/// If the format is invalid, an exception will be thrown.
++ (UIColor *)wf_colorWithHex:(NSString *)hex;
+
+/// Returns the hex string associated with the color, in #AARRGGBB format.
+- (NSString *)wf_hexString;
+
+/// Returns a linearly interpolated color between \c start and \c end colors, controlled by the
+/// parameter \c t, which will be clamped to \c [0, 1]. The \c start and \c end colors must be
+/// created with RGB colorspace.
++ (UIColor *)wf_lerpColorFrom:(UIColor *)start to:(UIColor *)end parameter:(CGFloat)t;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Wireframes/Wireframes/UIColor+Utilities.mm
@@ -0,0 +1,105 @@
+// Copyright (c) 2016 Lightricks. All rights reserved.
+// Created by Yaron Inger.
+
+#import "UIColor+Utilities.h"
+
+#import <inttypes.h>
+#import <LTKit/LTCGExtensions.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@implementation UIColor (Utilities)
+
++ (UIColor *)wf_colorWithHex:(NSString *)hex {
+  NSString *colorString = [hex characterAtIndex:0] == '#' ? [hex substringFromIndex:1] : hex;
+
+  CGFloat alpha, red, blue, green;
+  switch (colorString.length) {
+    case 3: // #RGB.
+      alpha = 1.0f;
+      red   = [self wf_colorComponentFrom:colorString start:0 length:1];
+      green = [self wf_colorComponentFrom:colorString start:1 length:1];
+      blue  = [self wf_colorComponentFrom:colorString start:2 length:1];
+      break;
+    case 4: // #ARGB.
+      alpha = [self wf_colorComponentFrom:colorString start:0 length:1];
+      red   = [self wf_colorComponentFrom:colorString start:1 length:1];
+      green = [self wf_colorComponentFrom:colorString start:2 length:1];
+      blue  = [self wf_colorComponentFrom:colorString start:3 length:1];
+      break;
+    case 6: // #RRGGBB.
+      alpha = 1.0f;
+      red   = [self wf_colorComponentFrom:colorString start:0 length:2];
+      green = [self wf_colorComponentFrom:colorString start:2 length:2];
+      blue  = [self wf_colorComponentFrom:colorString start:4 length:2];
+      break;
+    case 8: // #AARRGGBB.
+      alpha = [self wf_colorComponentFrom:colorString start:0 length:2];
+      red   = [self wf_colorComponentFrom:colorString start:2 length:2];
+      green = [self wf_colorComponentFrom:colorString start:4 length:2];
+      blue  = [self wf_colorComponentFrom:colorString start:6 length:2];
+      break;
+    default:
+      LTParameterAssert(NO, @"Given color value '%@' is invalid. It should be a hex value in one "
+                        "of the forms: #RBG, #ARGB, #RRGGBB, or #AARRGGBB", hex);
+      break;
+  }
+
+  return [UIColor colorWithRed:red green:green blue:blue alpha:alpha];
+}
+
++ (CGFloat)wf_colorComponentFrom:(NSString *)string start:(NSUInteger)start
+                          length:(NSUInteger)length {
+  NSString *substring = [string substringWithRange:NSMakeRange(start, length)];
+  NSString *fullHex = length == 2 ? substring :
+      [NSString stringWithFormat:@"%@%@", substring, substring];
+
+  unsigned int hexComponent;
+  if (![[NSScanner scannerWithString:fullHex] scanHexInt:&hexComponent]) {
+    LTParameterAssert(NO, @"Invalid hex digit given: %@", substring);
+  }
+  return hexComponent / 255.0;
+}
+
+- (NSString *)wf_hexString {
+  static const uint8_t kMax = std::numeric_limits<uint8_t>::max();
+
+  CGFloat r, g, b, a;
+  uint8_t r8, g8, b8, a8;
+
+  if ([self getRed:&r green:&g blue:&b alpha:&a]) {
+    r8 = std::round(r * kMax);
+    g8 = std::round(g * kMax);
+    b8 = std::round(b * kMax);
+    a8 = std::round(a * kMax);
+  } else if ([self getWhite:&r alpha:&a]) {
+    r8 = g8 = b8 = std::round(r * kMax);
+    a8 = std::round(a * kMax);
+  } else {
+    LTAssert(NO, @"Invalid color for conversion: %@", self);
+  }
+
+  return [NSString stringWithFormat:@"#%02" PRIX8 "%02" PRIX8 "%02" PRIX8 "%02" PRIX8,
+          a8, r8, g8, b8];
+}
+
++ (UIColor *)wf_lerpColorFrom:(UIColor *)start to:(UIColor *)end parameter:(CGFloat)t {
+  const CGFloat *startComponent = CGColorGetComponents(start.CGColor);
+  const CGFloat *endComponent = CGColorGetComponents(end.CGColor);
+
+  CGFloat startAlpha = CGColorGetAlpha(start.CGColor);
+  CGFloat endAlpha = CGColorGetAlpha(end.CGColor);
+
+  t = std::clamp(t, 0, 1);
+  
+  CGFloat r = startComponent[0] + (endComponent[0] - startComponent[0]) * t;
+  CGFloat g = startComponent[1] + (endComponent[1] - startComponent[1]) * t;
+  CGFloat b = startComponent[2] + (endComponent[2] - startComponent[2]) * t;
+  CGFloat a = startAlpha + (endAlpha - startAlpha) * t;
+
+  return [UIColor colorWithRed:r green:g blue:b alpha:a];
+}
+
+@end
+
+NS_ASSUME_NONNULL_END
