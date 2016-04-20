aa27b5128db7baa8f0c9b8839de808524c2fad63
Barak Yoresh <barak@lightricks.com>
2016-04-20 16:28:34 +0300

    PTNAlbum: Add generic implementation.

--- Photons/Photons/PTNAlbum.h
+++ Photons/Photons/PTNAlbum.h
@@ -3,7 +3,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@protocol PTNCollection;
+@protocol PTNAlbumDescriptor, PTNAssetDescriptor, PTNCollection;
 
 /// Represents a collection of assets and subalbums. Classes implementing this protocol are advised
 /// to be immutable value objects.
@@ -19,6 +19,19 @@ NS_ASSUME_NONNULL_BEGIN
 /// Sub albums contained in this album implementing the \c PTNAlbumDescriptor protocol. If there are
 /// no subalbums, an empty collection will be returned.
 @property (readonly, nonatomic) id<PTNCollection> subalbums;
+
+@end
+
+/// Implementation of \c PTNAlbum backed by an \c NSURL, an \c NSArray of \c PTNAlbumDescriptor
+/// objects and an \c NSArray of \c PTNAssetDescriptor objects.
+@interface PTNAlbum : NSObject <PTNAlbum>
+
+- (instancetype)init NS_UNAVAILABLE;
+
+/// Initializes with \c url, subalbums and assets.
+- (instancetype)initWithURL:(NSURL *)url
+                  subalbums:(NSArray<id<PTNAlbumDescriptor>> *)subalbums
+                     assets:(NSArray<id<PTNAssetDescriptor>> *)assets NS_DESIGNATED_INITIALIZER;
 
 @end
 
---
+++ Photons/PhotonsTests/PTNAlbumSpec.mm
@@ -0,0 +1,53 @@
+// Copyright (c) 2016 Lightricks. All rights reserved.
+// Created by Barak Yoresh.
+
+#import "PTNAlbum.h"
+
+#import "PTNDescriptor.h"
+
+SpecBegin(PTNAlbum)
+
+__block NSURL *url;
+__block NSArray *subalbums;
+__block NSArray *assets;
+
+beforeEach(^{
+  url = [NSURL URLWithString:@"http://www.foo.com"];
+  subalbums = @[
+    OCMProtocolMock(@protocol(PTNAlbumDescriptor)),
+    OCMProtocolMock(@protocol(PTNAlbumDescriptor))
+  ];
+  assets = @[
+    OCMProtocolMock(@protocol(PTNAssetDescriptor)),
+    OCMProtocolMock(@protocol(PTNAssetDescriptor))
+  ];
+});
+
+it(@"should create album with url, subalbums and assets", ^{
+  PTNAlbum *album = [[PTNAlbum alloc] initWithURL:url subalbums:subalbums assets:assets];
+
+  expect(album.url).to.equal(url);
+  expect(album.subalbums).to.equal(subalbums);
+  expect(album.assets).to.equal(assets);
+});
+
+context(@"equality", ^{
+  __block PTNAlbum *firstAlbum;
+  __block PTNAlbum *secondAlbum;
+
+  beforeEach(^{
+    firstAlbum = [[PTNAlbum alloc] initWithURL:url subalbums:subalbums assets:assets];
+    secondAlbum = [[PTNAlbum alloc] initWithURL:url subalbums:subalbums assets:assets];
+  });
+
+  it(@"should handle isEqual correctly", ^{
+    expect(firstAlbum).to.equal(secondAlbum);
+    expect(secondAlbum).to.equal(firstAlbum);
+  });
+
+  it(@"should create proper hash", ^{
+    expect(firstAlbum.hash).to.equal(secondAlbum.hash);
+  });
+});
+
+SpecEnd
