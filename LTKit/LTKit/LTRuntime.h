82c6cdc77b47d23a4ec7cca64f7f4a159fd42d26
Yaron Inger <yaron@lightricks.com>
2016-05-04 17:48:16 +0300

    LTRuntime: add LTIsRunningTests.

---
+++ LTKit/LTKit/LTRuntime.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2016 Lightricks. All rights reserved.
+// Created by Yaron Inger.
+
+NS_ASSUME_NONNULL_BEGIN
+
+/// Returns \c YES if tests are currently running or planned to be injected in the current execution
+/// of the app.
+BOOL LTIsRunningTests();
+
+NS_ASSUME_NONNULL_END
