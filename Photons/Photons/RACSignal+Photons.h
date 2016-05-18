3216fea6722df7df6afd2ff7ca8ebf58c0cf2566
Barak Yoresh <barak@lightricks.com>
2016-05-18 15:57:38 +0300

    RACSignal+Photons: Implement ptn_combineLatestWithIndex.

--- Photons/Photons/RACSignal+Photons.h
+++ Photons/Photons/RACSignal+Photons.h
@@ -23,6 +23,22 @@ NS_ASSUME_NONNULL_BEGIN
 /// \c NSUnderlyingErrorKey key.
 - (instancetype)ptn_wrapErrorWithError:(NSError *)error;
 
+/// Combines the latest values from each of the given \c signals by sending the latest value from
+/// each signal accompanied with the index of the signal that caused a new value to be sent over the
+/// receiver. For the returned signal to send an initial value, all the \c signals must send at least one
+/// value. The initially sent value will send a \c nil index.
+///
+/// If \c signals is empty, the returned signal will immediately complete upon subscription.
+///
+/// Returns a signal in the format <tt>((v_0, v_1, ..., v_n), index)</tt>, where
+/// <tt>{v_0, v_1, ..., v_n}</tt> are the latest values from the given \c signals and \c index is
+/// the index in the range <tt>{0, 1, ..., n}</tt> of the latest signal that caused a new value to
+/// be sent over the receiver. The returned signal forwards any \c error events, and completes when
+/// all input signals complete.
+///
+/// @return RACSignal<RACTuple *>.
++ (RACSignal *)ptn_combineLatestWithIndex:(id<NSFastEnumeration>)signals;
+
 @end
 
 NS_ASSUME_NONNULL_END
