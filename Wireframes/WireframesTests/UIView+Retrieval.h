148c9e5a8b8b6b57afe53ef24b29e61c9f5baab0
Yaron Inger <yaron@lightricks.com>
2016-05-18 16:50:01 +0300

    UIView+Retrieval: initial commit.

---
+++ Wireframes/WireframesTests/UIView+Retrieval.h
@@ -0,0 +1,23 @@
+// Copyright (c) 2016 Lightricks. All rights reserved.
+// Created by Yaron Inger.
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIView (Retrieval)
+
+/// Returns the first view that has an \c accessibilityIdentifier equal to the given one. The search
+/// order is DFS, so the following hierarchy:
+///
+/// + receiver
+/// |-- a
+/// |   \-- c
+/// |-- b
+/// |   \-- c
+/// |-- c
+///
+/// Will return the view \c a.c for an accessibility identifier of \c c.
+- (nullable instancetype)wf_viewForAccessibilityIdentifier:(NSString *)accessibilityIdentifier;
+
+@end
+
+NS_ASSUME_NONNULL_END
