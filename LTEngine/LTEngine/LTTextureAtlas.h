7edc582b2b9efe52734f5fa399b25d8a6e3c29ef
Ofir Gluzman <ogluzman@lightricks.com>
2016-05-22 19:11:31 +0300

    LTTextureAtlas: initial commit.

---
+++ LTEngine/LTEngine/LTTextureAtlas.h
@@ -0,0 +1,30 @@
+// Copyright (c) 2016 Lightricks. All rights reserved.
+// Created by Ofir Gluzman.
+
+NS_ASSUME_NONNULL_BEGIN
+
+@class LTTexture;
+
+/// Class for describing a collection of images packed in a single texture, the atlas.
+@interface LTTextureAtlas : NSObject
+
+- (instancetype)init NS_UNAVAILABLE;
+
+/// Initializes the class with a given atlas \c texture and \c areas. \c areas is a non empty
+/// dictionary mapping identifying strings to their corresponding boxed \c CGRects. Each rect
+/// describes the area of its unpacked image upon \c texture. All rects must be contained inside the
+/// \c texture size rect and should have positive widths and heights.
+- (instancetype)initWithAtlasTexture:(LTTexture *)texture
+                          imageAreas:(NSDictionary<NSString *, NSValue *> *)areas
+    NS_DESIGNATED_INITIALIZER;
+
+/// Packing texture that is composed from a collection of images upon it.
+@property (readonly, nonatomic) LTTexture *texture;
+
+/// Dictionary mapping identifying strings to their corresponding boxed \c CGRects. Each rect 
+/// describes the area of its unpacked image upon the \c texture.
+@property (readonly, nonatomic) NSDictionary<NSString *, NSValue *> *areas;
+
+@end
+
+NS_ASSUME_NONNULL_END
