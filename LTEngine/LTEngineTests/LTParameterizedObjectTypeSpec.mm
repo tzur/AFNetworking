dc7023206684b58e50c38e143bb530e3384cc962
Rouven Strauss <rouven@lightricks.com>
2016-05-23 17:24:58 +0300

    LTParameterizedObjectType: Initial commit.
    This commit adds LTParameterizedObjectType, a value class representing
    the type of certain basic parameterized objects.

---
+++ LTEngine/LTEngineTests/LTParameterizedObjectTypeSpec.mm
@@ -0,0 +1,22 @@
+// Copyright (c) 2016 Lightricks. All rights reserved.
+// Created by Rouven Strauss.
+
+#import "LTParameterizedObjectType.h"
+
+#import "LTBasicParameterizedObjectFactories.h"
+
+SpecBegin(LTParameterizedObjectType)
+
+it(@"should return a factory of the correct class for each type", ^{
+  NSDictionary<LTParameterizedObjectType *, Class> *typeToClass = @{
+    $(LTParameterizedObjectTypeDegenerate): [LTBasicDegenerateInterpolantFactory class],
+    $(LTParameterizedObjectTypeLinear): [LTBasicLinearInterpolantFactory class],
+    $(LTParameterizedObjectTypeCatmullRom): [LTBasicCatmullRomInterpolantFactory class]
+  };
+
+  [LTParameterizedObjectType enumerateEnumUsingBlock:^(LTParameterizedObjectType *value) {
+    expect([[value factory] class]).to.equal(typeToClass[value]);
+  }];
+});
+
+SpecEnd
