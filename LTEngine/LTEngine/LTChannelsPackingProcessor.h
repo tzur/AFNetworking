1d2729adcc4fd109804672331d92dac15a2d44cb
Ofir Gluzman <ogluzman@lightricks.com>
2016-05-30 10:39:13 +0300

    LTChannelsPackingProcessor: initial commit.

---
+++ LTEngine/LTEngine/LTChannelsPackingProcessor.h
@@ -0,0 +1,45 @@
+// Copyright (c) 2016 Lightricks. All rights reserved.
+// Created by Ofir Gluzman.
+
+#import "LTOneShotImageProcessor.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@class LTTexture;
+
+/// Processor for packing several single cahnnel textures into a single RGBA texture.
+/// If there are less than 4 input textures, the processor fills the rest of the channels in the
+/// output texture with zeros.
+@interface LTChannelsPackingProcessor : LTOneShotImageProcessor
+
+- (instancetype)init NS_UNAVAILABLE;
+
+- (instancetype)initWithDrawer:(id<LTTextureDrawer>)drawer
+                      strategy:(id<LTProcessingStrategy>)strategy
+          andAuxiliaryTextures:(NSDictionary *)auxiliaryTextures NS_UNAVAILABLE;
+
+- (instancetype)initWithVertexSource:(NSString *)vertexSource
+                      fragmentSource:(NSString *)fragmentSource input:(LTTexture *)input
+                           andOutput:(LTTexture *)output NS_UNAVAILABLE;
+
+- (instancetype)initWithDrawer:(id<LTTextureDrawer>)drawer
+                 sourceTexture:(LTTexture *)sourceTexture
+             auxiliaryTextures:(NSDictionary *)auxiliaryTextures
+                     andOutput:(LTTexture *)output NS_UNAVAILABLE;
+
+- (instancetype)initWithVertexSource:(NSString *)vertexSource
+                      fragmentSource:(NSString *)fragmentSource
+                       sourceTexture:(LTTexture *)sourceTexture
+                   auxiliaryTextures:(NSDictionary *)auxiliaryTextures
+                           andOutput:(LTTexture *)output NS_UNAVAILABLE;
+
+/// Initializes the processor with given \c inputs textures and an \c output texture. \c inputs must
+/// contain between 1 to 4 single channel textures of the same size and the same pixel format. \c
+/// output texture must be an RGBA texture with equal size and \c bitDepth to the input textures
+/// ones.
+- (instancetype)initWithInputs:(NSArray<LTTexture *> *)inputs output:(LTTexture *)output
+    NS_DESIGNATED_INITIALIZER;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ LTEngine/LTEngine/LTChannelsPackingProcessor.mm
@@ -0,0 +1,85 @@
+// Copyright (c) 2016 Lightricks. All rights reserved.
+// Created by Ofir Gluzman.
+
+#import "LTChannelsPackingProcessor.h"
+
+#import "LTGPUImageProcessor+Protected.h"
+#import "LTShaderStorage+LTChannelsPackingFsh.h"
+#import "LTShaderStorage+LTPassthroughShaderVsh.h"
+#import "LTTexture.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LTChannelsPackingProcessor ()
+
+/// Array of input single channel textures to pack.
+@property (readonly, nonatomic) NSArray<LTTexture *> *inputs;
+
+@end
+
+@implementation LTChannelsPackingProcessor
+
+- (instancetype)initWithInputs:(NSArray<LTTexture *> *)inputs output:(LTTexture *)output {
+  LTParameterAssert(inputs.count > 0 && inputs.count <= 4, @"input textures count must be between "
+                    "1 to 4 but input count is %lu", (unsigned long)inputs.count);
+  [LTChannelsPackingProcessor validateSizeOfInputTextures:inputs outputTexture:output];
+  [LTChannelsPackingProcessor validatePixelFormatOfInputTextures:inputs outputTexture:output];
+
+  if (self = [super initWithVertexSource:[LTPassthroughShaderVsh source]
+                          fragmentSource:[LTChannelsPackingFsh source]
+                                   input:inputs[0] andOutput:output]) {
+    _inputs = inputs;
+
+    self[[LTChannelsPackingFsh inputTexturesCount]] = @(inputs.count);
+    [self setAdditionalInputsInShader];
+  }
+
+  return self;
+}
+
++ (void)validateSizeOfInputTextures:(NSArray<LTTexture *> *)inputs
+                      outputTexture:(LTTexture *)output {
+  for (NSUInteger i = 0; i < inputs.count; ++i) {
+    LTParameterAssert(inputs[i].size == output.size, @"input textures and output texture must have "
+                      "the same size but input texture at index %lu is of size %@ and output "
+                      "texture is of size %@", (unsigned long)i, NSStringFromCGSize(inputs[i].size),
+                      NSStringFromCGSize(output.size));
+  }
+}
+
++ (void)validatePixelFormatOfInputTextures:(NSArray<LTTexture *> *)inputs
+                             outputTexture:(LTTexture *)output {
+  LTParameterAssert(output.components == LTGLPixelComponentsRGBA, @"output texture must have 4 "
+                    "channels but its pixel format is %@", output.pixelFormat);
+  for (NSUInteger i = 0; i < inputs.count; ++i) {
+    LTParameterAssert(inputs[i].components == LTGLPixelComponentsR, @"input textures must have "
+                      "only 1 channel but input texture pixel format at index %lu is %@",
+                      (unsigned long)i, inputs[i].pixelFormat);
+    LTParameterAssert(inputs[i].bitDepth == output.bitDepth, @"input textures and output texture "
+                      "must have the same bit depth but input texture pixel format at index %lu is "
+                      "%@ and output texture pixel format is %@",
+                      (unsigned long)i, inputs[i].pixelFormat, output.pixelFormat);
+  }
+}
+
+- (void)setAdditionalInputsInShader {
+  if (self.inputs.count == 1) {
+    return;
+  }
+
+  [self setAuxiliaryTexture:self.inputs[1] withName:[LTChannelsPackingFsh secondTexture]];
+  if (self.inputs.count == 2) {
+    return;
+  }
+
+  [self setAuxiliaryTexture:self.inputs[2] withName:[LTChannelsPackingFsh thirdTexture]];
+  if (self.inputs.count == 3) {
+    return;
+  }
+
+  [self setAuxiliaryTexture:self.inputs[3] withName:[LTChannelsPackingFsh fourthTexture]];
+}
+
+@end
+
+NS_ASSUME_NONNULL_END
