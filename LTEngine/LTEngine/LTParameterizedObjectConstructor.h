a0ff73a006941da1e30b643cdf877abeae0782f3
Rouven Strauss <rouven@lightricks.com>
2016-06-05 14:02:38 +0300

    LTParameterizedObjectConstructor: Initial commit.
    This commit adds LTParameterizedObjectConstructor, a mutable object
    responsible for constructing and iteratively extending an
    id<LTParameterizedObject> using a given factory of primitive
    parameterized objects and given Euclidean spline control points.

---
+++ LTEngine/LTEngine/LTParameterizedObjectConstructor.h
@@ -0,0 +1,50 @@
+// Copyright (c) 2016 Lightricks. All rights reserved.
+// Created by Rouven Strauss.
+
+#import "LTParameterizedObject.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@class LTControlPointModel, LTEuclideanSplineControlPoint;
+
+@protocol LTPrimitiveParameterizedObjectFactory;
+
+/// Mutable object responsible for constructing and iteratively extending an
+/// \c LTParameterizedObject using a given \c LTControlPointModel, specifying the type of the
+/// factory for basic parameterized objects and a possibly existing ordered collection of initial
+/// Euclidean spline control points. The object has two states: in its initial state the object does
+/// not provide any parameterized object. In its second state, reachable through the initial state,
+/// it does provide a parameterized object. The transition occurs when the total number of control
+/// points iteratively provided to the object reaches the \c numberOfRequiredValues of the
+/// aforementioned factory. Once a parameterized object has been constructed, additional incoming
+/// control points are used to extend it according to aforementioned factory. Changes of the
+/// parameterized object of an instance are guaranteed to occur exclusively and immediately as a
+/// result of a) its initialization and b) calls to the \c pushControlPoints: method.
+@interface LTParameterizedObjectConstructor : NSObject
+
+- (instancetype)init NS_UNAVAILABLE;
+
+/// Initializes with the given \c model.
+- (instancetype)initWithControlPointModel:(LTControlPointModel *)model NS_DESIGNATED_INITIALIZER;
+
+/// Adds the given \c control points.
+- (void)pushControlPoints:(NSArray<LTEuclideanSplineControlPoint *> *)controlPoints;
+
+/// Model representing the current \c parameterizedObject.
+- (LTControlPointModel *)controlPointModel;
+
+/// Iteratively constructed parameterized object. Is \c nil as long as the total number of control
+/// points provided to this object is smaller than the \c numberOfRequiredValues of the factory
+/// whose type was provided upon initialization.
+///
+/// @note The returned parameterized object constitutes an arc-length parameterized spline
+/// interpolating the control points of this instance according to the factory of the type provided
+/// upon initialization.
+///
+/// @important The returned parameterized object should not be stored since it might be modified by
+/// this object in the future.
+@property (readonly, nonatomic, nullable) id<LTParameterizedObject>parameterizedObject;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ LTEngine/LTEngine/LTParameterizedObjectConstructor.mm
@@ -0,0 +1,119 @@
+// Copyright (c) 2016 Lightricks. All rights reserved.
+// Created by Rouven Strauss.
+
+#import "LTParameterizedObjectConstructor.h"
+
+#import "LTBasicParameterizedObjectFactories.h"
+#import "LTCompoundParameterizedObjectFactory.h"
+#import "LTControlPointModel.h"
+#import "LTMutableEuclideanSpline.h"
+#import "LTParameterizedObjectType.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+#pragma mark -
+#pragma mark LTParameterizedObjectConstructorControlPointModel
+#pragma mark -
+
+/// Private subclass of the \c LTControlPointModel class for holding an \c LTMutableEuclideanSpline
+/// constructed by an \c LTParameterizedObjectConstructor.
+@interface LTParameterizedObjectConstructorControlPointModel : LTControlPointModel
+
+/// Initializes with the given \c type and the given \c spline.
+- (instancetype)initWithType:(LTParameterizedObjectType *)type
+                      spline:(nullable LTMutableEuclideanSpline *)spline;
+
+/// Spline constructed by an \c LTParameterizedObjectConstructor.
+@property (readonly, nonatomic, nullable) LTMutableEuclideanSpline *spline;
+
+@end
+
+@implementation LTParameterizedObjectConstructorControlPointModel
+
+- (instancetype)initWithType:(LTParameterizedObjectType *)type
+                      spline:(nullable LTMutableEuclideanSpline *)spline {
+  if (self = [super initWithType:type]) {
+    _spline = spline;
+  }
+  return self;
+}
+
+- (NSArray<LTEuclideanSplineControlPoint *> *)controlPoints {
+  return self.spline ? self.spline.controlPoints : @[];
+}
+
+@end
+
+#pragma mark -
+#pragma mark LTParameterizedObjectConstructor
+#pragma mark -
+
+@interface LTParameterizedObjectConstructor ()
+
+/// Type of the factory used for spline construction.
+@property (strong, nonatomic) LTParameterizedObjectType *type;
+
+/// Factory of primitive parameterized objects used as spline segments of the spline.
+@property (strong, nonatomic) id<LTBasicParameterizedObjectFactory> factory;
+
+/// Spline constructed by this instance.
+@property (strong, nonatomic) LTMutableEuclideanSpline *spline;
+
+/// Ordered collection used for temporarily buffering control points before construction of spline
+/// becomes feasible. Is set to \c nil upon construction of the spline.
+@property (strong, nonatomic, nullable) NSMutableArray<LTEuclideanSplineControlPoint *> *buffer;
+
+@end
+
+@implementation LTParameterizedObjectConstructor
+
+- (instancetype)initWithControlPointModel:(LTControlPointModel *)model {
+  LTParameterAssert(model);
+
+  if (self = [super init]) {
+    if ([model isKindOfClass:[LTParameterizedObjectConstructorControlPointModel class]]) {
+      LTParameterizedObjectConstructorControlPointModel *splineModel =
+          (LTParameterizedObjectConstructorControlPointModel *)model;
+      self.spline = splineModel.spline;
+    }
+    self.type = model.type;
+    self.factory = [model.type factory];
+    self.buffer = [NSMutableArray arrayWithCapacity:[[self.factory class] numberOfRequiredValues]];
+    [self pushControlPoints:model.controlPoints];
+  }
+  return self;
+}
+
+- (void)pushControlPoints:(NSArray<LTEuclideanSplineControlPoint *> *)controlPoints {
+  if (self.spline) {
+    [self.spline pushControlPoints:controlPoints];
+    return;
+  }
+
+  [self.buffer addObjectsFromArray:controlPoints];
+
+  if (self.buffer.count >= [[self.factory class] numberOfRequiredValues]) {
+    LTCompoundParameterizedObjectFactory *factory =
+        [[LTCompoundParameterizedObjectFactory alloc] initWithBasicFactory:self.factory];
+
+    self.spline = [[LTMutableEuclideanSpline alloc] initWithFactory:factory
+                                               initialControlPoints:self.buffer];
+    self.buffer = nil;
+    return;
+  }
+}
+
+- (LTControlPointModel *)controlPointModel {
+  return self.spline ?
+      [[LTParameterizedObjectConstructorControlPointModel alloc] initWithType:self.type
+                                                                       spline:self.spline] :
+      [[LTControlPointModel alloc] initWithType:self.type controlPoints:[self.buffer copy]];
+}
+
+- (nullable id<LTParameterizedObject>)parameterizedObject {
+  return self.spline;
+}
+
+@end
+
+NS_ASSUME_NONNULL_END
