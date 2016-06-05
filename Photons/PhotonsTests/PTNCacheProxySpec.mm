4667290a0c8e8e0e0beb0aa20497957cdd094e38
Barak Yoresh <barak@lightricks.com>
2016-06-05 14:23:46 +0300

    PTNCacheProxy: Fix a bug in -forwardingTargetForSelector. NSObject documentation states: >If an object implements (or inherits) this method, and returns a non-nil (and non-self) result, that returned object is used as the new receiver object and the message dispatch resumes to that new object. (Obviously if you return self from this method, the code would just fall into an infinite loop.)
    While I could not reproduce the infinite loop described, attempting with methods implemented by the underlying object, the proxy or neither - it's still safer to use the recommended implementation.
    Additional tests were added as well, as a precautionary measure.

--- Photons/PhotonsTests/PTNCacheProxySpec.mm
+++ Photons/PhotonsTests/PTNCacheProxySpec.mm
@@ -11,6 +11,7 @@
 
 SpecBegin(PTNCacheProxy)
 
+__block PTNCacheProxy *proxy;
 __block NSURL *url;
 __block NSArray *subalbums;
 __block NSArray *assets;
@@ -23,64 +24,62 @@ beforeEach(^{
   assets = @[@"baz", @"gaz"];
   underlyingObject = PTNCreateAlbum(url, assets, subalbums);
   cacheInfo = OCMClassMock([PTNCacheInfo class]);
+
+  proxy = [[PTNCacheProxy alloc] initWithUnderlyingObject:underlyingObject cacheInfo:cacheInfo];
 });
 
 it(@"should initialize with underlying object and cache info", ^{
-  PTNCacheProxy<id<PTNAlbum>> *proxy =
-      [[PTNCacheProxy alloc] initWithUnderlyingObject:underlyingObject cacheInfo:cacheInfo];
-
   expect(proxy.underlyingObject).to.equal(underlyingObject);
   expect(proxy.cacheInfo).to.equal(cacheInfo);
 });
 
 it(@"should proxy underlying object methods to underlying object", ^{
-  PTNCacheProxy<id<PTNAlbum>> *proxy =
-      [[PTNCacheProxy alloc] initWithUnderlyingObject:underlyingObject cacheInfo:cacheInfo];
-
   expect(((id<PTNAlbum>)proxy).url).to.equal(url);
   expect(((id<PTNAlbum>)proxy).subalbums).to.equal(subalbums);
   expect(((id<PTNAlbum>)proxy).assets).to.equal(assets);
 });
 
 it(@"should conform to protocols conformed by the underlying object", ^{
-  PTNCacheProxy<id<PTNAlbum>> *proxy =
-      [[PTNCacheProxy alloc] initWithUnderlyingObject:underlyingObject cacheInfo:cacheInfo];
-
   expect(proxy).to.conformTo(@protocol(PTNAlbum));
 });
 
 it(@"should respond to selectors supported by underlying object", ^{
-  PTNCacheProxy<id<PTNAlbum>> *proxy =
-      [[PTNCacheProxy alloc] initWithUnderlyingObject:underlyingObject cacheInfo:cacheInfo];
-
   expect(proxy).to.respondTo(@selector(url));
   expect(proxy).to.respondTo(@selector(subalbums));
   expect(proxy).to.respondTo(@selector(assets));
 });
 
 it(@"should not respond to selectors not supported by underlying object or the proxy", ^{
-  PTNCacheProxy<id<PTNAlbum>> *proxy =
-  [[PTNCacheProxy alloc] initWithUnderlyingObject:underlyingObject cacheInfo:cacheInfo];
-
   expect(proxy).toNot.respondTo(@selector(stringWithFormat:));
   expect(proxy).toNot.respondTo(@selector(count));
   expect(proxy).toNot.respondTo(@selector(readFromURL:options:error:));
 });
 
+it(@"should handle methods not implemented by forwarding to super", ^{
+  expect(^{
+    [(NSString *)proxy stringByAppendingString:@"foo"];
+  }).to.raise(NSInvalidArgumentException);
+});
+
 context(@"equality", ^{
   __block PTNCacheProxy *firstProxy;
   __block PTNCacheProxy *secondProxy;
+  __block PTNCacheProxy *otherProxy;
 
   beforeEach(^{
     firstProxy = [[PTNCacheProxy alloc] initWithUnderlyingObject:underlyingObject
                                                        cacheInfo:cacheInfo];
     secondProxy = [[PTNCacheProxy alloc] initWithUnderlyingObject:underlyingObject
                                                         cacheInfo:cacheInfo];
+    otherProxy = [[PTNCacheProxy alloc] initWithUnderlyingObject:underlyingObject
+        cacheInfo:OCMClassMock([PTNCacheInfo class])];
   });
 
   it(@"should handle isEqual correctly", ^{
     expect(firstProxy).to.equal(secondProxy);
     expect(secondProxy).to.equal(firstProxy);
+    expect(firstProxy).toNot.equal(otherProxy);
+    expect(secondProxy).toNot.equal(otherProxy);
   });
 
   it(@"should create proper hash", ^{
