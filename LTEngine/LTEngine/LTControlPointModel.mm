6a309f578040d8c5f1fc4064a4b320497194457c
Rouven Strauss <rouven@lightricks.com>
2016-06-05 14:02:22 +0300

    LTControlPointModel: Initial commit.
    This commit adds LTControlPointModel, a value class holding control
    points of a Euclidean spline and the type of a factory which can be used
    to construct a corresponding Euclidean spline from the control points.

---
+++ LTEngine/LTEngine/LTControlPointModel.mm
@@ -0,0 +1,52 @@
+// Copyright (c) 2016 Lightricks. All rights reserved.
+// Created by Rouven Strauss.
+
+#import "LTControlPointModel.h"
+
+#import "LTBasicParameterizedObjectFactory.h"
+#import "LTParameterizedObjectType.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@implementation LTControlPointModel
+
+#pragma mark -
+#pragma mark Initialization
+#pragma mark -
+
+- (instancetype)initWithType:(LTParameterizedObjectType *)type {
+  return [self initWithType:type controlPoints:@[]];
+}
+
+- (instancetype)initWithType:(LTParameterizedObjectType *)type
+                  controlPoints:(NSArray<LTEuclideanSplineControlPoint *> *)controlPoints {
+  if (self = [super init]) {
+    _type = type;
+    _controlPoints = [controlPoints copy];
+  }
+  return self;
+}
+
+#pragma mark -
+#pragma mark Equality
+#pragma mark -
+
+- (BOOL)isEqual:(LTControlPointModel *)model {
+  if (self == model) {
+    return YES;
+  }
+
+  if (![model isKindOfClass:[LTControlPointModel class]]) {
+    return NO;
+  }
+
+  return [self.type isEqual:model.type] && [self.controlPoints isEqualToArray:model.controlPoints];
+}
+
+- (NSUInteger)hash {
+  return self.type.hash ^ self.controlPoints.hash;
+}
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ LTEngine/LTEngineTests/LTControlPointModelSpec.mm
@@ -0,0 +1,78 @@
+// Copyright (c) 2015 Lightricks. All rights reserved.
+// Created by Rouven Strauss.
+
+#import "LTControlPointModel.h"
+
+#import "LTEuclideanSplineControlPoint.h"
+#import "LTParameterizedObjectType.h"
+
+SpecBegin(LTControlPointModel)
+
+__block LTControlPointModel *model;
+__block id typeMock;
+__block NSArray *controlPointMocks;
+
+beforeEach(^{
+  typeMock = OCMClassMock([LTParameterizedObjectType class]);
+  controlPointMocks = @[OCMClassMock([LTEuclideanSplineControlPoint class])];
+  model = [[LTControlPointModel alloc] initWithType:typeMock controlPoints:controlPointMocks];
+});
+
+afterEach(^{
+  model = nil;
+  typeMock = nil;
+  controlPointMocks = nil;
+});
+
+context(@"initialization", ^{
+  it(@"should initialize correctly", ^{
+    expect(model.type).to.equal(typeMock);
+    expect(model.controlPoints).to.equal(controlPointMocks);
+  });
+});
+
+context(@"NSObject protocol", ^{
+  context(@"equality", ^{
+    it(@"should return YES when comparing to itself", ^{
+      expect([model isEqual:model]).to.beTruthy();
+    });
+
+    it(@"should return YES when comparing to equal model", ^{
+      LTControlPointModel *anotherModel =
+          [[LTControlPointModel alloc] initWithType:typeMock controlPoints:controlPointMocks];
+      expect([model isEqual:anotherModel]).to.beTruthy();
+    });
+
+    it(@"should return NO when comparing to nil", ^{
+      expect([model isEqual:nil]).to.beFalsy();
+    });
+
+    it(@"should return NO when comparing to an object of a different class", ^{
+      expect([model isEqual:[[NSObject alloc] init]]).to.beFalsy();
+    });
+
+    it(@"should return NO when comparing to model with different type", ^{
+      LTParameterizedObjectType *anotherTypeMock = OCMClassMock([LTParameterizedObjectType class]);
+      LTControlPointModel *anotherModel =
+          [[LTControlPointModel alloc] initWithType:anotherTypeMock
+                                      controlPoints:controlPointMocks];
+      expect([model isEqual:anotherModel]).to.beFalsy();
+    });
+
+    it(@"should return NO when comparing to model with different control points", ^{
+      LTControlPointModel *anotherModel =
+          [[LTControlPointModel alloc] initWithType:typeMock controlPoints:@[]];
+      expect([model isEqual:anotherModel]).to.beFalsy();
+    });
+  });
+
+  context(@"hash", ^{
+    it(@"should return the same hash value for equal objects", ^{
+      LTControlPointModel *anotherModel =
+          [[LTControlPointModel alloc] initWithType:typeMock controlPoints:controlPointMocks];
+      expect(model.hash).to.equal(anotherModel.hash);
+    });
+  });
+});
+
+SpecEnd
