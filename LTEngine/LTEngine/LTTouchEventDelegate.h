ebb402dc03fc406e0846ee98839b80896a088ae0
Rouven Strauss <rouven@lightricks.com>
2016-06-06 16:37:01 +0300

    LTTouchEventDelegate: Update to use LTTouchEvent protocol.
    This commit updates LTTouchEventDelegate to use the LTTouchEvent
    protocol rather than the LTTouchEvent class.

--- LTEngine/LTEngine/LTTouchEventDelegate.h
+++ LTEngine/LTEngine/LTTouchEventDelegate.h
@@ -5,36 +5,39 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@class LTTouchEvent;
-
-/// Possible states of a touch event sequence.
-typedef NS_ENUM(NSUInteger, LTTouchEventSequenceState) {
-  /// Indicates that the touch event sequences has just started.
-  LTTouchEventSequenceStateStart,
-  /// Indicates that the touch event sequence is currently being continued.
-  LTTouchEventSequenceStateContinuation,
-  /// Indicates that the touch event sequence has just ended.
-  LTTouchEventSequenceStateEnd,
-  /// Indicates that the touch event sequence has just been cancelled.
-  LTTouchEventSequenceStateCancellation
-};
-
-/// Protocol to be implemented by objects to which \c LTTouchEvent objects are delegated.
+/// Protocol to be implemented by objects to which \c id<LTTouchEvent> objects are delegated.
+///
+/// @important Termination or cancellation of a touch event sequence is solely reported once, either
+/// via the \c receivedTouchEvents:predictedEvents:touchEventSequenceState: or the
+/// \c terminatedTouchEventSequencesWithIDs: method.
 @protocol LTTouchEventDelegate <NSObject>
 
 /// Called to inform the delegate that the given \c touchEvents and \c predictedTouchEvents have
-/// been provided by the given \c touchEventProvider. The \c touchEvents belong to a touch event
-/// sequence with the given \c state. The given \c predictedTouchEvents consist of possibly existing
-/// predicted touches. Both the \c touchEvents and the \c predictedTouchEvents are ordered according
-/// to their timestamps.
+/// been received. The \c touchEvents all belong to the same touch event sequence with the given
+/// \c state. The given \c predictedTouchEvents consist of possibly existing predicted touches. Both
+/// the \c touchEvents and the \c predictedTouchEvents are ordered according to their timestamps.
+///
+/// @important A call to this method with state \c LTTouchEventSequenceStateEnd or
+/// \c LTTouchEventSequenceStateCancellation is neither followed nor preceeded by a call to the
+/// \c terminatedTouchEventSequencesWithIDs: method with the same sequence ID.
 - (void)receivedTouchEvents:(LTTouchEvents *)touchEvents
             predictedEvents:(LTTouchEvents *)predictedTouchEvents
     touchEventSequenceState:(LTTouchEventSequenceState)state;
 
-/// Called to delegate the handling of the given updated \c touchEvents provided by the given
-/// \c touchEventProvider. The \c touchEvents are ordered according to their timestamps and belong
-/// to touch event sequences that have not ended yet.
+/// Called to delegate the handling of the given updated \c touchEvents. The \c touchEvents are
+/// ordered according to their timestamps and belong to touch event sequences that have not ended
+/// yet.
 - (void)receivedUpdatesOfTouchEvents:(LTTouchEvents *)touchEvents;
+
+/// Called to inform the delegate that all touch event sequences with the given \c sequenceIDs have
+/// been terminated with the given \c state. The given \c state is either
+/// \c LTTouchEventSequenceStateEnd or \c LTTouchEventSequenceStateCancellation. This call is
+/// neither preceeded nor followed by a call to
+/// \c receivedTouchEvents:predictedEvents:touchEventSequenceState: with state
+/// \c LTTouchEventSequenceStateEnd or \c LTTouchEventSequenceStateCancellation and a sequence ID
+/// provided in the given \c sequenceIDs.
+- (void)touchEventSequencesWithIDs:(NSSet<NSNumber *> *)sequenceIDs
+               terminatedWithState:(LTTouchEventSequenceState)state;
 
 @end
 
