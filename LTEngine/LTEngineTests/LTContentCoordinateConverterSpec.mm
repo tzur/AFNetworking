d321ba68bf44ad4550ab9d5b019813ab2ad0b456
Rouven Strauss <rouven@lightricks.com>
2016-06-20 10:28:54 +0300

    LTContentCoordinateConverter: Initial commit.
    This commit adds LTContentCoordinateConverter, a class converting points
    between the content coordinate system and the presentation coordinate
    system.

---
+++ LTEngine/LTEngineTests/LTContentCoordinateConverterSpec.mm
@@ -0,0 +1,65 @@
+// Copyright (c) 2016 Lightricks. All rights reserved.
+// Created by Rouven Strauss.
+
+#import "LTContentCoordinateConverter.h"
+
+#import "LTContentLocationProvider.h"
+
+static LTContentCoordinateConverter *LTTestConverter(CGRect visibleContentRect, CGFloat zoomScale,
+                                                     CGFloat contentScaleFactor) {
+  id<LTContentLocationProvider> locationProviderMock =
+      OCMProtocolMock(@protocol(LTContentLocationProvider));
+  OCMStub([locationProviderMock visibleContentRect]).andReturn(visibleContentRect);
+  OCMStub([locationProviderMock zoomScale]).andReturn(zoomScale);
+  OCMStub([locationProviderMock contentScaleFactor]).andReturn(contentScaleFactor);
+  return [[LTContentCoordinateConverter alloc] initWithLocationProvider:locationProviderMock];
+}
+
+SpecBegin(LTContentCoordinateConverter)
+
+context(@"initialization", ^{
+  it(@"should initialize correctly", ^{
+    LTContentCoordinateConverter *converter = LTTestConverter(CGRectMake(0, 0, 1, 1), 1, 1);
+    expect(converter).toNot.beNil();
+  });
+});
+
+context(@"conversion", ^{
+  it(@"should convert points from content coordinate system to presentation coordinate system", ^{
+    LTContentCoordinateConverter *converter = LTTestConverter(CGRectMake(0, 0, 1, 1), 1, 1);
+    CGPoint convertedPoint = [converter convertPointFromContentToView:CGPointMake(2, 3)];
+    expect(convertedPoint).to.beCloseToPoint(CGPointMake(2, 3));
+
+    converter = LTTestConverter(CGRectMake(1, 1, 1, 1), 1, 1);
+    convertedPoint = [converter convertPointFromContentToView:CGPointMake(2, 3)];
+    expect(convertedPoint).to.beCloseToPoint(CGPointMake(1, 2));
+
+    converter = LTTestConverter(CGRectMake(100, 200, 300, 400), 0.5, 3);
+    convertedPoint = [converter convertPointFromContentToView:CGPointMake(250, 400)];
+    expect(convertedPoint).to.beCloseToPoint(CGPointMake(25, 100 / 3.0));
+
+    converter = LTTestConverter(CGRectMake(0, -100, 300, 400), 0.2, 2);
+    convertedPoint = [converter convertPointFromContentToView:CGPointMake(0, 0)];
+    expect(convertedPoint).to.beCloseToPoint(CGPointMake(0, 10));
+  });
+
+  it(@"should convert points from presentation coordinate system to content coordinate system", ^{
+    LTContentCoordinateConverter *converter = LTTestConverter(CGRectMake(0, 0, 1, 1), 1, 1);
+    CGPoint convertedPoint = [converter convertPointFromViewToContent:CGPointMake(2, 3)];
+    expect(convertedPoint).to.beCloseToPoint(CGPointMake(2, 3));
+
+    converter = LTTestConverter(CGRectMake(1, 1, 1, 1), 1, 1);
+    convertedPoint = [converter convertPointFromViewToContent:CGPointMake(1, 2)];
+    expect(convertedPoint).to.beCloseToPoint(CGPointMake(2, 3));
+
+    converter = LTTestConverter(CGRectMake(100, 200, 300, 400), 0.5, 3);
+    convertedPoint = [converter convertPointFromViewToContent:CGPointMake(25, 100 / 3.0)];
+    expect(convertedPoint).to.beCloseToPoint(CGPointMake(250, 400));
+
+    converter = LTTestConverter(CGRectMake(0, -100, 300, 400), 0.2, 2);
+    convertedPoint = [converter convertPointFromViewToContent:CGPointMake(0, 10)];
+    expect(convertedPoint).to.beCloseToPoint(CGPointMake(0, 0));
+  });
+});
+
+SpecEnd
