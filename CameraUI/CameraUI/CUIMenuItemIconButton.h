9bfe994db2a5cd89d53517e82bf1c5f0443e6505
Reuven Siman Tov <rsimantov@lightricks.com>
2016-06-26 12:04:54 +0300

    CUIMenuItemIconButton - initial commit.

---
+++ CameraUI/CameraUI/CUIMenuItemIconButton.h
@@ -0,0 +1,24 @@
+// Copyright (c) 2016 Lightricks. All rights reserved.
+// Created by Reuven Siman Tov.
+
+#import "CUIMenuItemButton.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+/// \c UIButton that shows an icon of a \c CUIMenuItemViewModel.
+///
+/// Ignores the \c title and \c subitems properties of the \c CUIMenuItemViewModel.
+///
+/// The icon's color, highlighted color and selected color are being set according to current \c
+/// CUISharedTheme instance.
+@interface CUIMenuItemIconButton : UIButton <CUIMenuItemButton>
+
+- (instancetype)init NS_UNAVAILABLE;
+
+- (instancetype)initWithFrame:(CGRect)aRect NS_UNAVAILABLE;
+
++ (instancetype)buttonWithType:(UIButtonType)buttonType NS_UNAVAILABLE;
+
+@end
+
+NS_ASSUME_NONNULL_END
