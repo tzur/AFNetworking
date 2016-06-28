a82e7e32431a12bc27fff3128b26855ae343b38c
Rouven Strauss <rouven@lightricks.com>
2016-06-28 19:23:23 +0200

    LTContentLocationProvider: Improve API.
    This commit improves the API of the LTContentLocationProvider protocol
    and allows implementing objects to be completely immutable.

--- LTEngine/LTEngine/LTContentLocationProvider.h
+++ LTEngine/LTEngine/LTContentLocationProvider.h
@@ -3,15 +3,13 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@class LTViewNavigationState;
-
 /// Protocol which should be implemented by objects providing the location of a rectangle bounding
 /// pixel content that can be displayed inside a suitable view. The content rectangle is
 /// axis-aligned with the enclosing view.
 @protocol LTContentLocationProvider <NSObject>
 
-/// Size, in integer pixel units of the content coordinate system, of the content rectangle managed
-/// by this instance.
+/// Size, in integer pixel units of the content coordinate system, of the rectangle managed by this
+/// instance.
 @property (readonly, nonatomic) CGSize contentSize;
 
 /// Scale factor of the enclosing view.
@@ -23,8 +21,8 @@ NS_ASSUME_NONNULL_BEGIN
 /// enclosing view.
 @property (readonly, nonatomic) UIEdgeInsets contentInset;
 
-/// Rectangular subregion of the content rectangle, in point units of the content coordinate
-/// system, intersecting with the view enclosing the content rectangle.
+/// Rectangular subregion, in floating-point pixel units of the content coordinate system, of the
+/// content rectangle intersecting with the view enclosing the content rectangle.
 @property (readonly, nonatomic) CGRect visibleContentRect;
 
 /// Number of floating-point pixel units of the screen coordinate system per pixel unit of the
@@ -35,16 +33,6 @@ NS_ASSUME_NONNULL_BEGIN
 /// the zoom scale is defined as the ratio between the width of the enclosing view and the
 /// corresponding dimension of the content rectangle.
 @property (readonly, nonatomic) CGFloat zoomScale;
-
-/// View that can be used for acquiring touch and gesture locations in content coordinates.
-/// For example, the following will return the gesture location in content coordinates (in points):
-/// @code
-/// [gesture locationInView:ltView.viewForContentCoordinates]
-/// @endcode
-@property (readonly, nonatomic) UIView *viewForContentCoordinates;
-
-/// Current navigation state of this instance.
-@property (readonly, nonatomic) LTViewNavigationState *navigationState;
 
 @end
 
