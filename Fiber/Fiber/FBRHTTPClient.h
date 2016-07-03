fcedd665ffde323af25896ad3d71be780b781208
Daniel Lahyani <dlahyani@lightricks.com>
2016-07-03 22:47:41 +0300

    FBRHTTPSession: Refactor.
    FBRHTTPSession: Make it a non-reactive protocol for HTTP sessions.
    FBRHTTPClient: Provides reactive and more friendly interface on top of
    FBRHTTPSession.

--- Fiber/Fiber/FBRHTTPClient.h
+++ Fiber/Fiber/FBRHTTPClient.h
@@ -6,33 +6,21 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @class FBRHTTPSessionConfiguration;
+
 @protocol FBRHTTPSession;
 
-/// \c FBRHTTPClient is a wrapper object for \c FBRHTTPSession. It provides convenience reactive
-/// interface for HTTP clients.
+/// Wrapper object for \c FBRHTTPSession providing convenience reactive interface for making HTTP
+/// requests.
 ///
-/// @see FBRHTTPRequest, FBRHTTPSession, FBRHTTPSessionCofniguration.
+/// @see FBRHTTPRequest, FBRHTTPSession, FBRHTTPSessionConfiguration.
 @interface FBRHTTPClient : NSObject
 
 - (instancetype)init NS_UNAVAILABLE;
 
-/// Creates a new HTTP client using the default HTTP session implementation. The unserlying session
-/// is initialized with the configuration provided by \c -[FBRHTTPSessionConfiguration init].
-+ (instancetype)client;
-
-/// Creates a new HTTP client using the default HTTP session implementation. The underlying session
-/// is initialized with the given \c configuration. If \c baseURL is not \c nil it will be used as
-/// prefix URL for all HTTP requests issued by this client.
-///
-/// @see -[FBRHTTPClient initWithSession:baseURL:].
-+ (instancetype)clientWithSessionConfiguration:(FBRHTTPSessionConfiguration *)configuration
-                                       baseURL:(nullable NSURL *)baseURL;
-
-
 /// Initializes the receiver with a custom underlying HTTP \c session. If \c baseURL is not \c nil
 /// it will be used as prefix URL for all HTTP requests issued by this client. \c baseURL must not
-/// specify \c fragment or \c query string, if either is present an \c NSInvalidArgumentException
-/// is raised.
+/// specify \c fragment or \c query string, if either is present or if the \c scheme of the URL
+/// is not 'http' nor 'https' an \c NSInvalidArgumentException is raised.
 - (instancetype)initWithSession:(id<FBRHTTPSession>)session baseURL:(nullable NSURL *)baseURL
     NS_DESIGNATED_INITIALIZER;
 
@@ -42,10 +30,10 @@ NS_ASSUME_NONNULL_BEGIN
 ///
 /// @return <tt>RACSignal<FBRHTTPTaskProgress *></tt>. The signal sends the request on subscription,
 /// and delivers a sequence of \c FBRHTTPTaskProgress objects representing the task status as it
-/// progress until it completes. When the task completes the server response body is delivered
+/// progresses until it completes. When the task completes the server response body is delivered
 /// wrapped in an \c FBRHTTPTaskProgress object. The signal errs if a communication error occurs or
-/// if the server response indicates an error (i.e. status code not in the range \c [200, 299]).
-/// Values and errors are delivered on the main queue.
+/// if the server response indicates an error (i.e. status code not in the range
+/// <tt>[200, 299]</tt>). Values and errors are delivered on the main queue.
 - (RACSignal *)GET:(NSString *)URLString
     withParameters:(nullable FBRHTTPRequestParameters *)parameters;
 
@@ -55,10 +43,10 @@ NS_ASSUME_NONNULL_BEGIN
 ///
 /// @return <tt>RACSignal<FBRHTTPTaskProgress *></tt>. The signal sends the request on subscription,
 /// and delivers a sequence of \c FBRHTTPTaskProgress objects representing the task status as it
-/// progress until it completes. When the task completes the server response body is delivered
+/// progresses until it completes. When the task completes the server response body is delivered
 /// wrapped in an \c FBRHTTPTaskProgress object. The signal errs if a communication error occurs or
-/// if the server response indicates an error (i.e. status code not in the range \c [200, 299]).
-/// Values and errors are delivered on the main queue.
+/// if the server response indicates an error (i.e. status code not in the range
+/// <tt>[200, 299]</tt>). Values and errors are delivered on the main queue.
 - (RACSignal *)HEAD:(NSString *)URLString
      withParameters:(nullable FBRHTTPRequestParameters *)parameters;
 
@@ -68,10 +56,10 @@ NS_ASSUME_NONNULL_BEGIN
 ///
 /// @return <tt>RACSignal<FBRHTTPTaskProgress *></tt>. The signal sends the request on subscription,
 /// and delivers a sequence of \c FBRHTTPTaskProgress objects representing the task status as it
-/// progress until it completes. When the task completes the server response body is delivered
+/// progresses until it completes. When the task completes the server response body is delivered
 /// wrapped in an \c FBRHTTPTaskProgress object. The signal errs if a communication error occurs or
-/// if the server response indicates an error (i.e. status code not in the range \c [200, 299]).
-/// Values and errors are delivered on the main queue.
+/// if the server response indicates an error (i.e. status code not in the range
+/// <tt>[200, 299]</tt>). Values and errors are delivered on the main queue.
 - (RACSignal *)POST:(NSString *)URLString
      withParameters:(nullable FBRHTTPRequestParameters *)parameters;
 
@@ -81,10 +69,10 @@ NS_ASSUME_NONNULL_BEGIN
 ///
 /// @return <tt>RACSignal<FBRHTTPTaskProgress *></tt>. The signal sends the request on subscription,
 /// and delivers a sequence of \c FBRHTTPTaskProgress objects representing the task status as it
-/// progress until it completes. When the task completes the server response body is delivered
+/// progresses until it completes. When the task completes the server response body is delivered
 /// wrapped in an \c FBRHTTPTaskProgress object. The signal errs if a communication error occurs or
-/// if the server response indicates an error (i.e. status code not in the range \c [200, 299]).
-/// Values and errors are delivered on the main queue.
+/// if the server response indicates an error (i.e. status code not in the range
+/// <tt>[200, 299]</tt>). Values and errors are delivered on the main queue.
 - (RACSignal *)PUT:(NSString *)URLString
     withParameters:(nullable FBRHTTPRequestParameters *)parameters;
 
@@ -94,12 +82,10 @@ NS_ASSUME_NONNULL_BEGIN
 ///
 /// @return <tt>RACSignal<FBRHTTPTaskProgress *></tt>. The signal sends the request on subscription,
 /// and delivers a sequence of \c FBRHTTPTaskProgress objects representing the task status as it
-/// progress until it completes. When the task completes the server response body is delivered
+/// progresses until it completes. When the task completes the server response body is delivered
 /// wrapped in an \c FBRHTTPTaskProgress object. The signal errs if a communication error occurs or
-/// if the server response indicates an error (i.e. status code not in the range \c [200, 299]).
-/// Values and errors are delivered on the main queue.
-///
-/// @see FBRHTTPRequestParameters, FBRHTTPSessionRequestConfiguration.
+/// if the server response indicates an error (i.e. status code not in the range
+/// <tt>[200, 299]</tt>). Values and errors are delivered on the main queue.
 - (RACSignal *)PATCH:(NSString *)URLString
       withParameters:(nullable FBRHTTPRequestParameters *)parameters;
 
@@ -109,19 +95,17 @@ NS_ASSUME_NONNULL_BEGIN
 ///
 /// @return <tt>RACSignal<FBRHTTPTaskProgress *></tt>. The signal sends the request on subscription,
 /// and delivers a sequence of \c FBRHTTPTaskProgress objects representing the task status as it
-/// progress until it completes. When the task completes the server response body is delivered
+/// progresses until it completes. When the task completes the server response body is delivered
 /// wrapped in an \c FBRHTTPTaskProgress object. The signal errs if a communication error occurs or
-/// if the server response indicates an error (i.e. status code not in the range \c [200, 299]).
-/// Values and errors are delivered on the main queue.
-///
-/// @see FBRHTTPRequestParameters, FBRHTTPSessionRequestConfiguration.
+/// if the server response indicates an error (i.e. status code not in the range
+/// <tt>[200, 299]</tt>). Values and errors are delivered on the main queue.
 - (RACSignal *)DELETE:(NSString *)URLString
        withParameters:(nullable FBRHTTPRequestParameters *)parameters;
 
 /// Underlying HTTP session.
 @property (readonly, nonatomic) id<FBRHTTPSession> session;
 
-/// Base URL used as prefix for requests made by this client.
+/// Base URL used as prefix for requests made by this client or \c nil if no base URL is used.
 @property (readonly, nonatomic, nullable) NSURL *baseURL;
 
 @end
---
+++ Fiber/FiberTests/FBRHTTPClientSpec.mm
@@ -0,0 +1,255 @@
+// Copyright (c) 2016 Lightricks. All rights reserved.
+// Created by Daniel Lahyani.
+
+#import "FBRHTTPClient.h"
+
+#import <LTKit/NSError+LTKit.h>
+
+#import "FBRHTTPRequest.h"
+#import "FBRHTTPResponse.h"
+#import "FBRHTTPSession.h"
+#import "FBRHTTPTaskProgress.h"
+#import "NSErrorCodes+Fiber.h"
+
+/// Block used as a completion handler with a boolean \c finished parameter.
+typedef RACSignal *(^FBRHTTPClientRequestInitiator)
+    (FBRHTTPClient *client, NSString *URLString, FBRHTTPRequestParameters * _Nullable parameters);
+
+NSString * const kFBRHTTPClientRequestExamples = @"FBRHTTPClientRequestExamples";
+NSString * const kFBRHTTPClientExpectedMethod = @"FBRHTTPClientExpectedMethod";
+NSString * const kFBRHTTPClientRequestInitiator = @"FBRHTTPClientRequestInitiator";
+
+SpecBegin(FBRHTTPClient)
+
+sharedExamplesFor(kFBRHTTPClientRequestExamples, ^(NSDictionary *data) {
+  // Extracted from the data dictionary.
+  __block FBRHTTPClientRequestInitiator requestInitiator;
+  __block FBRHTTPRequestMethod *method;
+
+  // Initialized by the shared examples.
+  __block FBRHTTPClient *client;
+  __block id session;
+  __block id task;
+  __block NSURL *URL;
+
+  beforeEach(^{
+    method = data[kFBRHTTPClientExpectedMethod];
+    requestInitiator = data[kFBRHTTPClientRequestInitiator];
+
+    session = OCMStrictProtocolMock(@protocol(FBRHTTPSession));
+    task = OCMClassMock([NSURLSessionDataTask class]);
+    client = [[FBRHTTPClient alloc] initWithSession:session baseURL:nil];
+    URL = [NSURL URLWithString:@"http://foo.bar/index.html"];
+  });
+
+  it(@"should return a signal", ^{
+    RACSignal *signal = requestInitiator(client, URL.absoluteString, nil);
+    expect(signal).to.beKindOf([RACSignal class]);
+  });
+
+  it(@"should raise exception if the URL string is invalid", ^{
+    expect(^{
+      requestInitiator(client, @"/foo/bar", nil);
+    }).to.raise(NSInvalidArgumentException);
+  });
+
+  it(@"should send a request to the session when subscribed to", ^{
+    FBRHTTPRequestParameters *parameters = @{@"Foo": @"Bar"};
+    FBRHTTPRequest *expectedRequest =
+        [[FBRHTTPRequest alloc] initWithURL:URL method:method parameters:parameters
+                         parametersEncoding:nil headers:nil];
+    OCMExpect([session dataTaskWithRequest:expectedRequest progress:OCMOCK_ANY
+                                   success:OCMOCK_ANY failure:OCMOCK_ANY]).andReturn(task);
+
+    RACSignal *signal = requestInitiator(client, URL.absoluteString, parameters);
+    [signal subscribeNext:^(id) {}];
+    OCMVerifyAll(session);
+  });
+
+  it(@"should forward progress values when reported by the session", ^{
+    NSProgress *progress = [NSProgress progressWithTotalUnitCount:100];
+    __block FBRHTTPTaskProgressBlock progressBlock;
+    OCMStub([session dataTaskWithRequest:OCMOCK_ANY
+                                progress:[OCMArg checkWithBlock:^BOOL(id obj) {
+      progressBlock = obj;
+      return obj != nil;
+    }] success:OCMOCK_ANY failure:OCMOCK_ANY]).andReturn(task);
+
+    LLSignalTestRecorder *recorder =
+        [requestInitiator(client, URL.absoluteString, nil) testRecorder];
+
+    expect(progressBlock).toNot.beNil();
+    progress.completedUnitCount = 0;
+    progressBlock(progress);
+    progress.completedUnitCount = 50;
+    progressBlock(progress);
+    progress.completedUnitCount = 100;
+    progressBlock(progress);
+
+    expect(recorder).to.sendValues(@[
+      [[FBRHTTPTaskProgress alloc] initWithProgress:0],
+      [[FBRHTTPTaskProgress alloc] initWithProgress:0.5],
+      [[FBRHTTPTaskProgress alloc] initWithProgress:1]
+    ]);
+    expect(recorder).toNot.complete();
+  });
+
+  it(@"should forward server response when reported by the session", ^{
+    id responseMetadata = OCMClassMock([NSHTTPURLResponse class]);
+    FBRHTTPResponse *response = [[FBRHTTPResponse alloc] initWithMetadata:responseMetadata
+                                                                  content:nil];
+    OCMStub([session dataTaskWithRequest:OCMOCK_ANY progress:OCMOCK_ANY
+                                 success:([OCMArg invokeBlockWithArgs:response, nil])
+                                 failure:OCMOCK_ANY]).andReturn(task);
+
+    LLSignalTestRecorder *recorder =
+        [requestInitiator(client, URL.absoluteString, nil) testRecorder];
+
+    FBRHTTPTaskProgress *progress = [[FBRHTTPTaskProgress alloc] initWithResponse:response];
+    expect(recorder).to.sendValues(@[progress]);
+    expect(recorder).to.complete();
+  });
+
+  it(@"should err when session reports an error", ^{
+    NSError *error = [NSError lt_errorWithCode:1337];
+    OCMStub([session dataTaskWithRequest:OCMOCK_ANY progress:OCMOCK_ANY success:OCMOCK_ANY
+                                 failure:([OCMArg invokeBlockWithArgs:error, nil])])
+        .andReturn(task);
+
+    LLSignalTestRecorder *recorder =
+        [requestInitiator(client, URL.absoluteString, nil) testRecorder];
+
+    expect(recorder).to.matchError(^BOOL(NSError *error) {
+      return error.lt_isLTDomain && error.code == 1337;
+    });
+  });
+
+  it(@"should cancel the task when unsubscribed", ^{
+    OCMExpect([task cancel]);
+    OCMStub([session dataTaskWithRequest:OCMOCK_ANY progress:OCMOCK_ANY success:OCMOCK_ANY
+                                 failure:OCMOCK_ANY]).andReturn(task);
+
+    [[requestInitiator(client, URL.absoluteString, nil) subscribeNext:^(id) {}] dispose];
+
+    OCMVerifyAll(task);
+  });
+});
+
+context(@"initialization", ^{
+  it(@"should initialize with the given session object and nil base URL", ^{
+    id session = OCMProtocolMock(@protocol(FBRHTTPSession));
+    FBRHTTPClient *client = [[FBRHTTPClient alloc] initWithSession:session baseURL:nil];
+
+    expect(client.session).to.beIdenticalTo(session);
+    expect(client.baseURL).to.beNil();
+  });
+
+  it(@"should initialize with the given session and base URL", ^{
+    id session = OCMProtocolMock(@protocol(FBRHTTPSession));
+    NSURL *baseURL = [NSURL URLWithString:@"http://foo.bar/baz"];
+    FBRHTTPClient *client = [[FBRHTTPClient alloc] initWithSession:session baseURL:baseURL];
+
+    expect(client.session).to.beIdenticalTo(session);
+    expect(client.baseURL).to.equal(baseURL);
+  });
+
+  it(@"should raise exception if base URL specifies unsupported protocol", ^{
+    id session = OCMProtocolMock(@protocol(FBRHTTPSession));
+    NSURL *baseURL = [NSURL URLWithString:@"ftp://foo.bar/baz"];
+
+    expect(^{
+      FBRHTTPClient __unused *client =
+          [[FBRHTTPClient alloc] initWithSession:session baseURL:baseURL];
+    }).to.raise(NSInvalidArgumentException);
+  });
+
+  it(@"should raise exception if base URL specifies fragment", ^{
+    id session = OCMProtocolMock(@protocol(FBRHTTPSession));
+    NSURL *baseURL = [NSURL URLWithString:@"http://foo.bar/index.html#baz"];
+
+    expect(^{
+      FBRHTTPClient __unused *client =
+          [[FBRHTTPClient alloc] initWithSession:session baseURL:baseURL];
+    }).to.raise(NSInvalidArgumentException);
+  });
+
+  it(@"should raise exception if base URL specifies query string", ^{
+    id session = OCMProtocolMock(@protocol(FBRHTTPSession));
+    NSURL *baseURL = [NSURL URLWithString:@"http://foo.bar/index.html?bar=baz"];
+
+    expect(^{
+      FBRHTTPClient __unused *client =
+          [[FBRHTTPClient alloc] initWithSession:session baseURL:baseURL];
+    }).to.raise(NSInvalidArgumentException);
+  });
+});
+
+context(@"HTTP requests", ^{
+  context(@"GET", ^{
+    itShouldBehaveLike(kFBRHTTPClientRequestExamples, @{
+      kFBRHTTPClientExpectedMethod: $(FBRHTTPRequestMethodGet),
+      kFBRHTTPClientRequestInitiator:
+          ^RACSignal *(FBRHTTPClient *client, NSString *URLString,
+                       FBRHTTPRequestParameters * _Nullable parameters) {
+            return [client GET:URLString withParameters:parameters];
+          }
+    });
+  });
+
+  context(@"HEAD", ^{
+    itShouldBehaveLike(kFBRHTTPClientRequestExamples, @{
+      kFBRHTTPClientExpectedMethod: $(FBRHTTPRequestMethodHead),
+      kFBRHTTPClientRequestInitiator:
+          ^RACSignal *(FBRHTTPClient *client, NSString *URLString,
+                       FBRHTTPRequestParameters * _Nullable parameters) {
+            return [client HEAD:URLString withParameters:parameters];
+          }
+    });
+  });
+
+  context(@"POST", ^{
+    itShouldBehaveLike(kFBRHTTPClientRequestExamples, @{
+      kFBRHTTPClientExpectedMethod: $(FBRHTTPRequestMethodPost),
+      kFBRHTTPClientRequestInitiator:
+          ^RACSignal *(FBRHTTPClient *client, NSString *URLString,
+                       FBRHTTPRequestParameters * _Nullable parameters) {
+            return [client POST:URLString withParameters:parameters];
+          }
+    });
+  });
+
+  context(@"PUT", ^{
+    itShouldBehaveLike(kFBRHTTPClientRequestExamples, @{
+      kFBRHTTPClientExpectedMethod: $(FBRHTTPRequestMethodPut),
+      kFBRHTTPClientRequestInitiator:
+          ^RACSignal *(FBRHTTPClient *client, NSString *URLString,
+                       FBRHTTPRequestParameters * _Nullable parameters) {
+            return [client PUT:URLString withParameters:parameters];
+          }
+    });
+  });
+
+  context(@"PATCH", ^{
+    itShouldBehaveLike(kFBRHTTPClientRequestExamples, @{
+      kFBRHTTPClientExpectedMethod: $(FBRHTTPRequestMethodPatch),
+      kFBRHTTPClientRequestInitiator:
+          ^RACSignal *(FBRHTTPClient *client, NSString *URLString,
+                       FBRHTTPRequestParameters * _Nullable parameters) {
+            return [client PATCH:URLString withParameters:parameters];
+          }
+    });
+  });
+
+  context(@"DELETE", ^{
+    itShouldBehaveLike(kFBRHTTPClientRequestExamples, @{
+      kFBRHTTPClientExpectedMethod: $(FBRHTTPRequestMethodDelete),
+      kFBRHTTPClientRequestInitiator:
+          ^RACSignal *(FBRHTTPClient *client, NSString *URLString,
+                       FBRHTTPRequestParameters * _Nullable parameters) {
+            return [client DELETE:URLString withParameters:parameters];
+          }
+    });
+  });
+});
+
+SpecEnd
