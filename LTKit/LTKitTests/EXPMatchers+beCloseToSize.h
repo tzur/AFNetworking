fedf4b5f51dcca83c4b48049c3d5bebe2f9a5f7a
Rouven Strauss <rouven@lightricks.com>
2016-07-05 13:35:45 +0200

    EXPMatchers+beCloseToSize: Initial commit.
    This commit adds beCloseToSize and beCloseToSizeWithin preprocessor
    definitions, for convenience purposes.

---
+++ LTKit/LTKitTests/EXPMatchers+beCloseToSize.h
@@ -0,0 +1,7 @@
+// Copyright (c) 2016 Lightricks. All rights reserved.
+// Created by Rouven Strauss.
+
+#import "EXPMatchers+beCloseToPoint.h"
+
+#define beCloseToSize beCloseToPoint
+#define beCloseToSizeWithin beCloseToPointWithin
