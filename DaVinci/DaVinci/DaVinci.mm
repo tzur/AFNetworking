7db5a445e63c3f5b30e2cb0858fb0dbded089676
Rouven Strauss <rouven@lightricks.com>
2016-07-06 12:53:21 +0200

    DaVinci: Initial commit.
    This commit creates the DaVinci project, a project providing a highly
    configurable pipeline for continuous parameterized rendering. The
    main usage of the pipeline is brush stroke rendering.

---
+++ DaVinci/DaVinci/DaVinci.mm
@@ -0,0 +1,7 @@
+// Copyright (c) 2016 Lightricks. All rights reserved.
+// Created by Rouven Strauss.
+
+#import "DaVinci.h"
+
+@implementation DaVinci
+@end
---
+++ DaVinci/DaVinciTests/DaVinciTests.mm
@@ -0,0 +1,10 @@
+// Copyright (c) 2016 Lightricks. All rights reserved.
+// Created by Rouven Strauss.
+
+SpecBegin(DaVinci)
+
+it(@"fake test", ^{
+  expect(1).to.equal(1);
+});
+
+SpecEnd
