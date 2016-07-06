51f8b591e42e0441260b9f3a7f4d836ed8b7aa36
Rouven Strauss <rouven@lightricks.com>
2016-07-06 18:41:17 +0200

    LTTriangle: Replace by LTPrimitiveTriangle.
    This commit removes the LTTriangle class and uses the C++ implementation
    lt::Triangle instead.

--- LTEngine/LTEngine/LTTriangle.mm
+++ LTEngine/LTEngine/LTTriangle.mm
@@ -1,66 +1,105 @@
-// Copyright (c) 2014 Lightricks. All rights reserved.
+// Copyright (c) 2016 Lightricks. All rights reserved.
 // Created by Rouven Strauss.
 
 #import "LTTriangle.h"
 
 #import "LTGeometry.h"
 
-@interface LTTriangle ()
-
-@property (nonatomic) LTTriangleCorners corners;
-
-@end
-
-@implementation LTTriangle
-
-#pragma mark -
-#pragma mark Initialization
-#pragma mark -
-
-- (instancetype)initWithCorners:(const LTTriangleCorners &)corners {
-  if (self = [super init]) {
-    self.corners = corners;
-    CGPoint direction = self.corners[2] - self.corners[1];
-    BOOL cornersAreGivenInClockwiseOrientation =
-        LTPointLocationRelativeToRay(self.corners[0], self.corners[1], direction) ==
-        LTPointLocationRightOfRay;
-    if (!cornersAreGivenInClockwiseOrientation) {
-      self.corners = LTTriangleCorners{{self.corners[2], self.corners[1], self.corners[0]}};
+static BOOL LTCornersOfNullTriangle(const LTTriangleCorners &corners) {
+  for (CGPoint corner : corners) {
+    if (CGPointIsNull(corner)) {
+      return YES;
     }
   }
-  return self;
+  return NO;
 }
 
-#pragma mark -
-#pragma mark Point inclusion
-#pragma mark -
+static BOOL LTCornersOfPointTriangle(const LTTriangleCorners &corners) {
+  CGPoint firstCorner = corners.front();
 
-- (BOOL)containsPoint:(CGPoint)point {
-  NSUInteger size = self.corners.size();
-  for (NSUInteger i = 0; i < size; i++) {
-    CGPoint origin = self.corners[i];
-    CGPoint direction = self.corners[(i + 1) % size] - origin;
-    if (LTPointLocationRelativeToRay(point, origin, direction) == LTPointLocationLeftOfRay) {
+  for (CGPoint corner : corners) {
+    if (corner != firstCorner) {
       return NO;
     }
   }
   return YES;
 }
 
+static lt::Triangle::Type LTTypeForCorners(const LTTriangleCorners &corners) {
+  if (LTCornersOfNullTriangle(corners)) {
+    return lt::Triangle::Type::Null;
+  }
+
+  if (LTCornersOfPointTriangle(corners)) {
+    return lt::Triangle::Type::Point;
+  }
+
+  if (LTPointsAreCollinear({corners[0], corners[1], corners[2]})) {
+    return lt::Triangle::Type::Collinear;
+  }
+
+  CGPoint direction = corners[1] - corners[0];
+  BOOL clockwise = LTPointLocationRelativeToRay(corners[2], corners[0], direction) ==
+      LTPointLocationRightOfRay;
+  return clockwise ? lt::Triangle::Type::Clockwise : lt::Triangle::Type::CounterClockwise;
+}
+
+static LTTriangleCorners LTUpdatedCornersForType(const LTTriangleCorners &corners,
+                                                 lt::Triangle::Type type) {
+  if (type == lt::Triangle::Type::Null) {
+    return {{CGPointNull, CGPointNull, CGPointNull}};
+  }
+  return corners;
+}
+
+namespace lt {
+
 #pragma mark -
-#pragma mark Properties
+#pragma mark Initialization
 #pragma mark -
 
-- (CGPoint)v0 {
-  return self.corners[0];
+Triangle::Triangle(const LTTriangleCorners &corners) noexcept {
+  _triangleType = LTTypeForCorners(corners);
+  _v = LTUpdatedCornersForType(corners, _triangleType);
 }
 
-- (CGPoint)v1 {
-  return self.corners[1];
+#pragma mark -
+#pragma mark Point containment/relation
+#pragma mark -
+
+BOOL LTNonDegenerateTriangleContainsPoint(CGPoint point, const LTTriangleCorners &corners,
+                                          BOOL clockwise) {
+  LTPointLocation location = clockwise ? LTPointLocationLeftOfRay : LTPointLocationRightOfRay;
+
+  NSUInteger size = corners.size();
+  for (NSUInteger i = 0; i < size; ++i) {
+    CGPoint origin = corners[i];
+    CGPoint direction = corners[(i + 1) % size] - origin;
+    if (LTPointLocationRelativeToRay(point, origin, direction) == location) {
+      return NO;
+    }
+  }
+  return YES;
 }
 
-- (CGPoint)v2 {
-  return self.corners[2];
+static const CGFloat kEpsilon = 1e-6;
+
+BOOL Triangle::containsPoint(CGPoint point) const noexcept {
+  switch (_triangleType) {
+    case Triangle::Type::Null:
+      return NO;
+    case Triangle::Type::Point:
+      return point == _v[0];
+    case Triangle::Type::Collinear: {
+      CGPoints points{_v[0], _v[1], _v[2]};
+      CGPoints convexHull = LTConvexHull(points);
+      return LTDistanceFromEdge(convexHull.front(), convexHull.back(), point) < kEpsilon;
+    }
+    case Triangle::Type::Clockwise:
+      return LTNonDegenerateTriangleContainsPoint(point, _v, YES);
+    case Triangle::Type::CounterClockwise:
+      return LTNonDegenerateTriangleContainsPoint(point, _v, NO);
+  }
 }
 
-@end
+} // namespace lt
