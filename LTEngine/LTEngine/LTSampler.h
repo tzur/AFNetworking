67bfeb19faa21bf01a7294f6f9cfd1e1e2efe4c9
Rouven Strauss <rouven@lightricks.com>
2016-07-06 11:52:42 +0200

    LTSampler: Update API to use LTSampleValues rather than LTSamplerOutput.
    This commit updates the API of LTSampler to use the LTSampleValues
    protocol rather than the LTSamplerOutput protocol.

--- LTEngine/LTEngine/LTSampler.h
+++ LTEngine/LTEngine/LTSampler.h
@@ -2,25 +2,10 @@
 // Created by Rouven Strauss.
 
 #import "LTInterval.h"
-#import "LTParameterizedObject.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
-@protocol LTFloatSet;
-
-/// Protocol which should be implemented by immutable value classes constituting the output of an
-/// \c LTSampler object.
-@protocol LTSamplerOutput <NSObject>
-
-/// Parametric values at which the parameterized object has been sampled. Is empty if no values were
-/// sampled.
-@property (readonly, nonatomic) CGFloats sampledParametricValues;
-
-/// Mapping from \c parameterizationKeys of the sampled parameterized object to sampled values.
-/// \c nil if no values were sampled.
-@property (readonly, nonatomic, nullable) LTParameterizationKeyToValues *mappingOfSampledValues;
-
-@end
+@protocol LTFloatSet, LTParameterizedObject, LTSampleValues;
 
 /// Immutable object sampling a parameterized object according to values from a given \c LTFloatSet,
 /// in a given interval. The parameterized object is provided upon initialization. The output of a
@@ -29,12 +14,12 @@ NS_ASSUME_NONNULL_BEGIN
 @interface LTSampler : NSObject
 
 /// Samples the given \c parameterizedObject according to the values of the given \c discreteSet,
-/// belonging to the given \c interval. Returns a tuple consisting of a) the values at which the
-/// \c parameterizedObject has been sampled, and b) the mapping from keys of the sampled
+/// belonging to the given \c interval. Returns a tuple consisting of the values at which the
+/// \c parameterizedObject has been sampled, and the mapping from keys of the sampled
 /// \c parameterizedObject to the corresponding values.
-- (id<LTSamplerOutput>)samplesFromParameterizedObject:(id<LTParameterizedObject>)parameterizedObject
-                                     usingDiscreteSet:(id<LTFloatSet>)discreteSet
-                                             interval:(const lt::Interval<CGFloat> &)interval;
+- (id<LTSampleValues>)samplesFromParameterizedObject:(id<LTParameterizedObject>)parameterizedObject
+                                    usingDiscreteSet:(id<LTFloatSet>)discreteSet
+                                            interval:(const lt::Interval<CGFloat> &)interval;
 
 @end
 
--- LTEngine/LTEngine/LTSampler.mm
+++ LTEngine/LTEngine/LTSampler.mm
@@ -5,56 +5,9 @@
 
 #import "LTFloatSet.h"
 #import "LTParameterizedObject.h"
+#import "LTSampleValues.h"
 
 NS_ASSUME_NONNULL_BEGIN
-
-/// Object wrapping the output of the \c LTSampler object.
-@interface LTSamplerOutput : NSObject <LTSamplerOutput> {
-  /// Parametric values at which the parameterized object has been sampled.
-  CGFloats _sampledParametricValues;
-}
-
-- (instancetype)init NS_UNAVAILABLE;
-
-/// Initializes with the given \c sampledParametricValues, \c mapping and the given
-/// \c samplingScheme. The given \c sampledParametricValues are the parametric values at which the
-/// parameterized object has been sampled. The given \c mapping constitutes the mapping from keys of
-/// the sampled parameterized object to sampled values. Should be \c nil if no values were sampled.
-/// The given \c samplingScheme describes the desired continuation of future sampling.
-- (instancetype)initWithSampledParametricValues:(const CGFloats &)sampledParametricValues
-                                        mapping:(nullable LTParameterizationKeyToValues *)mapping
-    NS_DESIGNATED_INITIALIZER;
-
-/// Mapping from keys of the sampled parameterized object to sampled values. \c nil if no values
-/// were sampled.
-@property (strong, nonatomic, nullable) LTParameterizationKeyToValues *mappingOfSampledValues;
-
-@end
-
-@implementation LTSamplerOutput
-
-#pragma mark -
-#pragma mark Initialization
-#pragma mark -
-
-- (instancetype)initWithSampledParametricValues:(const CGFloats &)sampledParametricValues
-                                        mapping:(nullable LTParameterizationKeyToValues *)mapping {
-  if (self = [super init]) {
-    _sampledParametricValues = sampledParametricValues;
-    _mappingOfSampledValues = mapping;
-  }
-  return self;
-}
-
-#pragma mark -
-#pragma mark LTSamplerOutput
-#pragma mark -
-
-- (CGFloats)sampledParametricValues {
-  return _sampledParametricValues;
-}
-
-@end
 
 @implementation LTSampler
 
@@ -62,18 +15,18 @@ NS_ASSUME_NONNULL_BEGIN
 #pragma mark Public interface
 #pragma mark -
 
-- (id<LTSamplerOutput>)samplesFromParameterizedObject:(id<LTParameterizedObject>)parameterizedObject
+- (id<LTSampleValues>)samplesFromParameterizedObject:(id<LTParameterizedObject>)parameterizedObject
                                      usingDiscreteSet:(id<LTFloatSet>)discreteSet
                                              interval:(const lt::Interval<CGFloat> &)interval {
   CGFloats parametricValues = [discreteSet discreteValuesInInterval:interval];
 
   if (!parametricValues.size()) {
-    return [[LTSamplerOutput alloc] initWithSampledParametricValues:{} mapping:nil];
+    return [[LTSampleValues alloc] initWithSampledParametricValues:{} mapping:nil];
   }
 
   LTParameterizationKeyToValues *mapping =
       [parameterizedObject mappingForParametricValues:parametricValues];
-  return [[LTSamplerOutput alloc] initWithSampledParametricValues:parametricValues mapping:mapping];
+  return [[LTSampleValues alloc] initWithSampledParametricValues:parametricValues mapping:mapping];
 }
 
 @end
