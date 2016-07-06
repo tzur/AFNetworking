d0e2abf6493e2a630d0679a2702eb140b42c77a9
Rouven Strauss <rouven@lightricks.com>
2016-07-06 11:53:56 +0200

    LTFloatSampler: Initial commit.
    This commit adds LTFloatSampler, an object consecutively sampling a
    given parameterized object at parametric values retrieved from provided
    intervals of a float set.

---
+++ LTEngine/LTEngineTests/LTFloatSetSamplerSpec.mm
@@ -0,0 +1,197 @@
+// Copyright (c) 2016 Lightricks. All rights reserved.
+// Created by Rouven Strauss.
+
+#import "LTFloatSetSampler.h"
+
+#import "LTEasyVectorBoxing.h"
+#import "LTFloatSet.h"
+#import "LTOpenCVExtensions.h"
+#import "LTParameterizationKeyToValues.h"
+#import "LTSampleValues.h"
+#import "LTSamplerTestUtils.h"
+
+@interface LTFloatSetSamplerTestSet : NSObject <LTFloatSet>
+@property (nonatomic) CGFloats values;
+@end
+
+@implementation LTFloatSetSamplerTestSet
+
+- (CGFloats)discreteValuesInInterval:(__unused const lt::Interval<CGFloat> &)interval {
+  return self.values;
+}
+
+@end
+
+SpecBegin(LTFloatSetSamplerModel)
+
+__block LTFloatSetSamplerTestSet *floatSet;
+__block lt::Interval<CGFloat> interval;
+
+beforeEach(^{
+  floatSet = [[LTFloatSetSamplerTestSet alloc] init];
+  interval = lt::Interval<CGFloat>({0, 1}, lt::Interval<CGFloat>::EndpointInclusion::Closed,
+                                   lt::Interval<CGFloat>::EndpointInclusion::Closed);
+});
+
+it(@"should initialize correcty", ^{
+  LTFloatSetSamplerModel *model = [[LTFloatSetSamplerModel alloc] initWithFloatSet:floatSet
+                                                                          interval:interval];
+  expect(model.floatSet).to.equal(floatSet);
+  expect(model.interval == interval).to.beTruthy();
+});
+
+it(@"should return a sampler", ^{
+  LTFloatSetSamplerModel *model = [[LTFloatSetSamplerModel alloc] initWithFloatSet:floatSet
+                                                                          interval:interval];
+  expect([model sampler]).toNot.beNil();
+  expect([model sampler]).to.beKindOf([LTFloatSetSampler class]);
+});
+
+it(@"should return a sampler with the same model", ^{
+  LTFloatSetSamplerModel *model = [[LTFloatSetSamplerModel alloc] initWithFloatSet:floatSet
+                                                                          interval:interval];
+  LTFloatSetSampler *sampler = [model sampler];
+  expect([sampler currentModel]).to.equal(model);
+});
+
+SpecEnd
+
+SpecBegin(LTFloatSetSampler)
+
+__block LTFloatSetSamplerTestSet *floatSet;
+__block lt::Interval<CGFloat> interval;
+__block LTFloatSetSampler *sampler;
+__block LTSamplerTestParameterizedObject *parameterizedObject;
+__block NSOrderedSet<NSString *> *keys;
+
+beforeEach(^{
+  floatSet = [[LTFloatSetSamplerTestSet alloc] init];
+  interval = lt::Interval<CGFloat>({0, 2.5}, lt::Interval<CGFloat>::EndpointInclusion::Closed,
+                                   lt::Interval<CGFloat>::EndpointInclusion::Closed);
+  LTFloatSetSamplerModel *model = [[LTFloatSetSamplerModel alloc] initWithFloatSet:floatSet
+                                                                          interval:interval];
+  sampler = [model sampler];
+  parameterizedObject = [[LTSamplerTestParameterizedObject alloc] init];
+  parameterizedObject.minParametricValue = 0;
+  parameterizedObject.maxParametricValue = 10;
+  keys = [NSOrderedSet orderedSetWithArray:@[@"a"]];
+});
+
+context(@"sampling", ^{
+  it(@"should sample a given parameterized object", ^{
+    CGFloats values = {1, 2, 3};
+    floatSet.values = values;
+
+    parameterizedObject.expectedParametricValues = values;
+    cv::Mat1g matrix = (cv::Mat1g(1, 3) << 7, 8, 9);
+    LTParameterizationKeyToValues *mapping =
+        [[LTParameterizationKeyToValues alloc] initWithKeys:keys valuesPerKey:matrix];
+    parameterizedObject.returnedMapping = mapping;
+
+    id<LTSampleValues> sampleValues = [sampler nextSamplesFromParameterizedObject:parameterizedObject
+                                                            constrainedToInterval:interval];
+    expect($(sampleValues.sampledParametricValues)).to.equal($(values));
+    expect(sampleValues.mappingOfSampledValues).to.equal(mapping);
+  });
+
+  it(@"should consecutively sample a given parameterized object, using closed interval", ^{
+    CGFloats values = {1};
+    floatSet.values = values;
+
+    parameterizedObject.expectedParametricValues = values;
+    cv::Mat1g matrix(1, 1, 7);
+    LTParameterizationKeyToValues *mapping =
+        [[LTParameterizationKeyToValues alloc] initWithKeys:keys valuesPerKey:matrix];
+    parameterizedObject.returnedMapping = mapping;
+
+    lt::Interval<CGFloat> firstInterval({0, 1}, lt::Interval<CGFloat>::EndpointInclusion::Closed,
+                                        lt::Interval<CGFloat>::EndpointInclusion::Closed);
+
+    id<LTSampleValues> sampleValues = [sampler nextSamplesFromParameterizedObject:parameterizedObject
+                                                            constrainedToInterval:firstInterval];
+    expect($(sampleValues.sampledParametricValues)).to.equal($(values));
+    expect(sampleValues.mappingOfSampledValues).to.equal(mapping);
+
+    values = {};
+    floatSet.values = values;
+
+    lt::Interval<CGFloat> secondInterval({1, 2}, lt::Interval<CGFloat>::EndpointInclusion::Open,
+                                         lt::Interval<CGFloat>::EndpointInclusion::Open);
+
+    sampleValues = [sampler nextSamplesFromParameterizedObject:parameterizedObject
+                                         constrainedToInterval:secondInterval];
+    expect(sampleValues.sampledParametricValues.size()).to.equal(0);
+    expect(sampleValues.mappingOfSampledValues).to.beNil();
+  });
+
+  it(@"should consecutively sample a given parameterized object, using open interval", ^{
+    floatSet.values = {};
+
+    lt::Interval<CGFloat> firstInterval({0, 1}, lt::Interval<CGFloat>::EndpointInclusion::Closed,
+                                        lt::Interval<CGFloat>::EndpointInclusion::Open);
+
+    id<LTSampleValues> sampleValues = [sampler nextSamplesFromParameterizedObject:parameterizedObject
+                                                            constrainedToInterval:firstInterval];
+    expect(sampleValues.sampledParametricValues.size()).to.equal(0);
+    expect(sampleValues.mappingOfSampledValues).to.beNil();
+
+    CGFloats values = {1};
+    floatSet.values = values;
+
+    parameterizedObject.expectedParametricValues = values;
+    cv::Mat1g matrix(1, 1, 1);
+    LTParameterizationKeyToValues *mapping =
+    [[LTParameterizationKeyToValues alloc] initWithKeys:keys valuesPerKey:matrix];
+    parameterizedObject.returnedMapping = mapping;
+
+    lt::Interval<CGFloat> secondInterval({1, 2}, lt::Interval<CGFloat>::EndpointInclusion::Closed,
+                                         lt::Interval<CGFloat>::EndpointInclusion::Open);
+
+    sampleValues = [sampler nextSamplesFromParameterizedObject:parameterizedObject
+                                         constrainedToInterval:secondInterval];
+    expect($(sampleValues.sampledParametricValues)).to.equal($(values));
+    expect(sampleValues.mappingOfSampledValues).to.equal(mapping);
+  });
+});
+
+context(@"model", ^{
+  it(@"should provide a correct model after a single sample of a given parameterized object", ^{
+    floatSet.values = {};
+
+    lt::Interval<CGFloat> sampleInterval({0, 1}, lt::Interval<CGFloat>::EndpointInclusion::Closed,
+                                         lt::Interval<CGFloat>::EndpointInclusion::Open);
+
+    [sampler nextSamplesFromParameterizedObject:parameterizedObject
+                          constrainedToInterval:sampleInterval];
+
+    LTFloatSetSamplerModel *model = [sampler currentModel];
+    expect(model.floatSet).to.equal(floatSet);
+    expect(model.interval.min()).to.equal(1);
+    expect(model.interval.max()).to.equal(2.5);
+    expect(model.interval.minEndpointIncluded()).to.beTruthy();
+    expect(model.interval.maxEndpointIncluded()).to.beTruthy();
+  });
+
+  it(@"should provide correct model after consecutive samples of a given parameterized object", ^{
+    floatSet.values = {};
+
+    lt::Interval<CGFloat> firstInterval({0, 1}, lt::Interval<CGFloat>::EndpointInclusion::Closed,
+                                        lt::Interval<CGFloat>::EndpointInclusion::Open);
+    lt::Interval<CGFloat> secondInterval({1, 2}, lt::Interval<CGFloat>::EndpointInclusion::Closed,
+                                         lt::Interval<CGFloat>::EndpointInclusion::Closed);
+
+    [sampler nextSamplesFromParameterizedObject:parameterizedObject
+                          constrainedToInterval:firstInterval];
+    [sampler nextSamplesFromParameterizedObject:parameterizedObject
+                          constrainedToInterval:secondInterval];
+
+    LTFloatSetSamplerModel *model = [sampler currentModel];
+    expect(model.floatSet).to.equal(floatSet);
+    expect(model.interval.min()).to.equal(2);
+    expect(model.interval.max()).to.equal(2.5);
+    expect(model.interval.minEndpointIncluded()).to.beFalsy();
+    expect(model.interval.maxEndpointIncluded()).to.beTruthy();
+  });
+});
+
+SharedExamplesEnd
