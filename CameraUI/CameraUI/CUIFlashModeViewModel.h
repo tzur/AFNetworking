fa67a1695153db4c1dae206a9b1a85935fc0c903
Reuven Siman Tov <rsimantov@lightricks.com>
2016-07-07 11:26:41 +0300

    CUIFlashModeViewModel: initial commit.

---
+++ CameraUI/CameraUI/CUIFlashModeViewModel.h
@@ -0,0 +1,40 @@
+// Copyright (c) 2016 Lightricks. All rights reserved.
+// Created by Reuven Siman Tov.
+
+#import <AVFoundation/AVFoundation.h>
+
+#include "CUIMenuItemViewModel.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@protocol CAMFlashDevice;
+
+/// Object that conforms to the \c CUIMenuItemViewModel protocol for a single \c AVCaptureFlashMode
+/// (e.g. \c AVCaptureFlashModeAuto) of a given \c CAMFlashDevice object.
+///
+/// If the current flash mode doesn't match this object's flash mode, calling \c didTap will trigger
+/// a call to the \c CAMFlashDevice object to change its flash mode to this object's flash mode.
+///
+/// The \c hidden property is always \c NO.
+///
+/// The \c selected property is \c YES if the this object's \c flashMode matches the
+/// \c CAMFlashDevice object's \c currentFlashMode.
+///
+/// The \c subitems property is always \c nil.
+@interface CUIFlashModeViewModel : NSObject <CUIMenuItemViewModel>
+
+- (instancetype)init NS_UNAVAILABLE;
+
+/// Initializes this object with the given \c flashDevice, the \c flashMode that this object
+/// represents, and the \c title and \c iconURL that should be shown for this mode.
+- (instancetype)initWithFlashDevice:(id<CAMFlashDevice>)flashDevice
+                          flashMode:(AVCaptureFlashMode)flashMode
+                              title:(nullable NSString *)title
+                            iconURL:(nullable NSURL *)iconURL;
+
+/// Flash mode represented by this object.
+@property (readonly, nonatomic) AVCaptureFlashMode flashMode;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CameraUI/CameraUI/CUIFlashModeViewModel.mm
@@ -0,0 +1,60 @@
+// Copyright (c) 2016 Lightricks. All rights reserved.
+// Created by Reuven Siman Tov.
+
+#import "CUIFlashModeViewModel.h"
+
+#import <Camera/CAMFlashDevice.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CUIFlashModeViewModel ()
+
+/// \c CAMFlashDevice that this obejct represnts one of its flash modes.
+@property (strong, readonly, nonatomic) id<CAMFlashDevice> flashDevice;
+
+@end
+
+@implementation CUIFlashModeViewModel
+
+@synthesize title = _title;
+@synthesize iconURL = _iconURL;
+@synthesize selected = _selected;
+@synthesize hidden = _hidden;
+@synthesize subitems = _subitems;
+
+- (instancetype)initWithFlashDevice:(id<CAMFlashDevice>)flashDevice
+                          flashMode:(AVCaptureFlashMode)flashMode
+                              title:(nullable NSString *)title
+                            iconURL:(nullable NSURL *)iconURL {
+  LTParameterAssert(flashDevice, @"flashDevice is nil");
+  if (self = [super init]) {
+    _flashDevice = flashDevice;
+    _flashMode = flashMode;
+    _title = title;
+    _iconURL = iconURL;
+    [self setup];
+  }
+  return self;
+}
+
+- (void)setup {
+  _hidden = NO;
+  RAC(self, selected, @NO) = [RACObserve(self, flashDevice.currentFlashMode)
+      map:(id)^NSNumber *(NSNumber *flashMode) {
+        return @(flashMode.integerValue == self.flashMode);
+      }];
+}
+
+#pragma mark -
+#pragma mark CUIMenuItemViewModel
+#pragma mark -
+
+- (void)didTap {
+  if (self.flashMode != self.flashDevice.currentFlashMode) {
+    [[self.flashDevice setFlashMode:self.flashMode] subscribeCompleted:^{}];
+  }
+}
+
+@end
+
+NS_ASSUME_NONNULL_END
