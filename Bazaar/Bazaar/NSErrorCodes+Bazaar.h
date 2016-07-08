d583246cdd55a78462c254ae2c222406be319a1a
Daniel Lahyani <dlahyani@lightricks.com>
2016-07-08 15:32:13 +0300

    Bazaar: Add Bazaar specific error codes.
    NSErrorCodes+LTKit: Allocate Bazaar product ID.
    NSErrorCodes+Bazaar: Define Bazaar specific error codes.

---
+++ Bazaar/Bazaar/NSErrorCodes+Bazaar.h
@@ -0,0 +1,20 @@
+// Copyright (c) 2016 Lightricks. All rights reserved.
+// Created by Daniel Lahyani.
+
+#import <LTKit/NSErrorCodes+LTKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+/// Product ID.
+NS_ENUM(NSInteger) {
+  /// Product ID of Bazaar.
+  BazaarErrorCodeProductID = 10
+};
+
+/// All error codes produced by Bazaar.
+LTErrorCodesDeclare(BazaarErrorCodeProductID,
+  /// The process of application receipt validation has failed.
+  BZRErrorCodeReceiptValidationFailed
+);
+
+NS_ASSUME_NONNULL_END
---
+++ Bazaar/Bazaar/NSErrorCodes+Bazaar.mm
@@ -0,0 +1,14 @@
+// Copyright (c) 2016 Lightricks. All rights reserved.
+// Created by Daniel Lahyani.
+
+#import "NSErrorCodes+Bazaar.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+/// All error codes produced by Bazaar.
+LTErrorCodesImplement(BazaarErrorCodeProductID,
+  /// The process of application receipt validation has failed.
+  BZRErrorCodeReceiptValidationFailed
+);
+
+NS_ASSUME_NONNULL_END
