ba19be61ead03fbce36004041b2aded511765cfb
Rouven Strauss <rouven@lightricks.com>
2016-07-10 15:51:03 +0300

    DVNTextureMappingStageConfiguration+TextureAtlas: Initial commit.
    This commit adds DVNTextureMappingStageConfiguration+TextureAtlas, a
    category augmenting the DVNTextureMappingStageConfiguration class with
    convenience functionality to create a configuration of the texture
    mapping stage of the DVNPipeline using a texture atlas.

---
+++ DaVinci/DaVinci/DVNTextureMappingStageConfiguration+TextureAtlas.h
@@ -0,0 +1,23 @@
+// Copyright (c) 2016 Lightricks. All rights reserved.
+// Created by Rouven Strauss.
+
+#import "DVNTextureMappingStageConfiguration.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@class LTTextureAtlas;
+
+/// Category augmenting the \c DVNTextureMappingStageConfiguration class with convenience
+/// functionality to create a configuration of the texture mapping stage of the \c DVNPipeline using
+/// a texture atlas.
+@interface DVNTextureMappingStageConfiguration (TextureAtlas)
+
+/// Returns a new \c DVNTextureMappingStageConfiguration using the texture of the given
+/// \c textureAtlas and a \c DVNRandomTexCoordProviderModel as \c texCoordProviderModel. The
+/// \c randomState of the \c DVNRandomTexCoordProviderModel is random and the \c textureMapQuads are
+/// constructed from the \c areas of the given \c textureAtlas.
++ (instancetype)configurationFromTextureAtlas:(LTTextureAtlas *)textureAtlas;
+
+@end
+
+NS_ASSUME_NONNULL_END
