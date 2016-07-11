a1042203d9a75867ba4c63c7a00c843fdf93ad8a
Rouven Strauss <rouven@lightricks.com>
2016-07-11 11:46:52 +0300

    LTSplineControlPoint+AttributeKeys: Initial commit.
    This commit adds LTSplineControlPoint+AttributeKeys, a category
    augmenting the LTSplineControlPoint class with the ability to provide
    strings commonly used as keys of the attributes dictionary.

---
+++ LTEngine/LTEngine/LTSplineControlPoint+AttributeKeys.mm
@@ -0,0 +1,20 @@
+// Copyright (c) 2016 Lightricks. All rights reserved.
+// Created by Rouven Strauss.
+
+#import "LTSplineControlPoint+AttributeKeys.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@implementation LTSplineControlPoint (AttributeKeys)
+
++ (NSString *)keyForRadius {
+  return @"radius";
+}
+
++ (NSString *)keyForForce {
+  return @"force";
+}
+
+@end
+
+NS_ASSUME_NONNULL_END
