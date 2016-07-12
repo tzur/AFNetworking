dbeb05996ae4e9d3172f01abe3857d871af1e9fc
Rouven Strauss <rouven@lightricks.com>
2016-07-12 17:42:27 +0300

    DVNAttributeProvider: Forward declare correct protocol.
    This commit updates the DVNAttributeProvider to use dvn::GeometryValues
    than id<DVNQuadProviderOutput> objects.

--- DaVinci/DaVinci/DVNAttributeProvider.h
+++ DaVinci/DaVinci/DVNAttributeProvider.h
@@ -3,11 +3,13 @@
 
 #import <LTEngine/LTQuad.h>
 
+#import "DVNGeometryValues.h"
+
 NS_ASSUME_NONNULL_BEGIN
 
 @class LTAttributeData, LTGPUStruct;
 
-@protocol DVNAttributeProviderModel, DVNQuadProviderOutput;
+@protocol DVNAttributeProviderModel;
 
 /// Protocol which should be implemented by objects providing additional attributes for given
 /// quadrilateral geometry. The implementing objects might be stateful. To ensure an immutable
@@ -19,10 +21,10 @@ NS_ASSUME_NONNULL_BEGIN
 /// associated models.
 - (instancetype)init NS_UNAVAILABLE;
 
-/// Returns attribute data to be associated with the \c quads of the given \c output. The returned
+/// Returns attribute data to be associated with the \c quads of the given \c values. The returned
 /// attribute data is formatted for draw calls with the \c LTDrawingContextDrawModeTriangles
 /// setting, with vertex order <tt>(v0, v1, v2, v0, v2, v3)</tt> per quad.
-- (LTAttributeData *)attributeDataFromQuadProviderOutput:(id<DVNQuadProviderOutput>)output;
+- (LTAttributeData *)attributeDataFromGeometryValues:(dvn::GeometryValues)values;
 
 /// Returns an immutable model representing the current state of this object.
 - (id<DVNAttributeProviderModel>)currentModel;
