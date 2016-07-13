4d877912ecebb21474ff77e2263fc416ba9e6de1
Rouven Strauss <rouven@lightricks.com>
2016-07-13 12:33:44 +0300

    DVNTextureMappingStageConfiguration: Correctly implement isEqual:/hash.
    This commit updates DVNTextureMappingStageConfiguration to correctly
    implement the isEqual: and hash methods.

--- DaVinci/DaVinciTests/DVNTextureMappingStageConfigurationSpec.mm
+++ DaVinci/DaVinciTests/DVNTextureMappingStageConfigurationSpec.mm
@@ -3,22 +3,51 @@
 
 #import "DVNTextureMappingStageConfiguration.h"
 
-#import <LTEngine/LTTexture.h>
+#import <LTEngine/LTTexture+Factory.h>
+#import <LTKitTests/LTEqualityExamples.h>
 
 #import "DVNTexCoordProvider.h"
 
 SpecBegin(DVNTextureMappingStageConfiguration)
 
+static id<DVNTexCoordProviderModel> const kModel =
+    OCMProtocolMock(@protocol(DVNTexCoordProviderModel));
+
+__block id textureMock;
+
+beforeEach(^{
+  textureMock = OCMClassMock([LTTexture class]);
+});
+
 context(@"initialization", ^{
   it(@"should initialize correctly", ^{
-    id<DVNTexCoordProviderModel> modelMock = OCMProtocolMock(@protocol(DVNTexCoordProviderModel));
-    LTTexture *textureMock = OCMClassMock([LTTexture class]);
     DVNTextureMappingStageConfiguration *configuration =
-        [[DVNTextureMappingStageConfiguration alloc] initWithTexCoordProviderModel:modelMock
+        [[DVNTextureMappingStageConfiguration alloc] initWithTexCoordProviderModel:kModel
                                                                            texture:textureMock];
-    expect(configuration.texCoordProviderModel).to.equal(modelMock);
+    expect(configuration.texCoordProviderModel).to.equal(kModel);
     expect(configuration.texture).to.equal(textureMock);
   });
+});
+
+itShouldBehaveLike(kLTEqualityExamples, ^{
+  DVNTextureMappingStageConfiguration *configuration =
+      [[DVNTextureMappingStageConfiguration alloc] initWithTexCoordProviderModel:kModel
+                                                                         texture:textureMock];
+  DVNTextureMappingStageConfiguration *equalConfiguration =
+      [[DVNTextureMappingStageConfiguration alloc] initWithTexCoordProviderModel:kModel
+                                                                         texture:textureMock];
+  DVNTextureMappingStageConfiguration *differentConfiguration =
+      [[DVNTextureMappingStageConfiguration alloc]
+       initWithTexCoordProviderModel:OCMProtocolMock(@protocol(DVNTexCoordProviderModel))
+       texture:textureMock];
+  DVNTextureMappingStageConfiguration *anotherDifferentConfiguration =
+      [[DVNTextureMappingStageConfiguration alloc]
+       initWithTexCoordProviderModel:kModel texture:OCMClassMock([LTTexture class])];
+  return @{
+    kLTEqualityExamplesObject: configuration,
+    kLTEqualityExamplesEqualObject: equalConfiguration,
+    kLTEqualityExamplesDifferentObjects: @[differentConfiguration, anotherDifferentConfiguration]
+  };
 });
 
 SpecEnd
