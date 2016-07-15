a00028702f0f4dd00b4b4cfa399b0f4e04c7e161
Daniel Lahyani <dlahyani@lightricks.com>
2016-07-16 01:36:55 +0300

    Bazaar: Fix line breaking in recently added files.

--- Bazaar/Bazaar/BZRValidatricksReceiptValidationResponse.h
+++ Bazaar/Bazaar/BZRValidatricksReceiptValidationResponse.h
@@ -5,8 +5,8 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface BZRValidatricksReceiptValidationResponse :
-    BZRReceiptValidationResponse <MTLJSONSerializing>
+@interface BZRValidatricksReceiptValidationResponse : BZRReceiptValidationResponse
+    <MTLJSONSerializing>
 @end
 
 NS_ASSUME_NONNULL_END
