0a502ba0a19512c8aa9da15e804ae617ddfef709
Daniel Lahyani <dlahyani@lightricks.com>
2016-07-25 13:36:45 +0300

    FNDAppDelegate: Declare the window property in the header file.
    This change is introduced since the test host crashed with error saying
    "unrecognized selector sent to instance" for the `setWindow` selector,
    when the test host used for hosting tests of a project outside of
    Foundations workspace.

--- Foundations/FoundationsTestHost/FNDAppDelegate.h
+++ Foundations/FoundationsTestHost/FNDAppDelegate.h
@@ -5,6 +5,10 @@ NS_ASSUME_NONNULL_BEGIN
 
 /// Foundations Test Host application delegate.
 @interface FNDAppDelegate : UIResponder <UIApplicationDelegate>
+
+/// Main application window.
+@property (strong, nonatomic, nullable) UIWindow *window;
+
 @end
 
 NS_ASSUME_NONNULL_END
