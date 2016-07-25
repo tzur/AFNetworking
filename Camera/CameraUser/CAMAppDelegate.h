d08789a9dbf44a9ebe0750d0f76f2b1480c4d9ed
Daniel Lahyani <dlahyani@lightricks.com>
2016-07-25 17:12:35 +0300

    Foundations: Port Camera to FoundationsTestHost.

--- Camera/CameraUser/CAMAppDelegate.h
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2016 Lightricks. All rights reserved.
-// Created by Nir Bruner.
-
-#import <UIKit/UIKit.h>
-
-@interface CAMAppDelegate : UIResponder<UIApplicationDelegate>
-@property (strong, nonatomic) UIWindow *window;
-@end
