323339b58bcc8116529d995e87b6be1896c5e9b6
Daniel Lahyani <dlahyani@lightricks.com>
2016-07-25 17:15:08 +0300

    Foundations: Port Wireframes to FoundationsTestHost.

--- Wireframes/WireframesUser/WFAppDelegate.h
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2016 Lightricks. All rights reserved.
-// Created by Yaron Inger.
-
-#import <UIKit/UIKit.h>
-
-@interface WFAppDelegate : UIResponder <UIApplicationDelegate>
-@property (strong, nonatomic) UIWindow *window;
-@end
