03450b2a28281711bb614dcbbad78e185b0f6732
Lior Bar <lior@lightricks.com>
2016-07-29 11:48:38 +0300

    CUISingleChoiceMenuViewController: change view to view controller.

--- CameraUI/CameraUI/CUISingleChoiceMenuViewController.h
+++ CameraUI/CameraUI/CUISingleChoiceMenuViewController.h
@@ -8,18 +8,20 @@ NS_ASSUME_NONNULL_BEGIN
 /// Presents a left-to-right scrollable menu. The menu is comprised of menu item cells, only one
 /// menu item can be selected at any given time. This menu does not support submenus, model submenus
 /// are ignored.
-@interface CUISingleChoiceMenuView : UIView
+@interface CUISingleChoiceMenuViewController : UIViewController
 
-- (instancetype)initWithFrame:(CGRect)frame NS_UNAVAILABLE;
+- (instancetype)init NS_UNAVAILABLE;
 
 - (instancetype)initWithCoder:(NSCoder *)aDecoder NS_UNAVAILABLE;
 
-/// Initializes this menu with a frame, view model and <tt>cellClass. cellClass</tt> is used as the
-/// menu cells. \c cellClass must inherit from \c UICollectionViewCell and conform to the
+- (instancetype)initWithNibName:(nullable NSString *)nibName
+                         bundle:(nullable NSBundle *)bundle NS_UNAVAILABLE;
+
+/// Initializes this menu with a view model and <tt>cellClass. cellClass</tt> is used as the menu
+/// cells. \c cellClass must inherit from \c UICollectionViewCell and conform to the
 /// \c CUIMutableMenuItemView protocol.
-- (instancetype)initWithFrame:(CGRect)frame
-                menuViewModel:(CUISingleChoiceMenuViewModel *)menuViewModel
-                    cellClass:(Class)cellClass NS_DESIGNATED_INITIALIZER;
+- (instancetype)initWithMenuViewModel:(CUISingleChoiceMenuViewModel *)menuViewModel
+                            cellClass:(Class)cellClass NS_DESIGNATED_INITIALIZER;
 
 /// Number of menu items per row. This number must be strictly positive but can have a fraction
 /// part, meaning the last item will only be partially visible. Default value is 5.5.
