fb1b533bd76afdc88436bf84516531bdd826da28
Yaron Inger <yaron@lightricks.com>
2016-08-10 16:06:48 +0300

    LTEngine: upgrade to OpenCV 3.

--- LTEngine/LTEngine/LTOpenCVCore.h
+++ LTEngine/LTEngine/LTOpenCVCore.h
@@ -1,7 +1,6 @@
 // Copyright (c) 2015 Lightricks. All rights reserved.
 // Created by Yaron Inger.
 
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wnon-virtual-dtor"
-#import <opencv2/core/core.hpp>
-#pragma clang diagnostic pop
+#ifdef __cplusplus
+  #import <opencv2/core/core.hpp>
+#endif
