d14d48ca7a853334be394f3051c419f07750f296
byohay <ben@lightricks.com>
2016-08-28 17:51:18 +0300

    Bazaar: split BZRPaymentQueue into three protocols.

---
+++ Bazaar/Bazaar/BZRPaymentsPaymentQueue.h
@@ -0,0 +1,42 @@
+// Copyright (c) 2016 Lightricks. All rights reserved.
+// Created by Ben Yohay.
+
+NS_ASSUME_NONNULL_BEGIN
+
+@protocol BZRPaymentQueuePaymentsDelegate;
+
+/// \c BZRPaymentsPaymentQueue provides an interface for making payments and sending updates
+/// regarding payment transactions.
+@protocol BZRPaymentsPaymentQueue <NSObject>
+
+/// Adds a payment request. Will initiate sending updates of transaction associated with \c payment
+/// to \c paymentsDelegate.
+- (void)addPayment:(SKPayment *)payment;
+
+/// Delegate that will be receiving updates regarding payment transactions.
+@property (weak, nonatomic, nullable) id<BZRPaymentQueuePaymentsDelegate> paymentsDelegate;
+
+@end
+
+/// Protocol for delegates that want to receive updates regarding payment transactions.
+///
+/// @note Payment transactions are added to the payment queue in response to payment requests made
+/// by calling \c -[BZRPaymentQueue addPayment:].
+@protocol BZRPaymentQueuePaymentsDelegate <NSObject>
+
+/// Invoked when \c paymentQueue informs its delegates that the state of the payment transactions in
+/// \c transactions was updated.
+- (void)paymentQueue:(id<BZRPaymentsPaymentQueue>)paymentQueue
+    paymentTransactionsUpdated:(NSArray<SKPaymentTransaction *> *)transactions;
+
+@optional
+
+/// Invoked when \c paymentQueue informs its delegates that \c transactions were finished and
+/// removed from the queue. \c transactions will contain only payment transactions and no
+/// restoration transactions.
+- (void)paymentQueue:(id<BZRPaymentsPaymentQueue>)paymentQueue
+    paymentTransactionsRemoved:(NSArray<SKPaymentTransaction *> *)transactions;
+
+@end
+
+NS_ASSUME_NONNULL_END
