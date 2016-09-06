f10f7e91a4531cef88dc1cee4082af503b1f4d02
Barak Yoresh <barak@lightricks.com>
2016-09-06 17:44:17 +0300

    LTSpectaKeyWindowViewHook: Spin run loop after access. When adding view controllers' views to the key window, the view controllers are retained by UIKit for a short period. To overcome this we spin the run loop for a short duration, enabling the release of the retained references.

--- LTEngine/LTEngineTests/LTSpectaKeyWindowViewHook.mm
+++ LTEngine/LTEngineTests/LTSpectaKeyWindowViewHook.mm
@@ -19,8 +19,10 @@ void LTAddViewToWindow(UIView *view) {
 @implementation LTSpectaKeyWindowViewHook
 
 static UIView *_keyWindowView;
+static BOOL _didAccessView;
 
 + (void)beforeEach {
+  _didAccessView = NO;
   _keyWindowView = [[UIView alloc] initWithFrame:CGRectZero];
   [[UIApplication sharedApplication].keyWindow addSubview:_keyWindowView];
 }
@@ -28,9 +30,18 @@ static UIView *_keyWindowView;
 + (void)afterEach {
   [_keyWindowView removeFromSuperview];
   _keyWindowView = nil;
+
+  // Skip the current run loop to allow for release of objects retained by UIKit. This is required
+  // when the added view is the view of a \c UIViewController, as UIKit retains said view controller
+  // until the runloop is spun for a single time.
+  if (_didAccessView) {
+    NSDate *date = [NSDate dateWithTimeIntervalSinceNow:0.001];
+    [[NSRunLoop currentRunLoop] runUntilDate:date];
+  }
 }
 
 + (UIView *)keyWindowView {
+  _didAccessView = YES;
   return _keyWindowView;
 }
 
