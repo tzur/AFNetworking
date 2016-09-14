708e2997f5a1e66d85f4daffe92ebaefb6842bed
byohay <ben@lightricks.com>
2016-09-14 12:52:50 +0300

    BZRProductsInfoProvider: document each property as KVO compliant.

--- Bazaar/Bazaar/BZRProductsInfoProvider.h
+++ Bazaar/Bazaar/BZRProductsInfoProvider.h
@@ -6,35 +6,36 @@ NS_ASSUME_NONNULL_BEGIN
 @class BZRReceiptSubscriptionInfo;
 
 /// Interface for providing the list of purchased products, products with downloaded content, and
-/// subscription information. All properties are KVO-compliant.
+/// subscription information. 
 @protocol BZRProductsInfoProvider <NSObject>
 
 /// Returns the path to the content of the product specified by \c productIdentifier or \c nil if
 /// the product has no content.
 - (nullable LTPath *)pathToContentOfProduct:(NSString *)productIdentifier;
 
-/// List of products that were purchased by the user as in-app purchases.
+/// List of products that were purchased by the user as in-app purchases. KVO-compliant.
 @property (readonly, nonatomic) NSSet<NSString *> *purchasedProducts;
 
 /// List of products that were acquired by the user via subscription on the current device.
+/// KVO-compliant.
 @property (readonly, nonatomic) NSSet<NSString *> *acquiredViaSubscriptionProducts;
 
 /// List of products that were acquired by the user. This includes in-app purchases on all devices
-/// and products that the user acquired via subscription on the current device.
+/// and products that the user acquired via subscription on the current device. KVO-compliant.
 ///
 /// @note The user isn't necessarily allowed to use the products returned by this method, for
 /// example if he acquired a product via a subscription that expired. One should check
 /// \c productsAllowedToBeUsed to get the list of products that the user is allowed to use.
 @property (readonly, nonatomic) NSSet<NSString *> *acquiredProducts;
 
-/// List of products that the user is allowed to use.
+/// List of products that the user is allowed to use. KVO-compliant.
 @property (readonly, nonatomic) NSSet<NSString *> *allowedProducts;
 
 /// List of products that their content is already available on the device. Products
-/// without content will be in the list as well.
+/// without content will be in the list as well. KVO-compliant.
 @property (readonly, nonatomic) NSSet<NSString *> *downloadedContentProducts;
 
-/// Subscription information of the user.
+/// Subscription information of the user. KVO-compliant.
 @property (readonly, nonatomic, nullable) BZRReceiptSubscriptionInfo *subscriptionInfo;
 
 @end
