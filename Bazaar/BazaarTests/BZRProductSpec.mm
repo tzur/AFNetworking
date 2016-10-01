31dd243c7edaa64ecc56ccb1f0f680f5c93a01cd
Daniel Lahyani <dlahyani@lightricks.com>
2016-10-01 14:56:33 +0300

    BZRProduct: Add 'isSubscribersOnly' property to 'optionalPropertyKeys'.

--- Bazaar/BazaarTests/BZRProductSpec.mm
+++ Bazaar/BazaarTests/BZRProductSpec.mm
@@ -36,12 +36,10 @@ context(@"initialization", ^{
   it(@"should correctly specifiy optional properties", ^{
     NSSet<NSString *> *optionalProperties = [BZRProduct optionalPropertyKeys];
 
-    expect(nullableProperties.count).to.equal(2);
-    expect(nullableProperties).to.contain(@instanceKeypath(BZRProduct, contentFetcherParameters));
-    expect(nullableProperties).to.contain(@instanceKeypath(BZRProduct, priceInfo));
-    expect(optionalProperties.count).to.equal(2);
+    expect(optionalProperties.count).to.equal(3);
     expect(optionalProperties).to.contain(@instanceKeypath(BZRProduct, contentFetcherParameters));
     expect(optionalProperties).to.contain(@instanceKeypath(BZRProduct, priceInfo));
+    expect(optionalProperties).to.contain(@instanceKeypath(BZRProduct, isSubscribersOnly));
   });
 });
 
