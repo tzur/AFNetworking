c3c840f1c19276d136350f33786fd6843b55faec
Shachar Langbeheim <shachar@lightricks.com>
2016-10-26 10:54:28 +0300

    LTEventBusObserver: filter events according to filter block.

--- LTKit/LTKit/LTEventBusObserver.h
+++ LTKit/LTKit/LTEventBusObserver.h
@@ -15,12 +15,15 @@ NS_ASSUME_NONNULL_BEGIN
 
 - (instancetype)init NS_UNAVAILABLE;
 
+/// Block used for filtering events in an event bus observer.
+typedef BOOL (^LTEventBusObserverFilterBlock)(NSObject *);
+
 /// Initializes this logger that listens to events sent on \c eventBus and saves them to
-/// \c messageContainer. All entries will be logged, except those of a class listed in
-/// \c ignoredEventsClasses.
+/// \c messageContainer. All entries will be logged, except those the given \c eventFilter returns
+/// \c NO when received as parameter.
 - (instancetype)initWithMessageContainer:(id<LTMessageContainer>)messageContainer
                                 eventBus:(LTEventBus *)eventBus
-                    ignoredEventsClasses:(NSArray<Class> *)ignoredEventsClasses
+                             eventFilter:(LTEventBusObserverFilterBlock)eventFilter
     NS_DESIGNATED_INITIALIZER;
 @end
 
