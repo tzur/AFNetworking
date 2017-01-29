9d1ef681cf809f5f0dcaa3f180b1364553cf6070
Amit Yitzhack <ayitzhack@lightricks.com>
2017-01-29 08:55:29 +0200

    DVNBlendModel: Initial commit.

---
+++ DaVinci/DaVinci/DVNBlendMode.h
@@ -0,0 +1,21 @@
+// Copyright (c) 2017 Lightricks. All rights reserved.
+// Created by Amit Yitzhack.
+
+NS_ASSUME_NONNULL_BEGIN
+
+/// Types of blend modes. See https://en.wikipedia.org/wiki/Blend_modes and
+/// http://www.w3.org/TR/SVGCompositing/ for more details.
+LTEnumDeclare(NSUInteger, DVNBlendMode,
+  DVNBlendModeNormal,
+  DVNBlendModeDarken,
+  DVNBlendModeMultiply,
+  DVNBlendModeHardLight,
+  DVNBlendModeSoftLight,
+  DVNBlendModeLighten,
+  DVNBlendModeScreen,
+  DVNBlendModeColorBurn,
+  DVNBlendModeOverlay,
+  DVNBlendModeAddition
+);
+
+NS_ASSUME_NONNULL_END
