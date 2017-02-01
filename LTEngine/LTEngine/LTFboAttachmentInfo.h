e393de46493b2aa4af3cb6954b1f755d9ef1803d
Michael Kimyagarov <mkimi@lightricks.com>
2017-02-01 09:31:54 +0200

    LTFboAttachmentInfo: initial commit.
    LTFboAttachmentInfo holds Fbo's attachment information.

---
+++ LTEngine/LTEngine/LTFboAttachmentInfo.h
@@ -0,0 +1,32 @@
+// Copyright (c) 2016 Lightricks. All rights reserved.
+// Created by Michael Kimyagarov.
+
+NS_ASSUME_NONNULL_BEGIN
+
+@class LTTexture, LTRenderbuffer;
+
+@protocol LTFboAttachable;
+
+/// Describes how to attach an \c id<LTFboAttachable> to a framebuffer by providing the attachable
+/// and its level.
+@interface LTFboAttachmentInfo : NSObject
+
+- (instancetype)init NS_UNAVAILABLE;
+
+/// Initializes with the given \c attachable.
++ (instancetype)withAttachable:(id<LTFboAttachable>)attachable;
+
+/// Initializes with the given \c attachable and the given \c level.
+///
+/// @note \c attachable.attachableType must be \c LTFboAttachableTypeTexture2D.
++ (instancetype)withAttachable:(id<LTFboAttachable>)attachable level:(NSUInteger)level;
+
+/// Framebuffer's attachable.
+@property (readonly, nonatomic) id<LTFboAttachable> attachable;
+
+/// Mipmap level of an attachments. Only valid for textures and \c 0 otherwise.
+@property (readonly, nonatomic) NSUInteger level;
+
+@end
+
+NS_ASSUME_NONNULL_END
