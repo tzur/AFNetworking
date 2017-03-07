c51f206e05c9e61746eb4ba3d0aaec5d25ee162a
Bmantl <boris@lightricks.com>
2017-03-07 13:11:35 +0200

    Intelligence: project setup.

---
+++ Intelligence/Intelligence/INTIntelligence.h
@@ -0,0 +1,9 @@
+// Copyright (c) 2017 Lightricks. All rights reserved.
+// Created by Boris Talesnik.
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface INTIntelligence : NSObject
+@end
+
+NS_ASSUME_NONNULL_END
