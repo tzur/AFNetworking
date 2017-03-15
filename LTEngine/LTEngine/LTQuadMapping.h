540ebaeafafd7fc1a06099b88bd04d1b07ddb422
Rouven Strauss <rouven@lightricks.com>
2017-03-15 15:11:14 +0200

    LTQuadMapping: Add methods using lt::Quad and update docs.
    This commit augments the LTQuadMapping file with the same methods as the
    existing ones but using lt::Quad. This is desirable since the LTQuad
    class is currently restricted to corners provided in clockwise
    direction, a restriction which is obstructive when attempting to render
    to back-facing quads. In the scope of this PR, the docs of the file are
    updated as well. Note that no existing functionality is changed by this
    commit.

--- LTEngine/LTEngine/LTQuadMapping.h
+++ LTEngine/LTEngine/LTQuadMapping.h
@@ -1,26 +1,84 @@
 // Copyright (c) 2014 Lightricks. All rights reserved.
 // Created by Rouven Strauss.
 
-@class LTQuad;
+#import "LTQuad.h"
 
-/// Returns a 3x3 matrix which maps the canonical [0,1] x [0,1] rect to a rect in 3D space
-/// corresponding to a normalized version of the given \c quad. The normalized quad is defined by
-/// placing the given \c quad inside the rect [0,textureSize.width] x [0,textureSize.height] and
-/// normalizing to [0,1] x [0,1]. Note that projection to the XY plane requires division by the z
-/// coordinate.
-GLKMatrix3 LTTextureMatrix3ForQuad(LTQuad *quad, CGSize textureSize);
+#pragma mark -
+#pragma mark Canonical Square to Normalized Convex Quad in Homogeneous 3D space
+#pragma mark -
 
-/// Returns a 3x3 matrix which maps the canonical [0,1] x [0,1] rect to a rect in 3D space
-/// corresponding to the given \c quad. Note that projection to the XY plane requires division by
-/// the z coordinate.
-GLKMatrix3 LTMatrix3ForQuad(LTQuad *quad);
+/// Returns a <tt>3x3</tt> matrix which maps the canonical <tt>[0, 1] x [0, 1]</tt> square to a rect
+/// in 3D space whose orthographic projection onto the XY plane corresponds to a normalized version
+/// of the given \c quad. The normalized quad is defined by scaling the given \c quad by vector
+/// <tt>(1 / size.width, 1 / size.height)</tt> around \c CGPointZero.
+///
+/// @important The projection to the XY plane requires division by the \c z coordinate.
+GLKMatrix3 LTMatrix3ForNormalizedQuad(const lt::Quad &quad, CGSize size);
 
-/// Returns a 3x3 matrix which maps the given \c quad to the rect [\c 0, \c textureSize.width] x
-/// [\c 0, \c textureSize.height]. Returns the zero matrix if \c LTMatrix3ForQuad(quad) is not
-/// invertible.  Note that projection to the XY plane requires division by the z coordinate.
-GLKMatrix3 LTInvertedTextureMatrix3ForQuad(LTQuad *quad, CGSize textureSize);
+/// Returns a <tt>3x3</tt> matrix which maps the canonical <tt>[0, 1] x [0, 1]</tt> square to a rect
+/// in 3D space whose orthographic projection onto the XY plane corresponds to a normalized version
+/// of the given \c quad. The normalized quad is defined by scaling the given \c quad by vector
+/// <tt>(1 / size.width, 1 / size.height)</tt> around \c CGPointZero.
+///
+/// @important The projection to the XY plane requires division by the \c z coordinate.
+static inline GLKMatrix3 LTTextureMatrix3ForQuad(LTQuad *quad, CGSize size) {
+  return LTMatrix3ForNormalizedQuad(quad.quad, size);
+}
 
-/// Returns a 4x4 matrix which maps the canonical [0,1] x [0,1] rect to the given \c quad. The z
-/// coordinate is kept as is. Is used, e.g., for the \c modelview matrix in vertex shaders. Note
-/// that projection to the XY plane requires division by the w coordinate.
-GLKMatrix4 LTMatrix4ForQuad(LTQuad *quad);
+#pragma mark -
+#pragma mark Canonical Square to Convex Quad in Homogeneous 3D space
+#pragma mark -
+
+/// Returns a <tt>3x3</tt> matrix which maps the canonical <tt>[0, 1] x [0, 1]</tt> square to a rect
+/// in 3D space whose orthographic projection onto the XY plane corresponds to the given \c quad.
+///
+/// @important The projection to the XY plane requires division by the \c z coordinate.
+GLKMatrix3 LTMatrix3ForQuad(const lt::Quad &quad);
+
+/// Returns a <tt>3x3</tt> matrix which maps the canonical <tt>[0, 1] x [0, 1]</tt> square to a rect
+/// in 3D space whose orthographic projection onto the XY plane corresponds to the given \c quad.
+///
+/// @important The projection to the XY plane requires division by the \c z coordinate.
+static inline GLKMatrix3 LTMatrix3ForQuad(LTQuad *quad) {
+  return LTMatrix3ForQuad(quad.quad);
+}
+
+#pragma mark -
+#pragma mark Convex Quad to Rect in Homogeneous 3D space
+#pragma mark -
+
+/// Returns a <tt>3x3</tt> matrix which maps the given \c quad to the rect
+/// <tt>[0, size.width] x [0, size.height]. Returns the zero matrix if \c LTMatrix3ForQuad(quad) is
+/// not invertible.
+///
+/// @important The projection to the XY plane requires division by the \c z coordinate.
+GLKMatrix3 LTInvertedMatrix3ForQuad(const lt::Quad &quad, CGSize size);
+
+/// Returns a <tt>3x3</tt> matrix which maps the given \c quad to the rect
+/// <tt>[0, size.width] x [0, size.height]. Returns the zero matrix if \c LTMatrix3ForQuad(quad) is
+/// not invertible.
+///
+/// @important The projection to the XY plane requires division by the \c z coordinate.
+static inline GLKMatrix3 LTInvertedTextureMatrix3ForQuad(LTQuad *quad, CGSize size) {
+  return LTInvertedMatrix3ForQuad(quad.quad, size);
+}
+
+#pragma mark -
+#pragma mark Canonical Square to Quad in Homogeneous 4D space
+#pragma mark -
+
+/// Returns a <tt>4x4</tt> matrix which maps the canonical <tt>[0, 1] x [0, 1]</tt> square to the
+/// given \c quad. The \c z coordinate is kept as is. Can be used, e.g., for the \c modelview matrix
+/// in vertex shaders.
+///
+/// @important The projection to the XY plane requires division by the \c w coordinate.
+GLKMatrix4 LTMatrix4ForQuad(const lt::Quad &quad);
+
+/// Returns a <tt>4x4</tt> matrix which maps the canonical <tt>[0, 1] x [0, 1]</tt> square to the
+/// given \c quad. The \c z coordinate is kept as is. Can be used, e.g., for the \c modelview matrix
+/// in vertex shaders.
+///
+/// @important The projection to the XY plane requires division by the \c w coordinate.
+static inline GLKMatrix4 LTMatrix4ForQuad(LTQuad *quad) {
+  return LTMatrix4ForQuad(quad.quad);
+}
--- LTEngine/LTEngineTests/LTQuadMappingSpec.mm
+++ LTEngine/LTEngineTests/LTQuadMappingSpec.mm
@@ -19,7 +19,7 @@ static const GLKVector3 kTopRight = GLKVector3Make(1, 0, 1);
 static const GLKVector3 kBottomLeft = GLKVector3Make(0, 1, 1);
 static const GLKVector3 kBottomRight = GLKVector3Make(1, 1, 1);
 
-static const CGSize kTextureSize = CGSizeMake(8, 12);
+static const CGSize kSize = CGSizeMake(8, 12);
 
 static const CGPoint kQuadTopLeft = CGPointMake(0, 0);
 static const CGPoint kQuadTopRight = CGPointMake(1, 0);
@@ -27,83 +27,164 @@ static const CGPoint kQuadBottomLeft = CGPointMake(0, 1);
 static const CGPoint kQuadBottomRight = CGPointMake(1, 0.9);
 
 __block LTQuad *quad;
+__block GLKVector3 expectedTopLeft;
+__block GLKVector3 expectedTopRight;
+__block GLKVector3 expectedBottomRight;
+__block GLKVector3 expectedBottomLeft;
 
 beforeAll(^{
   LTQuadCorners corners{{kQuadTopLeft, kQuadTopRight, kQuadBottomRight, kQuadBottomLeft}};
   quad = [[LTQuad alloc] initWithCorners:corners];
 });
 
-it(@"should compute the correct three-dimensional matrix for texture mapping", ^{
-  GLKMatrix3 matrix = LTTextureMatrix3ForQuad(quad, kTextureSize);
+context(@"mapping of canonical square to normalized quad", ^{
+  beforeEach(^{
+    expectedTopLeft = GLKVector3Make(kQuadTopLeft.x / kSize.width,
+                                     kQuadTopLeft.y / kSize.height, 1);
+    expectedTopRight = GLKVector3Make(kQuadTopRight.x / kSize.width,
+                                      kQuadTopRight.y / kSize.height, 1);
+    expectedBottomLeft = GLKVector3Make(kQuadBottomLeft.x / kSize.width,
+                                        kQuadBottomLeft.y / kSize.height, 1);
+    expectedBottomRight = GLKVector3Make(kQuadBottomRight.x / kSize.width,
+                                         kQuadBottomRight.y / kSize.height, 1);
+  });
 
-  GLKVector3 expectedTopLeft = GLKVector3Make(kQuadTopLeft.x / kTextureSize.width,
-                                              kQuadTopLeft.y / kTextureSize.height, 1);
-  GLKVector3 expectedTopRight = GLKVector3Make(kQuadTopRight.x / kTextureSize.width,
-                                               kQuadTopRight.y / kTextureSize.height, 1);
-  GLKVector3 expectedBottomLeft = GLKVector3Make(kQuadBottomLeft.x / kTextureSize.width,
-                                                 kQuadBottomLeft.y / kTextureSize.height, 1);
-  GLKVector3 expectedBottomRight = GLKVector3Make(kQuadBottomRight.x / kTextureSize.width,
-                                                  kQuadBottomRight.y / kTextureSize.height, 1);
+  it(@"should compute the correct three-dimensional matrix for texture mapping", ^{
+    GLKMatrix3 matrix = LTTextureMatrix3ForQuad(quad, kSize);
 
-  GLKVector3 actualTopLeft = GLKMatrix3MultiplyVector3(matrix, kTopLeft);
-  actualTopLeft = actualTopLeft / actualTopLeft.z;
-  GLKVector3 actualTopRight = GLKMatrix3MultiplyVector3(matrix, kTopRight);
-  actualTopRight = actualTopRight / actualTopRight.z;
-  GLKVector3 actualBottomLeft = GLKMatrix3MultiplyVector3(matrix, kBottomLeft);
-  actualBottomLeft = actualBottomLeft / actualBottomLeft.z;
-  GLKVector3 actualBottomRight = GLKMatrix3MultiplyVector3(matrix, kBottomRight);
-  actualBottomRight = actualBottomRight / actualBottomRight.z;
+    GLKVector3 actualTopLeft = GLKMatrix3MultiplyVector3(matrix, kTopLeft);
+    actualTopLeft = actualTopLeft / actualTopLeft.z;
+    GLKVector3 actualTopRight = GLKMatrix3MultiplyVector3(matrix, kTopRight);
+    actualTopRight = actualTopRight / actualTopRight.z;
+    GLKVector3 actualBottomLeft = GLKMatrix3MultiplyVector3(matrix, kBottomLeft);
+    actualBottomLeft = actualBottomLeft / actualBottomLeft.z;
+    GLKVector3 actualBottomRight = GLKMatrix3MultiplyVector3(matrix, kBottomRight);
+    actualBottomRight = actualBottomRight / actualBottomRight.z;
 
-  expect(GLKVector3Length(actualTopLeft - expectedTopLeft)).to.beCloseToWithin(0, kEpsilon);
-  expect(GLKVector3Length(actualTopRight - expectedTopRight)).to.beCloseToWithin(0, kEpsilon);
-  expect(GLKVector3Length(actualBottomLeft - expectedBottomLeft)).to.beCloseToWithin(0, kEpsilon);
-  expect(GLKVector3Length(actualBottomRight - expectedBottomRight)).to.beCloseToWithin(0, kEpsilon);
+    expect(GLKVector3Length(actualTopLeft - expectedTopLeft)).to.beCloseToWithin(0, kEpsilon);
+    expect(GLKVector3Length(actualTopRight - expectedTopRight)).to.beCloseToWithin(0, kEpsilon);
+    expect(GLKVector3Length(actualBottomLeft - expectedBottomLeft)).to.beCloseToWithin(0, kEpsilon);
+    expect(GLKVector3Length(actualBottomRight - expectedBottomRight))
+        .to.beCloseToWithin(0, kEpsilon);
+  });
+
+  it(@"should compute three-dimensional matrix mapping canonical square to normalized quad", ^{
+    GLKMatrix3 matrix = LTMatrix3ForNormalizedQuad(quad.quad, kSize);
+
+    GLKVector3 actualTopLeft = GLKMatrix3MultiplyVector3(matrix, kTopLeft);
+    actualTopLeft = actualTopLeft / actualTopLeft.z;
+    GLKVector3 actualTopRight = GLKMatrix3MultiplyVector3(matrix, kTopRight);
+    actualTopRight = actualTopRight / actualTopRight.z;
+    GLKVector3 actualBottomLeft = GLKMatrix3MultiplyVector3(matrix, kBottomLeft);
+    actualBottomLeft = actualBottomLeft / actualBottomLeft.z;
+    GLKVector3 actualBottomRight = GLKMatrix3MultiplyVector3(matrix, kBottomRight);
+    actualBottomRight = actualBottomRight / actualBottomRight.z;
+
+    expect(GLKVector3Length(actualTopLeft - expectedTopLeft)).to.beCloseToWithin(0, kEpsilon);
+    expect(GLKVector3Length(actualTopRight - expectedTopRight)).to.beCloseToWithin(0, kEpsilon);
+    expect(GLKVector3Length(actualBottomLeft - expectedBottomLeft)).to.beCloseToWithin(0, kEpsilon);
+    expect(GLKVector3Length(actualBottomRight - expectedBottomRight))
+        .to.beCloseToWithin(0, kEpsilon);
+  });
 });
 
-it(@"should compute the correct three-dimensional matrix", ^{
-  GLKMatrix3 matrix = LTMatrix3ForQuad(quad);
+context(@"mapping of canonical square to quad", ^{
+  beforeEach(^{
+    expectedTopRight = GLKVector3Make(kQuadTopRight.x, kQuadTopRight.y, 1);
+    expectedTopLeft = GLKVector3Make(kQuadTopLeft.x, kQuadTopLeft.y, 1);
+    expectedBottomLeft = GLKVector3Make(kQuadBottomLeft.x, kQuadBottomLeft.y, 1);
+    expectedBottomRight = GLKVector3Make(kQuadBottomRight.x, kQuadBottomRight.y, 1);
+  });
 
-  GLKVector3 expectedTopRight = GLKVector3Make(kQuadTopRight.x, kQuadTopRight.y, 1);
-  GLKVector3 expectedTopLeft = GLKVector3Make(kQuadTopLeft.x, kQuadTopLeft.y, 1);
-  GLKVector3 expectedBottomLeft = GLKVector3Make(kQuadBottomLeft.x, kQuadBottomLeft.y, 1);
-  GLKVector3 expectedBottomRight = GLKVector3Make(kQuadBottomRight.x, kQuadBottomRight.y, 1);
+  it(@"should compute the correct three-dimensional matrix", ^{
+    GLKMatrix3 matrix = LTMatrix3ForQuad(quad);
 
-  GLKVector3 actualTopLeft = GLKMatrix3MultiplyVector3(matrix, kTopLeft);
-  actualTopLeft = actualTopLeft / actualTopLeft.z;
-  GLKVector3 actualTopRight = GLKMatrix3MultiplyVector3(matrix, kTopRight);
-  actualTopRight = actualTopRight / actualTopRight.z;
-  GLKVector3 actualBottomLeft = GLKMatrix3MultiplyVector3(matrix, kBottomLeft);
-  actualBottomLeft = actualBottomLeft / actualBottomLeft.z;
-  GLKVector3 actualBottomRight = GLKMatrix3MultiplyVector3(matrix, kBottomRight);
-  actualBottomRight = actualBottomRight / actualBottomRight.z;
+    GLKVector3 actualTopLeft = GLKMatrix3MultiplyVector3(matrix, kTopLeft);
+    actualTopLeft = actualTopLeft / actualTopLeft.z;
+    GLKVector3 actualTopRight = GLKMatrix3MultiplyVector3(matrix, kTopRight);
+    actualTopRight = actualTopRight / actualTopRight.z;
+    GLKVector3 actualBottomLeft = GLKMatrix3MultiplyVector3(matrix, kBottomLeft);
+    actualBottomLeft = actualBottomLeft / actualBottomLeft.z;
+    GLKVector3 actualBottomRight = GLKMatrix3MultiplyVector3(matrix, kBottomRight);
+    actualBottomRight = actualBottomRight / actualBottomRight.z;
 
-  expect(GLKVector3Length(actualTopLeft - expectedTopLeft)).to.beCloseToWithin(0, kEpsilon);
-  expect(GLKVector3Length(actualTopRight - expectedTopRight)).to.beCloseToWithin(0, kEpsilon);
-  expect(GLKVector3Length(actualBottomLeft - expectedBottomLeft)).to.beCloseToWithin(0, kEpsilon);
-  expect(GLKVector3Length(actualBottomRight - expectedBottomRight)).to.beCloseToWithin(0, kEpsilon);
+    expect(GLKVector3Length(actualTopLeft - expectedTopLeft)).to.beCloseToWithin(0, kEpsilon);
+    expect(GLKVector3Length(actualTopRight - expectedTopRight)).to.beCloseToWithin(0, kEpsilon);
+    expect(GLKVector3Length(actualBottomLeft - expectedBottomLeft)).to.beCloseToWithin(0, kEpsilon);
+    expect(GLKVector3Length(actualBottomRight - expectedBottomRight))
+        .to.beCloseToWithin(0, kEpsilon);
+  });
+
+  it(@"should compute three-dimensional matrix mapping canonical square to quad", ^{
+    GLKMatrix3 matrix = LTMatrix3ForQuad(quad.quad);
+
+    GLKVector3 actualTopLeft = GLKMatrix3MultiplyVector3(matrix, kTopLeft);
+    actualTopLeft = actualTopLeft / actualTopLeft.z;
+    GLKVector3 actualTopRight = GLKMatrix3MultiplyVector3(matrix, kTopRight);
+    actualTopRight = actualTopRight / actualTopRight.z;
+    GLKVector3 actualBottomLeft = GLKMatrix3MultiplyVector3(matrix, kBottomLeft);
+    actualBottomLeft = actualBottomLeft / actualBottomLeft.z;
+    GLKVector3 actualBottomRight = GLKMatrix3MultiplyVector3(matrix, kBottomRight);
+    actualBottomRight = actualBottomRight / actualBottomRight.z;
+
+    expect(GLKVector3Length(actualTopLeft - expectedTopLeft)).to.beCloseToWithin(0, kEpsilon);
+    expect(GLKVector3Length(actualTopRight - expectedTopRight)).to.beCloseToWithin(0, kEpsilon);
+    expect(GLKVector3Length(actualBottomLeft - expectedBottomLeft)).to.beCloseToWithin(0, kEpsilon);
+    expect(GLKVector3Length(actualBottomRight - expectedBottomRight))
+        .to.beCloseToWithin(0, kEpsilon);
+  });
 });
 
-it(@"should compute the correct four-dimensional matrix", ^{
-  GLKMatrix4 matrix = LTMatrix4ForQuad(quad);
+context(@"mapping of canonical square to quad", ^{
+  __block GLKVector4 expectedTopRight;
+  __block GLKVector4 expectedTopLeft;
+  __block GLKVector4 expectedBottomLeft;
+  __block GLKVector4 expectedBottomRight;
 
-  GLKVector4 expectedTopRight = GLKVector4Make(kQuadTopRight.x, kQuadTopRight.y, 0, 1);
-  GLKVector4 expectedTopLeft = GLKVector4Make(kQuadTopLeft.x, kQuadTopLeft.y, 0, 1);
-  GLKVector4 expectedBottomLeft = GLKVector4Make(kQuadBottomLeft.x, kQuadBottomLeft.y, 0, 1);
-  GLKVector4 expectedBottomRight = GLKVector4Make(kQuadBottomRight.x, kQuadBottomRight.y, 0, 1);
+  beforeEach(^{
+    expectedTopRight = GLKVector4Make(kQuadTopRight.x, kQuadTopRight.y, 0, 1);
+    expectedTopLeft = GLKVector4Make(kQuadTopLeft.x, kQuadTopLeft.y, 0, 1);
+    expectedBottomLeft = GLKVector4Make(kQuadBottomLeft.x, kQuadBottomLeft.y, 0, 1);
+    expectedBottomRight = GLKVector4Make(kQuadBottomRight.x, kQuadBottomRight.y, 0, 1);
+  });
 
-  GLKVector4 actualTopLeft = GLKMatrix4MultiplyVector4(matrix, LTGLKVector3To4(kTopLeft));
-  actualTopLeft = actualTopLeft / actualTopLeft.w;
-  GLKVector4 actualTopRight = GLKMatrix4MultiplyVector4(matrix, LTGLKVector3To4(kTopRight));
-  actualTopRight = actualTopRight / actualTopRight.w;
-  GLKVector4 actualBottomLeft = GLKMatrix4MultiplyVector4(matrix, LTGLKVector3To4(kBottomLeft));
-  actualBottomLeft = actualBottomLeft / actualBottomLeft.w;
-  GLKVector4 actualBottomRight = GLKMatrix4MultiplyVector4(matrix, LTGLKVector3To4(kBottomRight));
-  actualBottomRight = actualBottomRight / actualBottomRight.w;
+  it(@"should compute the correct four-dimensional matrix", ^{
+    GLKMatrix4 matrix = LTMatrix4ForQuad(quad);
 
-  expect(GLKVector4Length(actualTopLeft - expectedTopLeft)).to.beCloseToWithin(0, kEpsilon);
-  expect(GLKVector4Length(actualTopRight - expectedTopRight)).to.beCloseToWithin(0, kEpsilon);
-  expect(GLKVector4Length(actualBottomLeft - expectedBottomLeft)).to.beCloseToWithin(0, kEpsilon);
-  expect(GLKVector4Length(actualBottomRight - expectedBottomRight)).to.beCloseToWithin(0, kEpsilon);
+    GLKVector4 actualTopLeft = GLKMatrix4MultiplyVector4(matrix, LTGLKVector3To4(kTopLeft));
+    actualTopLeft = actualTopLeft / actualTopLeft.w;
+    GLKVector4 actualTopRight = GLKMatrix4MultiplyVector4(matrix, LTGLKVector3To4(kTopRight));
+    actualTopRight = actualTopRight / actualTopRight.w;
+    GLKVector4 actualBottomLeft = GLKMatrix4MultiplyVector4(matrix, LTGLKVector3To4(kBottomLeft));
+    actualBottomLeft = actualBottomLeft / actualBottomLeft.w;
+    GLKVector4 actualBottomRight = GLKMatrix4MultiplyVector4(matrix, LTGLKVector3To4(kBottomRight));
+    actualBottomRight = actualBottomRight / actualBottomRight.w;
+
+    expect(GLKVector4Length(actualTopLeft - expectedTopLeft)).to.beCloseToWithin(0, kEpsilon);
+    expect(GLKVector4Length(actualTopRight - expectedTopRight)).to.beCloseToWithin(0, kEpsilon);
+    expect(GLKVector4Length(actualBottomLeft - expectedBottomLeft)).to.beCloseToWithin(0, kEpsilon);
+    expect(GLKVector4Length(actualBottomRight - expectedBottomRight))
+        .to.beCloseToWithin(0, kEpsilon);
+  });
+
+  it(@"should compute four-dimensional matrix mapping canonical square to quad", ^{
+    GLKMatrix4 matrix = LTMatrix4ForQuad(quad.quad);
+
+    GLKVector4 actualTopLeft = GLKMatrix4MultiplyVector4(matrix, LTGLKVector3To4(kTopLeft));
+    actualTopLeft = actualTopLeft / actualTopLeft.w;
+    GLKVector4 actualTopRight = GLKMatrix4MultiplyVector4(matrix, LTGLKVector3To4(kTopRight));
+    actualTopRight = actualTopRight / actualTopRight.w;
+    GLKVector4 actualBottomLeft = GLKMatrix4MultiplyVector4(matrix, LTGLKVector3To4(kBottomLeft));
+    actualBottomLeft = actualBottomLeft / actualBottomLeft.w;
+    GLKVector4 actualBottomRight = GLKMatrix4MultiplyVector4(matrix, LTGLKVector3To4(kBottomRight));
+    actualBottomRight = actualBottomRight / actualBottomRight.w;
+
+    expect(GLKVector4Length(actualTopLeft - expectedTopLeft)).to.beCloseToWithin(0, kEpsilon);
+    expect(GLKVector4Length(actualTopRight - expectedTopRight)).to.beCloseToWithin(0, kEpsilon);
+    expect(GLKVector4Length(actualBottomLeft - expectedBottomLeft)).to.beCloseToWithin(0, kEpsilon);
+    expect(GLKVector4Length(actualBottomRight - expectedBottomRight))
+        .to.beCloseToWithin(0, kEpsilon);
+  });
 });
 
 SpecEnd
