12327cb2073c1dc9bfb7012e509b6706b15755b8
Barak Weiss <bweiss@lightricks.com>
2017-05-22 00:13:09 +0300

    Laboratory: Add errors.

---
+++ Laboratory/Laboratory/NSError+Laboratory.h
@@ -0,0 +1,58 @@
+// Copyright (c) 2017 Lightricks. All rights reserved.
+// Created by Barak Weiss.
+
+#import "NSErrorCodes+Laboratory.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSError (Laboratory)
+
+/// Creates an error with Lightricks' domain, given error code and the associated experiment.
++ (instancetype)lab_errorWithCode:(NSInteger)code
+             associatedExperiment:(NSString *)associatedExperiment;
+
+/// Creates an error with Lightricks' domain, given error code, the associated experiment and
+/// underlying error.
++ (instancetype)lab_errorWithCode:(NSInteger)code
+             associatedExperiment:(NSString *)associatedExperiment
+                  underlyingError:(nullable NSError *)underlyingError;
+
+/// Creates an error with Lightricks' domain, given error code and the associated variant.
++ (instancetype)lab_errorWithCode:(NSInteger)code
+                associatedVariant:(NSString *)associatedVariant;
+
+/// Creates an error with Lightricks' domain, given error code, the associated variant and
+/// underlying error.
++ (instancetype)lab_errorWithCode:(NSInteger)code
+                associatedVariant:(NSString *)associatedVariant
+                  underlyingError:(nullable NSError *)underlyingError;
+
+/// Creates an error with Lightricks' domain, given error code and the associated assignment key.
++ (instancetype)lab_errorWithCode:(NSInteger)code
+          associatedAssignmentKey:(NSString *)associatedAssignmentKey;
+
+/// Creates an error with Lightricks' domain, given error code, the associated experiment and the
+/// associated variant.
++ (instancetype)lab_errorWithCode:(NSInteger)code
+             associatedExperiment:(NSString *)associatedExperiment
+                associatedVariant:(NSString *)associatedVariant;
+
+/// Creates an error with Lightricks' domain, given error code, the associated experiment, the
+/// associated variant and underlying error.
++ (instancetype)lab_errorWithCode:(NSInteger)code
+             associatedExperiment:(NSString *)associatedExperiment
+                associatedVariant:(NSString *)associatedVariant
+                  underlyingError:(nullable NSError *)underlyingError;
+
+/// Expriment associated with the error.
+@property (readonly, nonatomic, nullable) NSString *lab_associatedExperiment;
+
+/// Variant associated with the error.
+@property (readonly, nonatomic, nullable) NSString *lab_associatedVariant;
+
+/// Assignment key associated with the error.
+@property (readonly, nonatomic, nullable) NSString *lab_associatedAssignmentKey;
+
+@end
+
+NS_ASSUME_NONNULL_END
