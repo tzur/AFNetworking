7f90a13ea95a29ac3a2c14ad646a0d1409924098
Neria Saada <neria@lightricks.com>
2017-05-24 14:11:40 +0300

    BZRProductContentMultiFetcher: Change to BZRCompositeContentFetcher.

---
+++ Bazaar/Bazaar/BZRCompositeContentFetcher.mm
@@ -0,0 +1,59 @@
+// Copyright (c) 2017 Lightricks. All rights reserved.
+// Created by Neria Saada.
+
+#import "BZRCompositeContentFetcher.h"
+
+#import "BZRLocalContentFetcher.h"
+#import "BZRProduct.h"
+#import "BZRProductContentFetcher.h"
+#import "BZRProductContentManager.h"
+#import "NSErrorCodes+Bazaar.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BZRCompositeContentFetcher ()
+
+/// Dictionary that maps content fetcher name to content fetcher class.
+@property (readonly, nonatomic) BZRContentFetchersDictionary *contentFetchers;
+
+@end
+
+@implementation BZRCompositeContentFetcher
+
+- (instancetype)init {
+  BZRLocalContentFetcher *localContentFetcher = [[BZRLocalContentFetcher alloc] init];
+
+  return [self initWithContentFetchers:@{
+    NSStringFromClass([localContentFetcher class]): localContentFetcher
+  }];
+}
+
+- (instancetype)initWithContentFetchers:(BZRContentFetchersDictionary *)contentFetchers {
+  if (self = [super init]) {
+    _contentFetchers = contentFetchers;
+  }
+  return self;
+}
+
+- (RACSignal *)fetchProductContent:(BZRProduct *)product {
+  id<BZRProductContentFetcher> contentFetcher =
+      self.contentFetchers[product.contentFetcherParameters.type];
+
+  if (!contentFetcher) {
+    NSError *error = [NSError lt_errorWithCode:BZRErrorCodeProductContentFetcherNotRegistered
+                                   description:@"Content fetcher of type %@ is not registered.",
+                                               product.contentFetcherParameters.type];
+    return [RACSignal error:error];
+  }
+
+  return [contentFetcher fetchProductContent:product];
+}
+
+- (nullable NSBundle *)contentBundleForProduct:(BZRProduct *)product {
+  return [self.contentFetchers[product.contentFetcherParameters.type]
+          contentBundleForProduct:product];
+}
+
+@end
+
+NS_ASSUME_NONNULL_END
