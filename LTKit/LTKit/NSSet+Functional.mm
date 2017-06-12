692df4927ab09c20a30de617ffb8fc1d95673fac
Barak Weiss <bweiss@lightricks.com>
2017-06-12 17:15:25 +0300

    NSSet+Functional: Initial commit.

---
+++ LTKit/LTKit/NSSet+Functional.mm
@@ -0,0 +1,30 @@
+// Copyright (c) 2017 Lightricks. All rights reserved.
+// Created by Barak Weiss.
+
+#import "NSSet+Functional.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@implementation NSSet (Functional)
+
+- (NSSet *)lt_map:(LTSetMapBlock)block {
+  LTParameterAssert(block);
+
+  auto mapped = [NSMutableSet setWithCapacity:self.count];
+  for (id object in self) {
+    [mapped addObject:block(object)];
+  }
+  return mapped;
+}
+
+- (instancetype)lt_filter:(LTSetFilterBlock)block {
+  LTParameterAssert(block);
+
+  return [self objectsPassingTest:^BOOL(id obj, BOOL *) {
+    return block(obj);
+  }];
+}
+
+@end
+
+NS_ASSUME_NONNULL_END
