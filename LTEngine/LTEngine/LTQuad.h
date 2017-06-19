ae3b783148131ce6efe497ee551272446d1be99c
Rouven Strauss <rouven@lightricks.com>
2017-06-19 12:42:24 +0300

    LTQuad: Adhere to code style guidelines.

--- LTEngine/LTEngine/LTQuad.h
+++ LTEngine/LTEngine/LTQuad.h
@@ -1,8 +1,8 @@
 // Copyright (c) 2014 Lightricks. All rights reserved.
 // Created by Rouven Strauss.
 
-#import <experimental/optional>
 #import <LTKit/LTHashExtensions.h>
+#import <experimental/optional>
 
 #import "LTGeometry.h"
 
