d98279de3355c1369fdff41a60eed78a396ed541
Barak Weiss <bweiss@lightricks.com>
2017-06-28 16:03:39 +0300

    LABAssignmentsSource: Conform to NSSecureCoding.

--- Laboratory/Laboratory/LABAssignmentsSource.mm
+++ Laboratory/Laboratory/LABAssignmentsSource.mm
@@ -3,9 +3,36 @@
 
 #import "LABAssignmentsSource.h"
 
+#import <LTKit/LTKeyPathCoding.h>
+
 NS_ASSUME_NONNULL_BEGIN
 
 @implementation LABVariant
+
+- (nullable instancetype)initWithCoder:(NSCoder *)aDecoder {
+  NSString * _Nullable name = [aDecoder decodeObjectOfClass:[NSString class]
+                                                     forKey:@instanceKeypath(LABVariant, name)];
+  NSDictionary * _Nullable assignments =
+      [aDecoder decodeObjectOfClass:[NSDictionary class]
+                             forKey:@instanceKeypath(LABVariant, assignments)];
+  NSString * _Nullable experiment =
+      [aDecoder decodeObjectOfClass:[NSString class]
+                             forKey:@instanceKeypath(LABVariant, experiment)];
+
+  if (![name isKindOfClass:NSString.class] ||
+      ![assignments isKindOfClass:NSDictionary.class] ||
+      ![experiment isKindOfClass:NSString.class]) {
+    return nil;
+  }
+
+  for (NSString *key in assignments) {
+    if (![key isKindOfClass:NSString.class]) {
+      return nil;
+    }
+  }
+
+  return [self initWithName:name assignments:assignments experiment:experiment];
+}
 
 - (instancetype)initWithName:(NSString *)name
                  assignments:(NSDictionary<NSString *, id> *)assignments
@@ -16,6 +43,16 @@ NS_ASSUME_NONNULL_BEGIN
     _experiment = experiment;
   }
   return self;
+}
+
+- (void)encodeWithCoder:(NSCoder *)aCoder {
+  [aCoder encodeObject:self.name forKey:@instanceKeypath(LABVariant, name)];
+  [aCoder encodeObject:self.assignments forKey:@instanceKeypath(LABVariant, assignments)];
+  [aCoder encodeObject:self.experiment forKey:@instanceKeypath(LABVariant, experiment)];
+}
+
++ (BOOL)supportsSecureCoding {
+  return YES;
 }
 
 @end
