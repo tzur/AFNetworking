a09fdeab08727ea5666a91ffc8634c142f282224
Yaron Inger <yaron@lightricks.com>
2017-07-05 14:42:40 +0300

    LTNullability: initial commit.

---
+++ LTKit/LTKitTests/LTNullabilitySpec.mm
@@ -0,0 +1,41 @@
+// Copyright (c) 2017 Lightricks. All rights reserved.
+// Created by Yaron Inger.
+
+#import "LTNullability.h"
+
+SpecBegin(LTNullability)
+
+it(@"should return original ObjC object if not nil", ^{
+  NSString * _Nullable object = @"foo";
+  expect(nn(object)).to.equal(object);
+});
+
+it(@"should return default ObjC object if nil", ^{
+  NSString * _Nullable object = nil;
+  NSString * defaultValue = @"bar";
+  expect(nn(object, defaultValue)).to.equal(defaultValue);
+});
+
+it(@"should return original pointer if not nil", ^{
+  int object = 7;
+  int * _Nullable ptr = &object;
+
+  expect(nn(ptr)).to.equal(ptr);
+});
+
+it(@"should return default pointer if nil", ^{
+  int object = 7;
+  int * _Nullable ptr = nil;
+  int *defaultValue = &object;
+
+  expect(nn(ptr, defaultValue)).to.equal(&object);
+});
+
+it(@"should return original array if not nil", ^{
+  char string[4] = "foo";
+
+  auto result = nn(string);
+  expect(result).to.equal(string);
+});
+
+SpecEnd
