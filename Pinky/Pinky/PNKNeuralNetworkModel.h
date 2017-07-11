ec3d1e1f0cb8119bc8af21fdf723f588870ed658
Ofir Bibi <ofir@lightricks.com>
2017-07-11 14:54:58 +0300

    PNKNeuralNetworkModel: initial commit.

---
+++ Pinky/Pinky/PNKNeuralNetworkModel.h
@@ -0,0 +1,44 @@
+// Copyright (c) 2017 Lightricks. All rights reserved.
+// Created by Ofir Bibi.
+
+#import "PNKNeuralNetworkOperationsModel.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+namespace pnk {
+
+#pragma mark -
+#pragma mark Neural Network model
+#pragma mark -
+
+/// Model representing a neural network. This model provides access to the various parameters of the
+/// neural network. This model supports a single preprocessing operation per network. Other
+/// operations are not limited in number. All operations should have distinct names to differentiate
+/// them not only within operation type but also between operation types.
+struct NeuralNetworkModel {
+  /// Model of the preprocessing done by the network. When the network does not perform
+  /// preprocessing, this value is \c nil.
+  std::unique_ptr<ImageChannelScalingModel> preprocessingModel;
+
+  /// Models of convolution kernels in the network, mapped by name.
+  std::unordered_map<std::string, ConvolutionKernelModel> convolutionKernels;
+
+  /// Models of pooling kernels in the network, mapped by name.
+  std::unordered_map<std::string, PoolingKernelModel> poolingKernels;
+
+  /// Models of affine (fully connected) kernels in the network, mapped by name.
+  std::unordered_map<std::string, AffineKernelModel> affineKernels;
+
+  /// Models of activation kernels in the network, mapped by name.
+  std::unordered_map<std::string, ActivationKernelModel> activationKernels;
+
+  /// Models of normalization kernels in the network, mapped by name.
+  std::unordered_map<std::string, NormalizationKernelModel> normalizationKernels;
+
+  /// Metadata of the network model, mapped by name.
+  std::unordered_map<std::string, std::string> networkMetadata;
+};
+
+} // namespace pnk
+
+NS_ASSUME_NONNULL_END
