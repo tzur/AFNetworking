1fc1366eb904597f087d9b1bea60c5fc0c3be9d5
Ofir Bibi <ofir@lightricks.com>
2017-07-21 00:01:08 +0300

    PNKComputeState: initial commit.

---
+++ Pinky/Pinky/PNKComputeState.h
@@ -0,0 +1,29 @@
+// Copyright (c) 2017 Lightricks. All rights reserved.
+// Created by Ofir Bibi.
+
+NS_ASSUME_NONNULL_BEGIN
+
+/// Creates a compute pipeline state for a given function.
+///
+/// @param functionName Name of the kernel to compile into the pipeline state.
+///
+/// @note This creates a non-specialized kernel function. Raises \c NSInternalInconsistencyException
+/// if the kernel with \c functionName has constants.
+id<MTLComputePipelineState> PNKCreateComputeState(id<MTLDevice> device, NSString *functionName);
+
+/// MTLFunctionConstantValues is not supported in simulator for Xcode 8. Solved in Xcode 9.
+#if PNK_USE_MPS
+
+/// Creates a compute pipeline state for a given function specialized for the given constants.
+///
+/// @param functionName Name of the kernel to compile into the pipeline state.
+/// @param constants Constant values to set in the kernel. Used to compile a specialized version of
+/// the kernel.
+///
+/// @note This creates a specialized kernel function.
+id<MTLComputePipelineState> PNKCreateComputeStateWithConstants(id<MTLDevice> device,
+    NSString *functionName, MTLFunctionConstantValues *constants);
+
+#endif // PNK_USE_MPS
+
+NS_ASSUME_NONNULL_END
