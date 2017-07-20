a882536919bf83d8c22997b5d2d86752417aeffc
Ofir Bibi <ofir@lightricks.com>
2017-07-20 08:59:44 +0300

    EXPMatchers+equalMTLSize: initial commit.

---
+++ Pinky/PinkyDeviceTests/EXPMatchers+equalMTLSize.h
@@ -0,0 +1,4 @@
+// Copyright (c) 2017 Lightricks. All rights reserved.
+// Created by Ofir Bibi.
+
+EXPMatcherInterface(equalMTLSize, (NSValue *expectedValue));
---
+++ Pinky/PinkyDeviceTests/EXPMatchers+equalMTLSize.mm
@@ -0,0 +1,48 @@
+// Copyright (c) 2017 Lightricks. All rights reserved.
+// Created by Ofir Bibi.
+
+#import "EXPMatchers+equalMTLSize.h"
+
+#import <Expecta/NSValue+Expecta.h>
+
+#import "LTEasyBoxing+Pinky.h"
+
+EXPMatcherImplementationBegin(equalMTLSize, (NSValue *expectedValue)) {
+  __block NSString *prerequisiteErrorMessage;
+
+  prerequisite(^BOOL{
+    if (strcmp([expectedValue _EXP_objCType], @encode(MTLSize))) {
+      prerequisiteErrorMessage = @"Size value is not MTLSize";
+    }
+    return !prerequisiteErrorMessage;
+  });
+
+  match(^BOOL{
+    MTLSize expected = [expectedValue MTLSizeValue];
+    MTLSize result = [actual MTLSizeValue];
+    return expected.width == result.width && expected.height == result.height &&
+        expected.depth == result.depth;
+  });
+
+  failureMessageForTo(^NSString *{
+    if (prerequisiteErrorMessage) {
+      return prerequisiteErrorMessage;
+    }
+    MTLSize expected = [expectedValue MTLSizeValue];
+    MTLSize result = [actual MTLSizeValue];
+    return [NSString stringWithFormat:@"expected (%lu, %lu, %lu), got (%lu, %lu, %lu)",
+            expected.width, expected.height, expected.depth, result.width, result.height,
+            result.depth];
+  });
+
+  failureMessageForNotTo(^NSString *{
+    if (prerequisiteErrorMessage) {
+      return prerequisiteErrorMessage;
+    }
+    MTLSize expected = [expectedValue MTLSizeValue];
+    return [NSString stringWithFormat:@"got unwanted expected value (%lu, %lu, %lu)",
+            expected.width, expected.height, expected.depth];
+  });
+}
+
+EXPMatcherImplementationEnd
