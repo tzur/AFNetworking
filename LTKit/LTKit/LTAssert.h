524d12a7d927b0b2ea914970a00f98ff35c8c5eb
Yaron Inger <yaron@lightricks.com>
2017-07-31 18:02:42 +0300

    LTAssert: log error only in Debug configurations.
    If an assert has failed, the code is already going to crash, and since
    the exception contains the logged message there's no need to duplicate
    it. In Debug configurations, however, this may be useful, since the
    exception message is not always very visible.
    
    Another good reason for removing the logging in non-Debug configurations
    is the binary size. Since assertions are very popular across the
    codebase, many logging strings are expected to be generated by asserts.
    Therefore, removing these logs may also have an impact on the binary
    size.

--- LTKit/LTKit/LTAssert.h
+++ LTKit/LTKit/LTAssert.h
@@ -10,7 +10,8 @@
 // Borrowed from http://www.mikeash.com/pyblog/friday-qa-2013-05-03-proper-use-of-asserts.html
 
 /// Asserts that the given expression or parameter is true. If \c LT_ABORT_ON_ASSERTIONS is defined,
-/// the program will \c abort() on assertion failure. Otherwise, an exception will be thrown, named
+/// the program will \c abort() on assertion failure. Otherwise, it will \c LogError the message (in
+/// Debug configurations) and an exception will be thrown, named
 /// \c NSInternalInconsistencyException for \c LTAssert and \c NSInvalidArgumentException for \c
 /// LTParameterAssert.
 
@@ -47,20 +48,24 @@
   [NSString stringWithFormat:@"%s is an abstract method and should be implemented in a subclass.", \
       __PRETTY_FUNCTION__]
 
+#if defined(DEBUG) && DEBUG
+  #define _LTAssertLogError(MESSAGE) LogError(@"%@", (MESSAGE))
+#else
+  #define _LTAssertLogError(MESSAGE)
+#endif
+
 #define _LTMethodNotImplementedAbort() \
   do { \
-    [[LTLogger sharedLogger] logWithFormat:@"%@" file:__FILE__ line:__LINE__ \
-        logLevel:LTLogLevelError, _LTMethodNotImplementedMessage()]; \
+    _LTAssertLogError(_LTMethodNotImplementedMessage()); \
     abort(); \
     __builtin_unreachable(); \
   } while (0)
 
 #define _LTMethodNotImplementedRaise() \
   do { \
-      [[LTLogger sharedLogger] logWithFormat:@"%@" file:__FILE__ line:__LINE__ \
-          logLevel:LTLogLevelError, _LTMethodNotImplementedMessage()]; \
-      [[[NSException alloc] initWithName:NSInternalInconsistencyException \
-          reason:_LTMethodNotImplementedMessage() userInfo:nil] raise]; \
+    _LTAssertLogError(_LTMethodNotImplementedMessage()); \
+    [[[NSException alloc] initWithName:NSInternalInconsistencyException \
+        reason:_LTMethodNotImplementedMessage() userInfo:nil] raise]; \
     __builtin_unreachable(); \
   } while (0)
 
@@ -83,8 +88,7 @@
 #define _LTAssertAndDo(EXPRESSION, MESSAGE, EXECUTE_AFTER) \
   do { \
     if (__builtin_expect(!(EXPRESSION), 0)) { \
-      [[LTLogger sharedLogger] logWithFormat:@"%@" file:__FILE__ line:__LINE__ \
-          logLevel:LTLogLevelError, MESSAGE]; \
+      _LTAssertLogError((MESSAGE)); \
       (EXECUTE_AFTER); \
       __builtin_unreachable(); \
     } \
