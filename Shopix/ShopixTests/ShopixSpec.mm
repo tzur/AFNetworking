fc75245da7fd893a823d3d52dc171cc5a6fa1715
Neria Saada <neria@lightricks.com>
2017-08-29 11:11:20 +0300

    Shopix: initial commit.
    Shopix is a library that provides a high-level API for subscription
    purchasing and restoration with a localized messages to the user on
    failure or success.
    
    This library intended to define a common behavior among all Lightricks
    ecosystem app that includes any subscription flow.

---
+++ Shopix/ShopixTests/ShopixSpec.mm
@@ -0,0 +1,12 @@
+// Copyright (c) 2017 Lightricks. All rights reserved.
+// Created by Neria Saada.
+
+#import "Shopix.h"
+
+SpecBegin(Shopix)
+
+// dummy test.
+it(@"should pass", {
+});
+
+SpecEnd
