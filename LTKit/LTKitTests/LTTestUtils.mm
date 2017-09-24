d4bc15eba24a82bd9c82f38a95f3ac4811668ac8
Yaron Inger <yaron@lightricks.com>
2017-09-24 20:43:12 +0300

    LTTestUtils: add device and simulator context and specs.

--- LTKit/LTKitTests/LTTestUtils.mm
+++ LTKit/LTKitTests/LTTestUtils.mm
@@ -12,13 +12,25 @@ NS_ASSUME_NONNULL_BEGIN
 #pragma mark Public methods
 #pragma mark -
 
-void sit(NSString __unused *name, id __unused block) {
+void scontext(NSString * __unused name, id __unused block) {
+#if TARGET_OS_SIMULATOR
+  context(name, block);
+#endif
+}
+
+void dcontext(NSString * __unused name, id __unused block) {
+#if !TARGET_OS_SIMULATOR && TARGET_OS_IPHONE
+  context(name, block);
+#endif
+}
+
+void sit(NSString * __unused name, id __unused block) {
 #if TARGET_OS_SIMULATOR
   it(name, block);
 #endif
 }
 
-void dit(NSString __unused *name, id __unused block) {
+void dit(NSString * __unused name, id __unused block) {
 #if !TARGET_OS_SIMULATOR && TARGET_OS_IPHONE
   it(name, block);
 #endif
