b83bcf9fd96b62b99e20f707b0491efaf275c38f
Daniel Lahyani <dlahyani@lightricks.com>
2017-10-08 19:32:38 +0300

    Shopix: Fix localization script.
    Wrap arguments with quotes to avoid issues with paths that contains
    white spaces. Also make sure `find` and `xargs` commands use '\0' as
    delimiters instead of '\n'.

--- Shopix/BuildTools/Localization/run_app_localization.sh
+++ Shopix/BuildTools/Localization/run_app_localization.sh
@@ -12,11 +12,11 @@ if [[ $# -ne 3 ]]; then
 fi
 
 mkdir -p "$OUT_DIR"/Base.lproj
-find "$PROJECT_DIR"/"$TARGET_NAME" -name \*.m -or -name \*.mm |
-  xargs genstrings -s _LDefault -o "$OUT_DIR"/Base.lproj
-iconv -f UTF-16 -t UTF-8 "$OUT_DIR"/Base.lproj/Localizable.strings > \
-  "$OUT_DIR"/Base.lproj/Localizable.strings.new
-mv "$OUT_DIR"/Base.lproj/Localizable.strings.new "$OUT_DIR"/Base.lproj/Localizable.strings
+find "$PROJECT_DIR/$TARGET_NAME" -name \*.m -or -name \*.mm -print0 | \
+  xargs -0 genstrings -s _LDefault -o "$OUT_DIR/Base.lproj"
+iconv -f UTF-16 -t UTF-8 "$OUT_DIR/Base.lproj/Localizable.strings" > \
+  "$OUT_DIR/Base.lproj/Localizable.strings.new"
+mv "$OUT_DIR/Base.lproj/Localizable.strings.new" "$OUT_DIR/Base.lproj/Localizable.strings"
 
 python "$PROJECT_DIR/../BuildTools/Localization/generate_app_localization.py" \
   "$PROJECT_DIR" "$OUT_DIR" "$OUT_DIR"
