2843356c21ffdade2a0d6201772e8308e8659ad1
Barak Weiss <bweiss@lightricks.com>
2017-10-30 13:37:19 +0200

    NSError+Laboratory: Add nullability annotations.

--- Laboratory/Laboratory/NSError+Laboratory.h
+++ Laboratory/Laboratory/NSError+Laboratory.h
@@ -9,39 +9,39 @@ NS_ASSUME_NONNULL_BEGIN
 
 /// Creates an error with Lightricks' domain, given error code and the associated experiment.
 + (instancetype)lab_errorWithCode:(NSInteger)code
-             associatedExperiment:(NSString *)associatedExperiment;
+             associatedExperiment:(nullable NSString *)associatedExperiment;
 
 /// Creates an error with Lightricks' domain, given error code, the associated experiment and
 /// underlying error.
 + (instancetype)lab_errorWithCode:(NSInteger)code
-             associatedExperiment:(NSString *)associatedExperiment
+             associatedExperiment:(nullable NSString *)associatedExperiment
                   underlyingError:(nullable NSError *)underlyingError;
 
 /// Creates an error with Lightricks' domain, given error code and the associated variant.
 + (instancetype)lab_errorWithCode:(NSInteger)code
-                associatedVariant:(NSString *)associatedVariant;
+                associatedVariant:(nullable NSString *)associatedVariant;
 
 /// Creates an error with Lightricks' domain, given error code, the associated variant and
 /// underlying error.
 + (instancetype)lab_errorWithCode:(NSInteger)code
-                associatedVariant:(NSString *)associatedVariant
+                associatedVariant:(nullable NSString *)associatedVariant
                   underlyingError:(nullable NSError *)underlyingError;
 
 /// Creates an error with Lightricks' domain, given error code and the associated assignment key.
 + (instancetype)lab_errorWithCode:(NSInteger)code
-          associatedAssignmentKey:(NSString *)associatedAssignmentKey;
+          associatedAssignmentKey:(nullable  NSString *)associatedAssignmentKey;
 
 /// Creates an error with Lightricks' domain, given error code, the associated experiment and the
 /// associated variant.
 + (instancetype)lab_errorWithCode:(NSInteger)code
-             associatedExperiment:(NSString *)associatedExperiment
-                associatedVariant:(NSString *)associatedVariant;
+             associatedExperiment:(nullable NSString *)associatedExperiment
+                associatedVariant:(nullable NSString *)associatedVariant;
 
 /// Creates an error with Lightricks' domain, given error code, the associated experiment, the
 /// associated variant and underlying error.
 + (instancetype)lab_errorWithCode:(NSInteger)code
-             associatedExperiment:(NSString *)associatedExperiment
-                associatedVariant:(NSString *)associatedVariant
+             associatedExperiment:(nullable NSString *)associatedExperiment
+                associatedVariant:(nullable NSString *)associatedVariant
                   underlyingError:(nullable NSError *)underlyingError;
 
 /// Expriment associated with the error.
