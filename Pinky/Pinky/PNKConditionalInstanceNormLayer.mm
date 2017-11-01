61b9e262c9f6c41e9f939f0d1b8ab44c3291f439
Ofir Bibi <ofir@lightricks.com>
2017-11-01 13:58:55 +0200

    PNKConditionalInstanceNormLayer: Initial commit.

---
+++ Pinky/Pinky/PNKConditionalInstanceNormLayer.mm
@@ -0,0 +1,116 @@
+// Copyright (c) 2017 Lightricks. All rights reserved.
+// Created by Ofir Bibi.
+
+#import "PNKConditionalInstanceNormLayer.h"
+
+#import "PNKBufferExtensions.h"
+#import "PNKComputeDispatch.h"
+#import "PNKComputeState.h"
+#import "PNKInstanceNormInternalKernel.h"
+#import "PNKNeuralNetworkOperationsModel.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+#if PNK_USE_MPS
+
+@interface PNKConditionalInstanceNormLayer ()
+
+/// Instance normalization kernel performing the operation underlying this layer.
+@property (readonly, nonatomic) PNKInstanceNormInternalKernel *instanceNormKernel;
+
+/// Matrix of scale parameters for the instance normalization operation. Each row of the matrix
+/// represents the scale of the corresponding condition.
+@property (readonly, nonatomic) cv::Mat1f scaleMatrix;
+
+/// Matrix of shift parameters for the instance normalization operation. Each row of the matrix
+/// represents the shift of the corresponding condition.
+@property (readonly, nonatomic) cv::Mat1f shiftMatrix;
+
+@end
+
+@implementation PNKConditionalInstanceNormLayer
+
+@synthesize kernelWidth = _kernelWidth;
+@synthesize kernelHeight = _kernelHeight;
+@synthesize inputFeatureChannels = _inputFeatureChannels;
+@synthesize outputFeatureChannels = _outputFeatureChannels;
+@synthesize strideX = _strideX;
+@synthesize strideY = _strideY;
+@synthesize groups = _groups;
+@synthesize isInputArray = _isInputArray;
+
+#pragma mark -
+#pragma mark Initialization
+#pragma mark -
+
+- (instancetype)initWithDevice:(id<MTLDevice>)device
+            normalizationModel:(const pnk::NormalizationKernelModel &)normalizationModel
+               activationModel:(const pnk::ActivationKernelModel &)activationModel {
+  if (self = [super init]) {
+    [self updatePropertiesWithNormalizationModel:normalizationModel];
+    _instanceNormKernel = [[PNKInstanceNormInternalKernel alloc]
+                           initWithDevice:device
+                           featureChannels:normalizationModel.inputFeatureChannels
+                           activationType:activationModel.activationType];
+
+    if (activationModel.activationType == pnk::ActivationTypeLeakyReLU ||
+        activationModel.activationType == pnk::ActivationTypePReLU) {
+      [self.instanceNormKernel setPReluParameters:activationModel.alpha];
+    }
+    [self setSingleCondition:0];
+  }
+  return self;
+}
+
+- (void)updatePropertiesWithNormalizationModel:(const pnk::NormalizationKernelModel &)model {
+  LTParameterAssert(model.instanceNormalization,
+                    @"Normalization model must be instance normalization");
+  LTParameterAssert(model.scale.cols % model.inputFeatureChannels == 0,
+                    @"Normalization model scale parameters must be a multiply of the number of "
+                    "input features (%lu), got %lu", (unsigned long)model.inputFeatureChannels,
+                    (unsigned long)model.scale.cols);
+  LTParameterAssert(model.shift.cols % model.inputFeatureChannels == 0,
+                    @"Normalization model shift parameters must be a multiply of the number of "
+                    "input features (%lu), got %lu", (unsigned long)model.inputFeatureChannels,
+                    (unsigned long)model.shift.cols);
+  _kernelWidth = 1;
+  _kernelHeight = 1;
+  _inputFeatureChannels = model.inputFeatureChannels;
+  _outputFeatureChannels = model.inputFeatureChannels;
+  _strideX = 1;
+  _strideY = 1;
+  _groups = 1;
+  _isInputArray = model.inputFeatureChannels > 4;
+
+  _conditionsCount = model.scale.cols / model.inputFeatureChannels;
+  _scaleMatrix = model.scale.reshape(0, (int)self.conditionsCount).clone();
+  _shiftMatrix = model.shift.reshape(0, (int)self.conditionsCount).clone();
+}
+
+- (void)setSingleCondition:(NSUInteger)condition {
+  LTParameterAssert(condition < self.conditionsCount,
+                    @"Condition number must be in [0, %lu), got %lu",
+                    (unsigned long)self.conditionsCount, (unsigned long)condition);
+  [self.instanceNormKernel setScaleParameters:self.scaleMatrix.row((int)condition)];
+  [self.instanceNormKernel setShiftParameters:self.shiftMatrix.row((int)condition)];
+}
+
+#pragma mark -
+#pragma mark PNKUnaryNeuralKernel
+#pragma mark -
+
+- (void)encodeToCommandBuffer:(id<MTLCommandBuffer>)commandBuffer
+                   inputImage:(MPSImage *)inputImage outputImage:(MPSImage *)outputImage {
+  [self.instanceNormKernel encodeToCommandBuffer:commandBuffer inputImage:inputImage
+                                    outputImage:outputImage];
+}
+
+- (MTLRegion)inputRegionForOutputSize:(MTLSize)outputSize {
+  return [self.instanceNormKernel inputRegionForOutputSize:outputSize];
+}
+
+@end
+
+#endif // PNK_USE_MPS
+
+NS_ASSUME_NONNULL_END
---
+++ Pinky/PinkyDeviceTests/PNKConditionalInstanceNormLayerSpec.mm
@@ -0,0 +1,247 @@
+// Copyright (c) 2017 Lightricks. All rights reserved.
+// Created by Ofir Bibi.
+
+#import "PNKConditionalInstanceNormLayer.h"
+
+#import "PNKNeuralNetworkModel.h"
+
+SpecBegin(PNKConditionalInstanceNormLayer)
+
+static const NSUInteger kInputWidth = 15;
+static const NSUInteger kInputHeight = 16;
+static const NSUInteger kInputRGBFeatureChannels = 3;
+static const NSUInteger kInputArrayFeatureChannels = 32;
+
+__block id<MTLDevice> device;
+__block id<MTLCommandBuffer> commandBuffer;
+__block PNKConditionalInstanceNormLayer *ciNormOp;
+
+beforeEach(^{
+  device = MTLCreateSystemDefaultDevice();
+  auto commandQueue = [device newCommandQueue];
+  commandBuffer = [commandQueue commandBuffer];
+});
+
+context(@"kernel input region", ^{
+  __block NSUInteger inputChannels;
+
+  beforeEach(^{
+    inputChannels = kInputRGBFeatureChannels;
+
+    pnk::ActivationKernelModel activationModel {
+      .activationType = pnk::ActivationTypeIdentity
+    };
+
+    pnk::NormalizationKernelModel normalizationModel {
+      .instanceNormalization = YES,
+      .inputFeatureChannels = inputChannels,
+      .scale = cv::Mat1f(1, (int)inputChannels),
+      .shift = cv::Mat1f(1, (int)inputChannels)
+    };
+
+    ciNormOp = [[PNKConditionalInstanceNormLayer alloc] initWithDevice:device
+                                                    normalizationModel:normalizationModel
+                                                       activationModel:activationModel];
+  });
+
+  it(@"should calculate primary input region correctly", ^{
+    MTLSize outputSize = {kInputWidth, kInputHeight, inputChannels};
+    MTLRegion inputRegion = [ciNormOp inputRegionForOutputSize:outputSize];
+    expect($(inputRegion.size)).to.equalMTLSize($(outputSize));
+  });
+});
+
+context(@"set conditions", ^{
+  __block NSUInteger conditionsCount;
+
+  beforeEach(^{
+    conditionsCount = 3;
+
+    pnk::ActivationKernelModel activationModel {
+      .activationType = pnk::ActivationTypeIdentity
+    };
+
+    pnk::NormalizationKernelModel normalizationModel {
+      .instanceNormalization = YES,
+      .inputFeatureChannels = kInputRGBFeatureChannels,
+      .scale = cv::Mat1f(1, (int)(kInputRGBFeatureChannels * conditionsCount)),
+      .shift = cv::Mat1f(1, (int)(kInputRGBFeatureChannels * conditionsCount))
+    };
+
+    ciNormOp = [[PNKConditionalInstanceNormLayer alloc] initWithDevice:device
+                                                    normalizationModel:normalizationModel
+                                                       activationModel:activationModel];
+  });
+
+  it(@"raise when trying to set a condition out of bounds", ^{
+    expect(^{
+      [ciNormOp setSingleCondition:conditionsCount];
+    }).to.raise(NSInvalidArgumentException);
+  });
+
+  it(@"set condition correctly when setting a condition in bounds", ^{
+    expect(^{
+      [ciNormOp setSingleCondition:conditionsCount - 1];
+    }).toNot.raiseAny();
+  });
+});
+
+context(@"conditional instance normalization PNKUnaryKernel encoding", ^{
+  __block NSBundle *bundle;
+
+  beforeEach(^{
+    bundle = [NSBundle bundleForClass:[PNKConditionalInstanceNormLayerSpec class]];
+  });
+
+  itShouldBehaveLike(kPNKUnaryKernelExamples, ^{
+    NSUInteger inputChannels = 4;
+
+    pnk::ActivationKernelModel activationModel {
+      .activationType = pnk::ActivationTypeIdentity
+    };
+    pnk::NormalizationKernelModel normalizationModel {
+      .instanceNormalization = YES,
+      .inputFeatureChannels = inputChannels,
+      .scale = PNKLoadFloatTensorFromBundleResource(bundle, @"instanceNorm_nonarray_scale.weights"),
+      .shift = PNKLoadFloatTensorFromBundleResource(bundle, @"instanceNorm_nonarray_shift.weights")
+    };
+
+    PNKConditionalInstanceNormLayer *kernel =
+        [[PNKConditionalInstanceNormLayer alloc] initWithDevice:device
+                                             normalizationModel:normalizationModel
+                                                activationModel:activationModel];
+
+    auto inputMat =
+        PNKLoadHalfFloatTensorFromBundleResource(bundle, @"instanceNorm_nonarray_input.tensor");
+    auto expectedMat =
+        PNKLoadHalfFloatTensorFromBundleResource(bundle, @"instanceNorm_nonarray_output.tensor");
+
+    return @{
+      kPNKKernelExamplesKernel: kernel,
+      kPNKKernelExamplesDevice: device,
+      kPNKKernelExamplesPixelFormat: @(MPSImageFeatureChannelFormatFloat16),
+      kPNKKernelExamplesOutputChannels: @(inputChannels),
+      kPNKKernelExamplesOutputWidth: @(kInputWidth),
+      kPNKKernelExamplesOutputHeight: @(kInputHeight),
+      kPNKKernelExamplesPrimaryInputMat: $(inputMat),
+      kPNKKernelExamplesExpectedMat: $(expectedMat)
+    };
+  });
+
+  itShouldBehaveLike(kPNKUnaryKernelExamples, ^{
+    NSUInteger inputChannels = kInputArrayFeatureChannels;
+
+    pnk::ActivationKernelModel activationModel {
+      .activationType = pnk::ActivationTypeIdentity
+    };
+    pnk::NormalizationKernelModel normalizationModel {
+      .instanceNormalization = YES,
+      .inputFeatureChannels = inputChannels,
+      .scale = PNKLoadFloatTensorFromBundleResource(bundle, @"instanceNorm_scale.weights"),
+      .shift = PNKLoadFloatTensorFromBundleResource(bundle, @"instanceNorm_shift.weights")
+    };
+
+    PNKConditionalInstanceNormLayer *kernel =
+        [[PNKConditionalInstanceNormLayer alloc] initWithDevice:device
+                                             normalizationModel:normalizationModel
+                                                activationModel:activationModel];
+
+    auto inputMat = PNKLoadHalfFloatTensorFromBundleResource(bundle, @"instanceNorm_input.tensor");
+    auto expectedMat = PNKLoadHalfFloatTensorFromBundleResource(bundle,
+                                                                @"instanceNorm_output.tensor");
+
+    return @{
+      kPNKKernelExamplesKernel: kernel,
+      kPNKKernelExamplesDevice: device,
+      kPNKKernelExamplesPixelFormat: @(MPSImageFeatureChannelFormatFloat16),
+      kPNKKernelExamplesOutputChannels: @(inputChannels),
+      kPNKKernelExamplesOutputWidth: @(kInputWidth),
+      kPNKKernelExamplesOutputHeight: @(kInputHeight),
+      kPNKKernelExamplesPrimaryInputMat: $(inputMat),
+      kPNKKernelExamplesExpectedMat: $(expectedMat)
+    };
+  });
+
+  itShouldBehaveLike(kPNKUnaryKernelExamples, ^{
+    NSUInteger inputChannels = kInputArrayFeatureChannels;
+
+    pnk::ActivationKernelModel activationModel {
+      .activationType = pnk::ActivationTypeReLU
+    };
+    pnk::NormalizationKernelModel normalizationModel {
+      .instanceNormalization = YES,
+      .inputFeatureChannels = inputChannels,
+      .scale = PNKLoadFloatTensorFromBundleResource(bundle, @"instanceNorm_relu_scale.weights"),
+      .shift = PNKLoadFloatTensorFromBundleResource(bundle, @"instanceNorm_relu_shift.weights")
+    };
+
+    PNKConditionalInstanceNormLayer *kernel =
+        [[PNKConditionalInstanceNormLayer alloc] initWithDevice:device
+                                             normalizationModel:normalizationModel
+                                                activationModel:activationModel];
+
+    auto inputMat = PNKLoadHalfFloatTensorFromBundleResource(bundle,
+                                                             @"instanceNorm_relu_input.tensor");
+    auto expectedMat = PNKLoadHalfFloatTensorFromBundleResource(bundle,
+                                                                @"instanceNorm_relu_output.tensor");
+
+    return @{
+      kPNKKernelExamplesKernel: kernel,
+      kPNKKernelExamplesDevice: device,
+      kPNKKernelExamplesPixelFormat: @(MPSImageFeatureChannelFormatFloat16),
+      kPNKKernelExamplesOutputChannels: @(inputChannels),
+      kPNKKernelExamplesOutputWidth: @(kInputWidth),
+      kPNKKernelExamplesOutputHeight: @(kInputHeight),
+      kPNKKernelExamplesPrimaryInputMat: $(inputMat),
+      kPNKKernelExamplesExpectedMat: $(expectedMat)
+    };
+  });
+});
+
+context(@"PNKUnaryKernel with MPSTemporaryImage", ^{
+  itShouldBehaveLike(kPNKTemporaryImageUnaryExamples, ^{
+    pnk::ActivationKernelModel activationModel {
+      .activationType = pnk::ActivationTypeIdentity
+    };
+    pnk::NormalizationKernelModel normalizationModel {
+      .instanceNormalization = YES,
+      .inputFeatureChannels = kInputRGBFeatureChannels,
+      .scale = cv::Mat1f(1, (int)kInputRGBFeatureChannels),
+      .shift = cv::Mat1f(1, (int)kInputRGBFeatureChannels)
+    };
+
+    ciNormOp = [[PNKConditionalInstanceNormLayer alloc] initWithDevice:device
+                                                    normalizationModel:normalizationModel
+                                                       activationModel:activationModel];
+
+    return @{
+      kPNKTemporaryImageExamplesKernel: ciNormOp,
+      kPNKTemporaryImageExamplesDevice: device,
+      kPNKTemporaryImageExamplesIsArray: @(NO)
+    };
+  });
+
+  itShouldBehaveLike(kPNKTemporaryImageUnaryExamples, ^{
+    pnk::ActivationKernelModel activationModel {
+      .activationType = pnk::ActivationTypeIdentity
+    };
+    pnk::NormalizationKernelModel normalizationModel {
+      .instanceNormalization = YES,
+      .inputFeatureChannels = kInputArrayFeatureChannels,
+      .scale = cv::Mat1f(1, (int)kInputArrayFeatureChannels),
+      .shift = cv::Mat1f(1, (int)kInputArrayFeatureChannels)
+    };
+
+    ciNormOp = [[PNKConditionalInstanceNormLayer alloc] initWithDevice:device
+                                                    normalizationModel:normalizationModel
+                                                       activationModel:activationModel];
+
+    return @{
+      kPNKTemporaryImageExamplesKernel: ciNormOp,
+      kPNKTemporaryImageExamplesDevice: device,
+      kPNKTemporaryImageExamplesIsArray: @(YES)
+    };
+  });
+});
+
+SpecEnd
