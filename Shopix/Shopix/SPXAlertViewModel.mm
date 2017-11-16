ba7b86d5a300f410c279496d73be2fcd19c88db5
Daniel Lahyani <dlahyani@lightricks.com>
2017-11-16 14:11:32 +0200

    SPXAlertViewModel: Add view-model protocol and default implementation.

---
+++ Shopix/Shopix/SPXAlertViewModel.mm
@@ -0,0 +1,59 @@
+// Copyright (c) 2017 Lightricks. All rights reserved.
+// Created by Daniel Lahyani.
+
+#import "SPXAlertViewModel.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+#pragma mark -
+#pragma mark SPXAlertButtonViewModel
+#pragma mark -
+
+@implementation SPXAlertButtonViewModel
+
+@synthesize title = _title;
+@synthesize action = _action;
+
+- (instancetype)initWithTitle:(NSString *)title action:(RACSignal *)action {
+  if (self = [super init]) {
+    _title = [title copy];
+    _action = [[RACCommand alloc] initWithSignalBlock:^(id) {
+      return action;
+    }];
+  }
+  return self;
+}
+
+@end
+
+#pragma mark -
+#pragma mark SPXAlertViewModel
+#pragma mark -
+
+@implementation SPXAlertViewModel
+
+@synthesize title = _title;
+@synthesize message = _message;
+@synthesize buttons = _buttons;
+@synthesize defaultButtonIndex = _defaultButtonIndex;
+
+- (instancetype)initWithTitle:(NSString *)title message:(nullable NSString *)message
+                      buttons:(NSArray<SPXAlertButtonViewModel *> *)buttons
+           defaultButtonIndex:(nullable NSNumber *)defaultButtonIndex {
+  LTParameterAssert(buttons.count > 0, @"Alert must have at least one button");
+  LTParameterAssert(!defaultButtonIndex || defaultButtonIndex.unsignedLongValue < buttons.count,
+           @"Default button index (%@) must be lower than the number of buttons (%lu)",
+           defaultButtonIndex, (unsigned long)buttons.count);
+
+  if (self = [super init]) {
+    _title = [title copy];
+    _message = [message copy];
+    _buttons = [buttons copy];
+    _defaultButtonIndex = defaultButtonIndex;
+  }
+  return self;
+}
+
+@end
+
+NS_ASSUME_NONNULL_END
