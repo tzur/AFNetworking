6ad28fccf9972374c45b55593b9a8f9a9ceed8f8
Michael Kimyagarov <mkimi@lightricks.com>
2017-11-23 09:39:21 +0200

    TINTestsCommon: initial commit.

---
+++ TinCan/TinCanTests/TINTestsCommon.h
@@ -0,0 +1,9 @@
+// Copyright (c) 2017 Lightricks. All rights reserved.
+// Created by Michael Kimyagarov.
+
+NS_ASSUME_NONNULL_BEGIN
+
+/// TestHost's application group ID, used in specs.
+extern NSString * const kTINTestHostAppGroupID;
+
+NS_ASSUME_NONNULL_END
---
+++ TinCan/TinCanTests/TINTestsCommon.mm
@@ -0,0 +1,11 @@
+// Copyright (c) 2017 Lightricks. All rights reserved.
+// Created by Michael Kimyagarov.
+
+#import "TINTestsCommon.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+/// TestHost's application group ID, used in specs.
+NSString * const kTINTestHostAppGroupID = @"group.com.lightricks.TestHost";
+
+NS_ASSUME_NONNULL_END
