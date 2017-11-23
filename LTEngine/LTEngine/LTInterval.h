35d1cf7afa39fd7335a264aa23639433ae7b8833
Rouven Strauss <rouven@lightricks.com>
2017-11-23 12:13:39 +0100

    LTInterval: Use correct terminology for infimum and supremum.
    Before this commit lt::Interval falsely used the terms minimum and
    maximum for its infimum and supremum. The names of related methods are
    being renamed as well.

--- LTEngine/LTEngine/LTInterval.h
+++ LTEngine/LTEngine/LTInterval.h
@@ -18,51 +18,47 @@ public:
   };
 
   /// Returns an empty interval.
-  Interval() noexcept : minValue(0), maxValue(0),
-      minEndpointInclusion(Open), maxEndpointInclusion(Open) {}
+  Interval() noexcept : _inf(0), _sup(0), _infInclusion(Open), _supInclusion(Open) {}
 
-  /// Initializes with the given \c values, the \c minEndpointInclusion indicating whether the
-  /// minimum end point of the interval is included, and the \c maxEndpointInclusion indicating
-  /// whether the maximum end point of the interval is included.
-  Interval(std::pair<T, T> values, EndpointInclusion minEndpointInclusion,
-           EndpointInclusion maxEndpointInclusion) noexcept :
-      minValue(std::min(values.first, values.second)),
-      maxValue(std::max(values.first, values.second)),
-      minEndpointInclusion(minEndpointInclusion), maxEndpointInclusion(maxEndpointInclusion) {}
+  /// Initializes with the given \c values, the \c infInclusion indicating whether the infimum of
+  /// the interval is included, and the \c supInclusion indicating whether the supremum of the
+  /// interval is included.
+  Interval(std::pair<T, T> values, EndpointInclusion infInclusion,
+           EndpointInclusion supInclusion) noexcept :
+      _inf(std::min(values.first, values.second)), _sup(std::max(values.first, values.second)),
+      _infInclusion(infInclusion), _supInclusion(supInclusion) {}
 
   /// Initializes with the given \c values and the \c endpointInclusion indicating whether the end
   /// points of the interval are included.
   Interval(std::pair<T, T> values, EndpointInclusion endpointInclusion) noexcept :
-      minValue(std::min(values.first, values.second)),
-      maxValue(std::max(values.first, values.second)),
-      minEndpointInclusion(endpointInclusion), maxEndpointInclusion(endpointInclusion) {}
+      _inf(std::min(values.first, values.second)), _sup(std::max(values.first, values.second)),
+      _infInclusion(endpointInclusion), _supInclusion(endpointInclusion) {}
 
   /// Initializes with the given \c values and closed end points.
   Interval(std::pair<T, T> values) noexcept :
-      minValue(std::min(values.first, values.second)),
-      maxValue(std::max(values.first, values.second)),
-      minEndpointInclusion(Closed), maxEndpointInclusion(Closed) {}
+      _inf(std::min(values.first, values.second)), _sup(std::max(values.first, values.second)),
+      _infInclusion(Closed), _supInclusion(Closed) {}
 
   /// Return a hash value for this interval.
   size_t hash() const {
-    return std::hash<T>()(minValue) ^ std::hash<T>()(maxValue) ^
-        std::hash<T>()(minEndpointInclusion) ^ std::hash<T>()(maxEndpointInclusion);
+    return std::hash<T>()(_inf) ^ std::hash<T>()(_sup) ^
+        std::hash<T>()(_infInclusion) ^ std::hash<T>()(_supInclusion);
   }
 
   /// Returns \c true if this interval is empty.
   bool isEmpty() const {
-    if (minValue == maxValue) {
-      return !minEndpointIncluded() || !maxEndpointIncluded();
-    } else if (!minEndpointIncluded() && !maxEndpointIncluded()) {
-      return std::nextafter(minValue, maxValue) == maxValue;
+    if (_inf == _sup) {
+      return !infIncluded() || !supIncluded();
+    } else if (!infIncluded() && !supIncluded()) {
+      return std::nextafter(_inf, _sup) == _sup;
     }
     return false;
   }
 
   /// Returns \c true if this interval contains the given \c value.
   bool contains(T value) const {
-    return (minEndpointIncluded() ? value >= minValue : value > minValue) &&
-        (maxEndpointIncluded() ? value <= maxValue : value < maxValue);
+    return (infIncluded() ? value >= _inf : value > _inf) &&
+        (supIncluded() ? value <= _sup : value < _sup);
   }
 
   /// Returns \c true if this interval intersects with the given \c interval.
@@ -73,72 +69,71 @@ public:
   /// Returns a new interval constituting the intersection between this interval and the given
   /// \c interval.
   lt::Interval<T> intersectionWith(lt::Interval<CGFloat> interval) const {
-    T min = std::max(minValue, interval.minValue);
-    T max = std::min(maxValue, interval.maxValue);
+    T inf = std::max(_inf, interval._inf);
+    T sup = std::min(_sup, interval._sup);
 
-    if (min > max) {
+    if (inf > sup) {
       return Interval();
     }
 
-    EndpointInclusion minInclusion = contains(min) && interval.contains(min) ? Closed : Open;
-    EndpointInclusion maxInclusion = contains(max) && interval.contains(max) ? Closed : Open;
-    return Interval({min, max}, minInclusion, maxInclusion);
+    EndpointInclusion infInclusion = contains(inf) && interval.contains(inf) ? Closed : Open;
+    EndpointInclusion supInclusion = contains(sup) && interval.contains(sup) ? Closed : Open;
+    return Interval({inf, sup}, infInclusion, supInclusion);
   }
 
-  /// Returns the minimum value of this interval.
-  T min() const {
-    return minValue;
+  /// Returns the infimum of this interval.
+  T inf() const {
+    return _inf;
   };
 
-  /// Returns the maximum value of this interval.
-  T max() const {
-    return maxValue;
+  /// Returns the supremum of this interval.
+  T sup() const {
+    return _sup;
   }
 
-  /// Returns the linearly interpolated value for parametric value \c t. In particular, \c min() is
-  /// returned for \c t equalling \c 0 and \c max() is returned for \c t equalling \c 1. If this
-  /// interval is empty, an assertion is raised.
+  /// Returns the linearly interpolated value for parametric value \c t. In particular, the minimum
+  /// of this interval is returned for \c t equalling \c 0 and the maximum of this interval is
+  /// returned for \c t equalling \c 1. If this interval is empty, an assertion is raised.
   T valueAt(T t) const {
     LTParameterAssert(!isEmpty(), @"No interpolation is possible for empty interval ");
-    T min = minEndpointIncluded() ? minValue : std::nextafter(minValue, maxValue);
-    T max = maxEndpointIncluded() ? maxValue : std::nextafter(maxValue, minValue);
+    T min = infIncluded() ? _inf : std::nextafter(_inf, _sup);
+    T max = supIncluded() ? _sup : std::nextafter(_sup, _inf);
     return (1 - t) * min + t * max;
   }
 
-  /// Returns \c true if the minimum endpoint of this interval belongs to the interval.
-  bool minEndpointIncluded() const {
-    return minEndpointInclusion == Closed;
+  /// Returns \c true if the infimum of this interval belongs to the interval.
+  bool infIncluded() const {
+    return _infInclusion == Closed;
   }
 
-  /// Returns \c true if the maximum endpoint of this interval belongs to the interval.
-  bool maxEndpointIncluded() const {
-    return maxEndpointInclusion == Closed;
+  /// Returns \c true if the supremum of this interval belongs to the interval.
+  bool supIncluded() const {
+    return _supInclusion == Closed;
   }
 
   /// Returns a string representation of this interval.
   NSString *description() const {
-    return [NSString stringWithFormat:@"%@%g, %g%@", minEndpointIncluded() ? @"[" : @"(",
-            minValue, maxValue, maxEndpointIncluded() ? @"]" : @")"];
+    return [NSString stringWithFormat:@"%@%g, %g%@", infIncluded() ? @"[" : @"(",
+            _inf, _sup, supIncluded() ? @"]" : @")"];
   }
 
 private:
-  /// Minimum value of this interval.
-  T minValue;
-  /// Maximum value of this interval.
-  T maxValue;
-  /// \c true if the minimum endpoint of this interval belongs to the interval.
-  EndpointInclusion minEndpointInclusion;
-  /// \c true if the maximum endpoint of this interval belongs to the interval.
-  EndpointInclusion maxEndpointInclusion;
+  /// Infimum of this interval.
+  T _inf;
+  /// Supremum of this interval.
+  T _sup;
+  /// \c true if the infimum of this interval belongs to the interval.
+  EndpointInclusion _infInclusion;
+  /// \c true if the supremum of this interval belongs to the interval.
+  EndpointInclusion _supInclusion;
 };
 
 } // namespace lt
 
 template <typename T>
 constexpr bool operator==(lt::Interval<T> lhs, lt::Interval<T> rhs) {
-  return lhs.min() == rhs.min() && lhs.max() == rhs.max() &&
-      lhs.minEndpointIncluded() == rhs.minEndpointIncluded() &&
-      lhs.maxEndpointIncluded()== rhs.maxEndpointIncluded();
+  return lhs.inf() == rhs.inf() && lhs.sup() == rhs.sup() &&
+      lhs.infIncluded() == rhs.infIncluded() && lhs.supIncluded()== rhs.supIncluded();
 }
 
 template <typename T>
