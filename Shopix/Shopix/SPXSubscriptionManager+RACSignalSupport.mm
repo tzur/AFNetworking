f25795dc8c4a100ba0743097dc4399dba221b0d6
Daniel Lahyani <dlahyani@lightricks.com>
2017-12-03 13:46:44 +0200

    SPXSubscriptionManager: Add signal-based reactive interface.
    Wrap all `SPXSubscriptionManager` operations with methods that return
    `RACSignal` which delivers values, errors and completion based on the
    values sent to the completion handler.
    
    Delegate methods were also wrapped with signals and now spare the need
    to implement the `SPXSubscriptionManagerDelegate` protocol.

---
+++ Shopix/Shopix/SPXSubscriptionManager+RACSignalSupport.mm
@@ -0,0 +1,135 @@
+// Copyright (c) 2017 Lightricks. All rights reserved.
+// Created by Daniel Lahyani.
+
+#import "SPXSubscriptionManager+RACSignalSupport.h"
+
+#import <Bazaar/NSErrorCodes+Bazaar.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@implementation SPXSubscriptionManager (RACSignalSupport)
+
+#pragma mark -
+#pragma mark Operations
+#pragma mark -
+
+- (RACSignal<NSDictionary<NSString *, BZRProduct *> *> *)fetchProductsInfo:
+    (NSSet<NSString *> *)productIdentifiers {
+  @weakify(self);
+  return [RACSignal createSignal:^RACDisposable * _Nullable(id<RACSubscriber> subscriber) {
+    @strongify(self);
+    if (!self) {
+      [subscriber sendCompleted];
+      return nil;
+    }
+
+    [self fetchProductsInfo:productIdentifiers
+          completionHandler:^(NSDictionary<NSString *,BZRProduct *> * _Nullable productsInfo,
+                              NSError * _Nullable error) {
+            if (error) {
+              [subscriber sendError:error];
+              return;
+            } else if (productsInfo) {
+              [subscriber sendNext:productsInfo];
+            }
+
+            [subscriber sendCompleted];
+          }];
+
+    return nil;
+  }];
+}
+
+- (RACSignal<BZRReceiptSubscriptionInfo *> *)purchaseSubscription:(NSString *)productIdentifier {
+  @weakify(self);
+  return [RACSignal createSignal:^RACDisposable * _Nullable(id<RACSubscriber> subscriber) {
+    @strongify(self);
+    if (!self) {
+      [subscriber sendCompleted];
+      return nil;
+    }
+
+    [self purchaseSubscription:productIdentifier
+             completionHandler:^(BZRReceiptSubscriptionInfo * _Nullable subscriptionInfo,
+                                 NSError * _Nullable error) {
+               if (error && error.code != BZRErrorCodeOperationCancelled) {
+                 [subscriber sendError:error];
+                 return;
+               } else if (subscriptionInfo) {
+                 [subscriber sendNext:subscriptionInfo];
+               }
+
+               [subscriber sendCompleted];
+             }];
+    return nil;
+  }];
+}
+
+- (RACSignal<BZRReceiptInfo *> *)restorePurchases {
+  @weakify(self);
+  return [RACSignal createSignal:^RACDisposable * _Nullable(id<RACSubscriber> subscriber) {
+    @strongify(self);
+    if (!self) {
+      [subscriber sendCompleted];
+      return nil;
+    }
+
+    [self restorePurchasesWithCompletionHandler:^(BZRReceiptInfo * _Nullable receiptInfo,
+                                                  NSError * _Nullable error) {
+      if (error && error.code != BZRErrorCodeOperationCancelled) {
+        [subscriber sendError:error];
+        return;
+      } else if (receiptInfo) {
+        [subscriber sendNext:receiptInfo];
+      }
+
+      [subscriber sendCompleted];
+    }];
+    return nil;
+  }];
+}
+
+#pragma mark -
+#pragma mark Delegate Replacement
+#pragma mark -
+
+static void RACUseDelegateProxy(SPXSubscriptionManager *self) {
+  if (self.delegate == self.spx_delegateProxy) {
+    return;
+  }
+
+  self.spx_delegateProxy.rac_proxiedDelegate = self.delegate;
+  self.delegate = (id)self.spx_delegateProxy;
+}
+
+- (RACDelegateProxy *)spx_delegateProxy {
+  RACDelegateProxy *proxy = objc_getAssociatedObject(self, _cmd);
+  if (!proxy) {
+    proxy = [[RACDelegateProxy alloc] initWithProtocol:@protocol(SPXSubscriptionManagerDelegate)];
+    objc_setAssociatedObject(self, _cmd, proxy, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+  }
+
+  return proxy;
+}
+
+- (RACSignal<id<SPXAlertViewModel>> *)alertRequested {
+  RACUseDelegateProxy(self);
+  return [[[self spx_delegateProxy]
+      signalForSelector:@selector(presentAlertWithViewModel:)]
+      map:^id<SPXAlertViewModel>(RACTuple *parameters) {
+        return nn(parameters.first);
+      }];
+}
+
+- (RACSignal<LTVoidBlock> *)feedbackMailComposerRequested {
+  RACUseDelegateProxy(self);
+  return [[[self spx_delegateProxy]
+      signalForSelector:@selector(presentFeedbackMailComposerWithCompletionHandler:)]
+      map:^LTVoidBlock(RACTuple *parameters) {
+        return nn(parameters.first);
+      }];
+}
+
+@end
+
+NS_ASSUME_NONNULL_END
