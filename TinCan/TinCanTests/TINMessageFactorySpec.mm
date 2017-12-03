9ebb9c58e33cca37b84201055d5199c04195c1c0
Michael Kimyagarov <mkimi@lightricks.com>
2017-12-03 16:09:50 +0200

    TINTestsCommon: implement TINCleanupTestHostAppGroupDirectory method.

--- TinCan/TinCanTests/TINMessageFactorySpec.mm
+++ TinCan/TinCanTests/TINMessageFactorySpec.mm
@@ -27,25 +27,6 @@ static UIImage *TINUIImage(CGSize size, UIColor *fillColor) {
   return image;
 }
 
-/// Removes the contents of an application group directory associated with
-/// \c kTINTestHostAppGroupID, if it exists.
-static void TINCleanupTestHostAppGroupDirectory() {
-  auto _Nullable url = [NSURL tin_appGroupDirectoryURL:kTINTestHostAppGroupID];
-  if (!url) {
-    return;
-  }
-  LTAssert(url.path, @"Failed obtainig the path of url: %@", url);
-
-  auto fileManager = [NSFileManager defaultManager];
-  NSError *error;
-  for (NSString *itemName in [fileManager contentsOfDirectoryAtPath:nn(url.path) error:&error]) {
-    auto _Nullable itemURL = [url URLByAppendingPathComponent:itemName];
-    LTAssert(itemURL, @"Failed appending: %@ to url: %@", itemName, url);
-    auto success = [fileManager removeItemAtURL:nn(itemURL) error:&error];
-    LTAssert(success && !error, @"Error: %@ when removing item: %@", error, itemURL);
-  }
-}
-
 SpecBegin(TINMessageFactory)
 
 static NSString * const kTINTargetScheme = @"TargetScheme";
--- TinCan/TinCanTests/TINTestsCommon.h
+++ TinCan/TinCanTests/TINTestsCommon.h
@@ -1,9 +1,15 @@
 // Copyright (c) 2017 Lightricks. All rights reserved.
 // Created by Michael Kimyagarov.
 
+#import "TINMessage.h"
+
 NS_ASSUME_NONNULL_BEGIN
 
 /// TestHost's application group ID, used in specs.
 extern NSString * const kTINTestHostAppGroupID;
+
+/// Removes the contents of an application group directory associated with
+/// \c kTINTestHostAppGroupID, if it exists.
+void TINCleanupTestHostAppGroupDirectory();
 
 NS_ASSUME_NONNULL_END
