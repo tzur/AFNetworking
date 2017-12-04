a6de451a0cc7b47ee1776a38f6e57e0b884ec74e
Michael Kimyagarov <mkimi@lightricks.com>
2017-12-04 20:53:41 +0200

    NSURL+TinCan: add tin_appGroupDirectoryURL method.

--- TinCan/TinCanTests/NSURL+TinCanSpec.mm
+++ TinCan/TinCanTests/NSURL+TinCanSpec.mm
@@ -8,12 +8,22 @@ SpecBegin(NSURL_TinCan)
 it(@"should return a valid message directory url", ^{
   auto _Nullable url = [NSURL tin_messageDirectoryURLWithAppGroup:kTINTestHostAppGroupID
                                                            scheme:@"foo" identifier:[NSUUID UUID]];
-  expect(url).notTo.beNil();
+  expect(url.path).notTo.beNil();
 });
 
 it(@"should return nil for invalid app group id", ^{
   auto _Nullable url = [NSURL tin_messageDirectoryURLWithAppGroup:@"foo" scheme:@"bar"
                                                        identifier:[NSUUID UUID]];
+  expect(url).to.beNil();
+});
+
+it(@"should return a valid app group directory", ^{
+  auto _Nullable url = [NSURL tin_appGroupDirectoryURL:kTINTestHostAppGroupID];
+  expect(url.path).notTo.beNil();
+});
+
+it(@"should return nil for invalid app group", ^{
+  auto _Nullable url = [NSURL tin_appGroupDirectoryURL:@"foo"];
   expect(url).to.beNil();
 });
 
