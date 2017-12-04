d3c7d12ddd7a403d5b383c04d17bc422f3d77342
Neria Saada <neria@lightricks.com>
2017-12-04 19:42:48 +0200

    SPXSubscriptionTermsViewModel: Add initializer for custom colors.

--- Shopix/Shopix/SPXSubscriptionTermsViewModel.h
+++ Shopix/Shopix/SPXSubscriptionTermsViewModel.h
@@ -32,14 +32,25 @@ NS_ASSUME_NONNULL_BEGIN
 - (instancetype)init NS_UNAVAILABLE;
 
 /// Initializes with the default terms content. The full subscription terms-of-use and
-/// privacy-policy URLs are set by \c fullTermsURL and \c privacyPolicyURL.
+/// privacy-policy URLs are set by \c fullTermsURL and \c privacyPolicyURL. \c termsTextColor and
+/// \c linksColor are set to the default colors.
 - (instancetype)initWithFullTerms:(NSURL *)fullTermsURL privacyPolicy:(NSURL *)privacyPolicyURL;
 
 /// Initializes with \c termsOverview, with the full subscription terms-of-use and privacy-policy
-/// URLs are set by \c fullTermsURL and \c privacyPolicyURL.
+/// URLs are set by \c fullTermsURL and \c privacyPolicyURL. \c termsTextColor and \c linksColor
+/// are set to the default colors.
 - (instancetype)initWithTermsOverview:(NSString *)termsOverview
                             fullTerms:(NSURL *)fullTermsURL
-                        privacyPolicy:(NSURL *)privacyPolicyURL NS_DESIGNATED_INITIALIZER;
+                        privacyPolicy:(NSURL *)privacyPolicyURL;
+
+/// Initializes with \c termsOverview, with the full subscription terms-of-use and privacy-policy
+/// URLs are set by \c fullTermsURL and \c privacyPolicyURL. \c termsOverviewColor is the color for
+/// \c termsText and \c linksColor is the color for \c termsOfUseLink and \c privacyPolicyLink.
+- (instancetype)initWithTermsOverview:(NSString *)termsOverview
+                            fullTerms:(NSURL *)fullTermsURL
+                        privacyPolicy:(NSURL *)privacyPolicyURL
+                       termsTextColor:(UIColor *)termsTextColor
+                           linksColor:(UIColor *)linksColor NS_DESIGNATED_INITIALIZER;
 
 @end
 
