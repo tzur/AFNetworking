15d1472e91c98956a0a87d1a93ae49877290cd03
byohay <ben@lightricks.com>
2017-12-05 11:32:52 +0200

    Bazaar: store receipt data to storage every receipt validation.
    The receipt data is stored so that other applications on the same device
    will be able to use them for validation. The receipt data is not stored
    if the validation error occurred because of a malformed receipt data.

--- Bazaar/Bazaar/BZRValidatedReceiptValidationStatusProvider.h
+++ Bazaar/Bazaar/BZRValidatedReceiptValidationStatusProvider.h
@@ -3,6 +3,8 @@
 
 #import "BZRReceiptValidationStatusProvider.h"
 
+@class BZRReceiptDataCache;
+
 @protocol BZRReceiptValidationParametersProvider, BZRReceiptValidator;
 
 NS_ASSUME_NONNULL_BEGIN
@@ -16,14 +18,19 @@ NS_ASSUME_NONNULL_BEGIN
 /// Initializes with \c receiptValidator set to \c [[BZRValidatricksReceiptValidator alloc] init]
 /// and with \c validationParametersProvider.
 - (instancetype)initWithValidationParametersProvider:(id<BZRReceiptValidationParametersProvider>)
-    validationParametersProvider;
+    validationParametersProvider receiptDataCache:(BZRReceiptDataCache *)receiptDataCache
+    currentApplicationBundleID:(NSString *)currentApplicationBundleID;
 
 /// Initializes \c receiptValidator is used to validate the receipt and return the latest
 /// \c BZRReceiptValidationStatus. \c validationParametersProvider is used to provide validation
-/// parameters to \c receiptValidator to validate the receipt.
+/// parameters to \c receiptValidator to validate the receipt. \c receiptDataCache is used to store
+/// the receipt validation status when validation is requested. \c currentApplicationBundleID is
+/// used to store the receipt data to the storage of the current application.
 - (instancetype)initWithReceiptValidator:(id<BZRReceiptValidator>)receiptValidator
     validationParametersProvider:(id<BZRReceiptValidationParametersProvider>)
-    validationParametersProvider NS_DESIGNATED_INITIALIZER;
+    validationParametersProvider receiptDataCache:(BZRReceiptDataCache *)receiptDataCache
+    currentApplicationBundleID:(NSString *)currentApplicationBundleID
+    NS_DESIGNATED_INITIALIZER;
 
 @end
 
