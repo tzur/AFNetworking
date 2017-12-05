b6818ed515a059fd600acc295cf4e9729035a77a
Barak Weiss <bweiss@lightricks.com>
2017-12-05 17:03:07 +0200

    PTNPhotoKitAssetManager: Support withMetadata image fetch options.
    This option is supported only for image assets.

--- Photons/Photons/PTNPhotoKitAssetManager.h
+++ Photons/Photons/PTNPhotoKitAssetManager.h
@@ -8,6 +8,8 @@ NS_ASSUME_NONNULL_BEGIN
 @protocol PTNAuthorizationManager, PTNPhotoKitChangeManager, PTNPhotoKitFetcher,
     PTNPhotoKitImageManager, PTNPhotoKitObserver;
 
+@class PTNImageResizer;
+
 /// Asset manager which backs PhotoKit assets.
 @interface PTNPhotoKitAssetManager : NSObject <PTNAssetManager>
 
@@ -19,22 +21,23 @@ NS_ASSUME_NONNULL_BEGIN
 /// @see -initWithFetcher:observer:imageManager:authorizationManager:changeManager.
 - (instancetype)init;
 
-/// Initializes with the default implementations of \c PTNPhotoKitFetcher and
-/// \c PTNPhotoKitChangeManager, \c PTNPhotoKitDeferringImageManager initialized with the given
-/// \c authorizationManager, \c PTNPHotoKitObserver initialized with the shared iOS photo library
-/// and the given \c authorizationManager.
+/// Initializes with the default implementations of \c PTNPhotoKitFetcher
+/// \c PTNPhotoKitChangeManager and \c PTNImageResizer. \c PTNPhotoKitDeferringImageManager
+/// initialized with the given \c authorizationManager, \c PTNPHotoKitObserver initialized with the
+/// shared iOS photo library and the given \c authorizationManager.
 ///
 /// @see -initWithFetcher:observer:imageManager:authorizationManager:changeManager.
 - (instancetype)initWithAuthorizationManager:(id<PTNAuthorizationManager>)authorizationManager;
 
 /// Initializes with a \c PTNPhotoKitFetcher, \c PTNPhotoKitObserver, \c PTNPhotoKitImageManager and
-/// \c PTNPhotoKitChangeManager used to interact with the iOS photo library.
+/// \c PTNPhotoKitChangeManager used to interact with the iOS photo library and \c PTNImageResizer
+/// to resize images.
 - (instancetype)initWithFetcher:(id<PTNPhotoKitFetcher>)fetcher
                        observer:(id<PTNPhotoKitObserver>)observer
                    imageManager:(id<PTNPhotoKitImageManager>)imageManager
            authorizationManager:(id<PTNAuthorizationManager>)authorizationManager
                   changeManager:(id<PTNPhotoKitChangeManager>)changeManager
-    NS_DESIGNATED_INITIALIZER;
+                   imageResizer:(PTNImageResizer *)imageResizer NS_DESIGNATED_INITIALIZER;
 
 @end
 
