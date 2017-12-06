34987be086874c9add3fdc0cda8f93f66813c362
Gershon Hochman <gershon@lightricks.com>
2017-12-06 15:06:09 +0200

    PNKTestUtils: add PNKFillMatrix helper function.

--- Pinky/PinkyDeviceTests/PNKTestUtils.h
+++ Pinky/PinkyDeviceTests/PNKTestUtils.h
@@ -19,4 +19,8 @@ cv::Mat1f PNKLoadFloatTensorFromBundleResource(NSBundle *bundle, NSString *resou
 /// Reads an array of float16 numbers from \c fileName and returns them as a row vector.
 cv::Mat1hf PNKLoadHalfFloatTensorFromBundleResource(NSBundle *bundle, NSString *resource);
 
+/// Creates a half-float matrix with given \c rows, \c columns and \c channels and fills it with
+/// zeroes and ones in 3D chessboard pattern.
+cv::Mat PNKFillMatrix(int rows, int columns, int channels);
+
 NS_ASSUME_NONNULL_END
