63723952b5a8aa2e08336b73aab34b3c306ced1b
Amit Goldstein <amit@lightricks.com>
2017-12-19 14:38:06 +0200

    EXPMatchers+beCloseToLTVector: initial commit.

---
+++ LTEngine/LTEngineTests/EXPMatchers+beCloseToLTVector.h
@@ -0,0 +1,6 @@
+// Copyright (c) 2017 Lightricks. All rights reserved.
+// Created by Amit Goldstein.
+
+#import <LTEngineTests/EXPMatchers+beCloseToGLKVector.h>
+
+#define beCloseToLTVectorWithin beCloseToGLKVectorWithin
