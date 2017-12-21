b3aac1268e0bfb9d533d1bbe906a6c6e522d412a
Amit Goldstein <amit@lightricks.com>
2017-12-21 17:59:56 +0200

    PNKColorTransferHistogram: initial commit.

---
+++ Pinky/Pinky/PNKColorTransferHistogram.h
@@ -0,0 +1,54 @@
+// Copyright (c) 2017 Lightricks. All rights reserved.
+// Created by Amit Goldstein.
+
+NS_ASSUME_NONNULL_BEGIN
+
+#if PNK_USE_MPS
+
+@protocol MTLBuffer;
+
+/// Computes the histogram per each channel for a given \c MTLBuffer after a given \c 3x3 linear
+/// transformation is applied on them. Input is expected to be Float RGBA, and the alpha channel is
+/// ignored. The transformation is applied on the fly, such that the input buffer is left unchanged.
+@interface PNKColorTransferHistogram : NSObject
+
+- (instancetype)init NS_UNAVAILABLE;
+
+/// Initializes with the given \c device, desired number of bins in the histogram, and number of
+/// pixels in the buffer that will be given as input.
+- (instancetype)initWithDevice:(id<MTLDevice>)device histogramBins:(NSUInteger)histogramBins
+                     inputSize:(NSUInteger)inputSize NS_DESIGNATED_INITIALIZER;
+
+/// Encodes the operations performed by the kernels to \c commandBuffer, using \c inputBuffer as
+/// input while applying the \c 3x3 transformation in \c transformBuffer on the fly during the
+/// calculation such that it is left unchanged. Output is written to \c histogramBuffer which is
+/// expected to contain <tt>4 * histogramBins * sizeof(uint32_t)</tt>, where the fourth channel
+/// exists due to Metal padding requirements and should be ignored. The bins are equally distributed
+/// between the minimum and maximum values defined in \c minValueBuffer and \c maxValueBuffer, such
+/// that every value is clamped between them for the purposes of histogram calculation.
+
+/// @note \c minValueBuffer and \c maxValueBuffer must contain at least \c 4 floats, and the last
+/// channel is ignored.
+/// @note \c transformBuffer should represent a \c 3x3 float matrix in \c 12 floats due to padding,
+/// such that each 4th float is ignored.
+- (void)encodeToCommandBuffer:(id<MTLCommandBuffer>)commandBuffer
+                  inputBuffer:(id<MTLBuffer>)inputBuffer
+              transformBuffer:(id<MTLBuffer>)transformBuffer
+               minValueBuffer:(id<MTLBuffer>)minValueBuffer
+               maxValueBuffer:(id<MTLBuffer>)maxValueBuffer
+              histogramBuffer:(id<MTLBuffer>)histogramBuffer;
+
+/// Number of histogram entries, or "bins" for each channel.
+@property (readonly, nonatomic) NSUInteger histogramBins;
+
+/// Number of pixels in the input buffer that will be provided.
+@property (readonly, nonatomic) NSUInteger inputSize;
+
+/// Maximum supported number of histogram bins.
+@property (class, readonly, nonatomic) NSUInteger maxSupportedHistogramBins;
+
+@end
+
+#endif // PNK_USE_MPS
+
+NS_ASSUME_NONNULL_END
---
+++ Pinky/Pinky/PNKColorTransferHistogram.mm
@@ -0,0 +1,132 @@
+// Copyright (c) 2017 Lightricks. All rights reserved.
+// Created by Amit Goldstein.
+
+#import "PNKColorTransferHistogram.h"
+
+#import "PNKComputeDispatch.h"
+#import "PNKComputeState.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+#if PNK_USE_MPS
+
+@interface PNKColorTransferHistogram ()
+
+/// Device to encode kernel operations.
+@property (readonly, nonatomic) id<MTLDevice> device;
+
+/// Compiled state of kernel that performs the first stage in the reduction, computing the partial
+/// histograms for each threadgroup, after applying a \c 3x3 tanformation on the fly.
+@property (readonly, nonatomic) id<MTLComputePipelineState> computePartialHistogramsState;
+
+/// Compiled state of kernel that performs the second stage in the reduction, merging all the
+/// partial histograms computed in the first stage into a single histogram.
+@property (readonly, nonatomic) id<MTLComputePipelineState> mergeHistogramsState;
+
+/// Intermediate buffer for holding the partial histograms for each threadgroup.
+@property (readonly, nonatomic) id<MTLBuffer> partialHistogramsBuffer;
+
+/// Number of partial histograms computed on the first stage of the reduction.
+@property (readonly, nonatomic) NSUInteger partialHistogramsCount;
+
+@end
+
+@implementation PNKColorTransferHistogram
+
+/// Maximum number of histogram bins supported by the kernel, based on the available threadgroup
+/// memory on the lower end devices.
+static const uint kMaxHistogramBins = 1024;
+
+/// Maximum total threadgroup memory allocation of devices with 16KB limit, since in some iOS and
+/// tvOS feature sets, the driver may consume up to 32 bytes of a device's total threadgroup memory.
+static const NSUInteger kMaxThreadgroupMemoryLength16K = (1 << 14) - 32;
+
+/// Maximum total threadgroup memory allocation of devices with 32KB limit, since in some iOS and
+/// tvOS feature sets, the driver may consume up to 32 bytes of a device's total threadgroup memory.
+static const NSUInteger kMaxThreadgroupMemoryLength32K = (1 << 15) - 32;
+
+- (instancetype)initWithDevice:(id<MTLDevice>)device histogramBins:(NSUInteger)histogramBins
+                     inputSize:(NSUInteger)inputSize {
+  LTParameterAssert(inputSize > 0, @"Invalid input size, must be greater than zero");
+  LTParameterAssert(histogramBins >= 2 && histogramBins <= kMaxHistogramBins,
+                    @"Invalid histogram bins (%lu), must be in range [2,%lu].",
+                    (unsigned long)histogramBins, (unsigned long)kMaxHistogramBins);
+
+  if (self = [super init]) {
+    _device = device;
+    _histogramBins = histogramBins;
+    _inputSize = inputSize;
+    [self createComputeStates];
+    [self createBuffers];
+  }
+  return self;
+}
+
+- (void)createComputeStates {
+  uint size = (uint)self.inputSize;
+  ushort histogramBins = self.histogramBins;
+  auto constants = [[MTLFunctionConstantValues alloc] init];
+  [constants setConstantValue:&histogramBins type:MTLDataTypeUShort withName:@"kHistogramBins"];
+  [constants setConstantValue:&size type:MTLDataTypeUInt withName:@"kInputSize"];
+
+  _computePartialHistogramsState = self.isDeviceWithMaxThreadgroupMemoryOf32K ?
+      PNKCreateComputeStateWithConstants(self.device, @"computePartialHistograms32K", constants) :
+      PNKCreateComputeStateWithConstants(self.device, @"computePartialHistograms16K", constants);
+
+  _partialHistogramsCount = self.computePartialHistogramsState.maxTotalThreadsPerThreadgroup;
+  _mergeHistogramsState = self.partialHistogramsCount == 1024 ?
+      PNKCreateComputeStateWithConstants(self.device, @"mergeHistograms1024", constants):
+      PNKCreateComputeStateWithConstants(self.device, @"mergeHistograms512", constants);
+}
+
+- (BOOL)isDeviceWithMaxThreadgroupMemoryOf32K {
+  if (@available(iOS 11.0, *)) {
+    return self.device.maxThreadgroupMemoryLength >= kMaxThreadgroupMemoryLength32K;
+  }
+  return NO;
+}
+
+- (void)createBuffers {
+  auto bufferLength = self.partialHistogramsCount * self.histogramBins * 4 * sizeof(uint);
+  _partialHistogramsBuffer = [self.device newBufferWithLength:bufferLength
+                                                      options:MTLResourceStorageModePrivate];
+}
+
+- (void)encodeToCommandBuffer:(id<MTLCommandBuffer>)commandBuffer
+                  inputBuffer:(id<MTLBuffer>)inputBuffer
+              transformBuffer:(id<MTLBuffer>)transformBuffer
+               minValueBuffer:(id<MTLBuffer>)minValueBuffer
+               maxValueBuffer:(id<MTLBuffer>)maxValueBuffer
+              histogramBuffer:(id<MTLBuffer>)histogramBuffer {
+  auto *buffers = @[inputBuffer, transformBuffer, minValueBuffer, maxValueBuffer,
+                    self.partialHistogramsBuffer];
+  PNKComputeDispatch(self.computePartialHistogramsState, commandBuffer,
+                     buffers, @[], @"computePartialHistograms",
+                     {self.numberOfThreadsPerThreadgroupForComputePartialHistograms, 1, 1},
+                     {self.partialHistogramsCount, 1, 1});
+
+  PNKComputeDispatch(self.mergeHistogramsState, commandBuffer,
+                     @[self.partialHistogramsBuffer, histogramBuffer], @[], @"mergeHistograms",
+                     {self.partialHistogramsCount, 1, 1}, {self.histogramBins, 1, 1});
+}
+
+- (NSUInteger)numberOfThreadsPerThreadgroupForComputePartialHistograms {
+  auto warpSize = self.computePartialHistogramsState.threadExecutionWidth;
+  auto maxThreadsPerThreadgroup = self.computePartialHistogramsState.maxTotalThreadsPerThreadgroup;
+  auto maxMemoryLength = self.isDeviceWithMaxThreadgroupMemoryOf32K ?
+      kMaxThreadgroupMemoryLength32K : kMaxThreadgroupMemoryLength16K;
+
+  auto sharedBufferElements = maxMemoryLength / 3 / sizeof(uint);
+  auto maxNumberOfWarps = sharedBufferElements / self.histogramBins;
+  return std::min(maxNumberOfWarps * warpSize, maxThreadsPerThreadgroup);
+}
+
++ (NSUInteger)maxSupportedHistogramBins {
+  return kMaxHistogramBins;
+}
+
+@end
+
+#endif // PNK_USE_MPS
+
+NS_ASSUME_NONNULL_END
