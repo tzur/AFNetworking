14a10b9fb00279016472a7de6970bd038b397c0a
Barak Weiss <bweiss@lightricks.com>
2017-12-25 11:26:30 +0200

    Milkshake: initial commit.

---
+++ Milkshake/Milkshake/SHKPlaceholder.m
@@ -0,0 +1,12 @@
+// Copyright (c) 2017 Lightricks. All rights reserved.
+// Created by Barak Weiss.
+
+#import "SHKPlaceholder.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@implementation SHKPlaceholder
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Milkshake/MilkshakeTests/SHKPlaceholderSpec.mm
@@ -0,0 +1,11 @@
+// Copyright (c) 2017 Lightricks. All rights reserved.
+// Created by Barak Weiss.
+
+#import "SHKPlaceholder.h"
+
+SpecBegin(SHKPlaceholder)
+
+it(@"should act as placeholder", ^{
+});
+
+SpecEnd
