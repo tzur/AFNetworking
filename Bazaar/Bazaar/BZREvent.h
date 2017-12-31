8ed6995f39f19d0ad6674b19769c07f78e21c77c
Daniel Lahyani <dlahyani@lightricks.com>
2017-12-31 11:44:42 +0200

    BZREvent: Add new event type - BZREventTypeInformational.

--- Bazaar/Bazaar/BZREvent.h
+++ Bazaar/Bazaar/BZREvent.h
@@ -9,6 +9,7 @@ NS_ASSUME_NONNULL_BEGIN
 LTEnumDeclare(NSUInteger, BZREventType,
   BZREventTypeNonCriticalError,
   BZREventTypeCriticalError,
+  BZREventTypeInformational,
   BZREventTypeReceiptValidationStatusReceived
 );
 
