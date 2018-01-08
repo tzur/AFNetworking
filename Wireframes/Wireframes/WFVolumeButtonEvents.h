e3f88cb2ec58794201884c617ed12c2673598263
Michael Kimyagarov <mkimi@lightricks.com>
2018-01-08 16:00:58 +0200

    WFVolumeButtonEvents: initial commit.

---
+++ Wireframes/Wireframes/WFVolumeButtonEvents.h
@@ -0,0 +1,55 @@
+// Copyright (c) 2018 Lightricks. All rights reserved.
+// Created by Michael Kimyagarov.
+
+NS_ASSUME_NONNULL_BEGIN
+
+/// Supported volume button events.
+LTEnumDeclare(NSUInteger, WFVolumeButtonEvent,
+  /// Volume up button pressed.
+  WFVolumeButtonEventVolumeUpPress,
+  /// Volume up button released.
+  WFVolumeButtonEventVolumeUpRelease,
+  /// Volume down button pressed.
+  WFVolumeButtonEventVolumeDownPress,
+  /// Volume down button released.
+  WFVolumeButtonEventVolumeDownRelease
+);
+
+/// Returns a signal which sends \c WFVolumeButtonEvent each time user presses or releases devices
+/// volume buttons. The returned signal doesn't err or complete. It's equivalent to the following
+/// code:
+///
+/// @code
+/// WFVolumeButtonEvents([UIApplication sharedApplication]);
+/// @endcode
+NS_EXTENSION_UNAVAILABLE_IOS("") RACSignal<NSNumber *> *WFVolumeButtonEvents();
+
+/// Returns a signal which sends \c WFVolumeButtonEvent each time user presses or releases devices
+/// volume buttons. The returned signal doesn't err or complete. The given \c application is used to
+/// enable / disable the generation of volume button events.
+///
+/// @important volume event generation starts whenever the returned signal is subscribed. During
+/// that time the default behaviour of volume buttons is disabled. The default behavior is restored
+/// when there's no active subscriptions left to signals returned by this method.
+///
+/// @note whenever volume event generation is enabled the system (iOS) generates \c UIPressesEvent
+/// which is delivered to <tt>-[UIResponder presses{Began,Ended}:withEvent:]<\tt>. (Many key
+/// objects are also responders, including the \c UIApplication, \c UIViewController, and
+/// \c UIView). Make sure you understand the UIKit's event handing mechanism described in
+/// "Understanding Event Handling, Responders, and the Responder Chain"
+/// https://developer.apple.com/documentation/uikit/touches_presses_and_gestures/understanding_event_handling_responders_and_the_responder_chain
+///
+/// @note volume button events are generated differently by different accessories.
+/// 1. Bluetooth selfie stick can perform in following ways:
+///    a. Generate \c WFVolumeButtonEventVolumeUp{Press,Release} whenever the "take photo" button is
+///       pressed and released correspondingly.
+///    b. Generate a pair of \c WFVolumeButtonEventVolumeUp{Press,Release} events whenever the "take
+///       photo" button is released. No event is sent whenever the up volume button is pressed.
+/// 2. Apple's original earphones (with 3.5mm or Lightning jacks) generates a pair of
+///    \c WFVolumeButtonEventVolume{Up,Down}Press and \c WFVolumeButtonEventVolume{Up,Down}Relese
+///    events upon EACH press on the volume up / down button. When the button is released no event
+///    is sent.
+NS_EXTENSION_UNAVAILABLE_IOS("") RACSignal<NSNumber *> *
+    WFVolumeButtonEvents(UIApplication *application);
+
+NS_ASSUME_NONNULL_END
---
+++ Wireframes/WireframesTests/WFVolumeButtonEventsSpec.mm
@@ -0,0 +1,61 @@
+// Copyright (c) 2018 Lightricks. All rights reserved.
+// Created by Michael Kimyagarov.
+
+#import "WFVolumeButtonEvents.h"
+
+#import "WFVolumeButtonEvent+NotificationName.h"
+
+@interface UIApplication ()
+- (void)setWantsVolumeButtonEvents:(BOOL)enable;
+@end
+
+/// Posts a notification which corresponds to the given \c event using the default notification
+/// center.
+static void WFPostNotification(WFVolumeButtonEvent *event) {
+  [[NSNotificationCenter defaultCenter] postNotificationName:event.notificationName object:nil];
+}
+
+SpecBegin(WFVolumeButtonEvents)
+
+it(@"should return a signal which fires volume button events", ^{
+  auto recorder = [WFVolumeButtonEvents() testRecorder];
+  WFPostNotification($(WFVolumeButtonEventVolumeUpPress));
+  expect(recorder).will.sendValuesWithCount(1);
+  expect(recorder.values[0]).to.equal($(WFVolumeButtonEventVolumeUpPress));
+
+  WFPostNotification($(WFVolumeButtonEventVolumeUpRelease));
+  expect(recorder).will.sendValuesWithCount(2);
+  expect(recorder.values[1]).to.equal($(WFVolumeButtonEventVolumeUpRelease));
+
+  WFPostNotification($(WFVolumeButtonEventVolumeDownPress));
+  expect(recorder).will.sendValuesWithCount(3);
+  expect(recorder.values[2]).to.equal($(WFVolumeButtonEventVolumeDownPress));
+
+  WFPostNotification($(WFVolumeButtonEventVolumeDownRelease));
+  expect(recorder).will.sendValuesWithCount(4);
+  expect(recorder.values[3]).to.equal($(WFVolumeButtonEventVolumeDownRelease));
+});
+
+it(@"should fire the same event in multiple signals", ^{
+  auto recorder = [WFVolumeButtonEvents() testRecorder];
+  auto recorder2 = [WFVolumeButtonEvents() testRecorder];
+  WFPostNotification($(WFVolumeButtonEventVolumeUpPress));
+
+  expect(recorder).will.sendValues(@[$(WFVolumeButtonEventVolumeUpPress)]);
+  expect(recorder2).will.sendValues(@[$(WFVolumeButtonEventVolumeUpPress)]);
+});
+
+it(@"should disable events generation when no active subscription", ^{
+  UIApplication *applicationMock = OCMStrictClassMock([UIApplication class]);
+  OCMExpect([applicationMock setWantsVolumeButtonEvents:YES]);
+  OCMExpect([applicationMock setWantsVolumeButtonEvents:NO]);
+  auto events = WFVolumeButtonEvents(applicationMock);
+  auto events1 = WFVolumeButtonEvents(applicationMock);
+  auto disposable = [events subscribeNext:^(NSNumber * _Nullable) {}];
+  auto disposable1 = [events1 subscribeNext:^(NSNumber * _Nullable) {}];
+  [disposable dispose];
+  [disposable1 dispose];
+  OCMVerifyAll((id)applicationMock);
+});
+
+SpecEnd
