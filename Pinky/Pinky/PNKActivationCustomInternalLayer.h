18da5a8d01d5b4a33caa413ddb1830800907d462
Gershon Hochman <gershon@lightricks.com>
2018-01-14 13:21:57 +0200

    PNKActivationCustomInternalLayer: initial commit.

---
+++ Pinky/Pinky/PNKActivationCustomInternalLayer.h
@@ -0,0 +1,40 @@
+// Copyright (c) 2018 Lightricks. All rights reserved.
+// Created by Gershon Hochman.
+
+#import "PNKKernel.h"
+#import "PNKNeuralNetworkTypeDefinitions.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+#if PNK_USE_MPS
+
+namespace pnk {
+  struct ActivationKernelModel;
+}
+
+/// Layer performing activation of a tensor.
+@interface PNKActivationCustomInternalLayer : NSObject <PNKUnaryKernel>
+
+- (instancetype)init NS_UNAVAILABLE;
+
+/// Initializes a new layer that runs on \c device and performs activation as defined by
+/// \c activationModel.
+- (instancetype)initWithDevice:(id<MTLDevice>)device
+               activationModel:(const pnk::ActivationKernelModel &)activationModel
+    NS_DESIGNATED_INITIALIZER;
+
+/// Encodes the operation performed by the kernel to \c commandBuffer using \c inputImage as
+/// input. Output is written asynchronously to \c outputImage. \c outputImage must be the same size
+/// and number of channels as \c inputImage.
+- (void)encodeToCommandBuffer:(id<MTLCommandBuffer>)commandBuffer
+                   inputImage:(MPSImage *)inputImage outputImage:(MPSImage *)outputImage;
+
+/// Returns \c YES if the given \c activationType is supported by this class.
++ (BOOL)doesSupportActivationType:(pnk::ActivationType)type;
+
+@end
+
+#endif
+
+NS_ASSUME_NONNULL_END
+
