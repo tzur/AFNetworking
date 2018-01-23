7452284e6b898d763cc1fad634e846aefc397b88
Rouven Strauss <rouven@lightricks.com>
2018-01-23 15:13:08 +0100

    DVNBrushModelVersion: Initial commit.
    Adds the DVNBrushModelVersion enum, for the sake of type safety when
    dealing with different versions.

---
+++ DaVinci/DaVinci/DVNBrushModelVersion.h
@@ -0,0 +1,11 @@
+// Copyright (c) 2018 Lightricks. All rights reserved.
+// Created by Rouven Strauss.
+
+NS_ASSUME_NONNULL_BEGIN
+
+/// Enumeration of the existing brush versions provided by DaVinci.
+LTEnumDeclare(NSUInteger, DVNBrushModelVersion,
+  DVNBrushModelVersionV1
+);
+
+NS_ASSUME_NONNULL_END
---
+++ DaVinci/DaVinci/DVNBrushModelVersion.mm
@@ -0,0 +1,14 @@
+// Copyright (c) 2018 Lightricks. All rights reserved.
+// Created by Rouven Strauss.
+
+#import "DVNBrushModelVersion.h"
+
+#import "DVNBrushModel.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+LTEnumImplement(NSUInteger, DVNBrushModelVersion,
+  DVNBrushModelVersionV1
+);
+
+NS_ASSUME_NONNULL_END
