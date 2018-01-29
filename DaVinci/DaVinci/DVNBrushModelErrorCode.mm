3c18de466d061641b896c19b674f650788fb3416
Rouven Strauss <rouven@lightricks.com>
2018-01-29 11:08:31 +0100

    DVNBrushModelErrorCode: Initial commit.
    Adds DVNBrushModelErrorCode, an enumeration of error codes related to
    DVNBrushModel.

---
+++ DaVinci/DaVinci/DVNBrushModelErrorCode.mm
@@ -0,0 +1,13 @@
+// Copyright (c) 2018 Lightricks. All rights reserved.
+// Created by Rouven Strauss.
+
+#import "DVNBrushModelErrorCode.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+LTEnumImplement(NSUInteger, DVNBrushModelErrorCode,
+  DVNBrushModelErrorCodeNoSerializedVersion,
+  DVNBrushModelErrorCodeNoValidVersion
+);
+
+NS_ASSUME_NONNULL_END
