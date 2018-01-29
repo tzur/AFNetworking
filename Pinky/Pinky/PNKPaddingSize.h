bcf22019fac85a75ea27c907d4727a4dba238706
Gershon Hochman <gershon@lightricks.com>
2018-01-29 14:01:41 +0200

    PNKReflectionPadding: upgrade to non-symmetric padding.

---
+++ Pinky/Pinky/PNKPaddingSize.h
@@ -0,0 +1,18 @@
+// Copyright (c) 2018 Lightricks. All rights reserved.
+// Created by Gershon Hochman.
+
+namespace pnk {
+
+/// Structure that stores parameters for padding.
+struct PaddingSize {
+  /// Left padding.
+  NSUInteger left;
+  /// Top padding.
+  NSUInteger top;
+  /// Right padding.
+  NSUInteger right;
+  /// Bottom padding.
+  NSUInteger bottom;
+};
+
+} // namespace pnk
