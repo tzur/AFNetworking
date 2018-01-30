f3221869ec3126f24cde9baccd1e82fac9acf7eb
Gershon Hochman <gershon@lightricks.com>
2018-01-30 09:53:26 +0200

    PNKBufferUtils: replace MTLTexture-wrapper with MPSImage-wrapper.
    As we switch to using MPSImage in all our kernel APIs we also change
    this API that wraps a CVPixelBuffer coming from outside to produce
    MPSImage as well.

--- Pinky/Pinky/PNKPixelBufferUtils.h
+++ Pinky/Pinky/PNKPixelBufferUtils.h
@@ -9,14 +9,17 @@ NS_ASSUME_NONNULL_BEGIN
 /// formats are 1- and 4-channels non-planar formats with either uchar or half-float channels.
 void PNKAssertPixelBufferFormat(CVPixelBufferRef pixelBuffer);
 
-/// Creates a Metal texture wrapper of the \c pixelBuffer for use with \c device and returns it to
+/// Creates an \c MPSImage wrapper of the \c pixelBuffer for use with \c device and returns it to
 /// the caller. \c pixelBuffer must have one of supported pixel formats. Currently supported pixel
-/// formats are 1- and 4-channels non-planar formats with either uchar or half-float channels.
+/// formats are 1- and 4-channels non-planar formats with either uchar or half-float channels. The
+/// wrapper image will have feature channels count equal to \c featureChannels. If
+/// \c featureChannels is zero - the default channel count of the pixel buffer will be used.
 ///
 /// @important This function does not have any synchronization mechanisms whatsoever. The
 /// synchronization of reading/writing the \c pixelBuffer from the CPU side and reading/writing
 /// the Metal texture wrapper from the GPU side are the caller's responsibility.
-id<MTLTexture> PNKTextureFromPixelBuffer(CVPixelBufferRef pixelBuffer, id<MTLDevice> device);
+MPSImage *PNKImageFromPixelBuffer(CVPixelBufferRef pixelBuffer, id<MTLDevice> device,
+                                  NSUInteger featureChannels = 0);
 
 #endif
 
