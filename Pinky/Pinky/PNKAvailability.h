316b61eb24a75d470b2c04de1b168adf0af0caa4
Ofir Bibi <ofir@lightricks.com>
2018-02-01 14:37:33 +0200

    PNKAvailability: remove MPS guards on function definition.

--- Pinky/Pinky/PNKAvailability.h
+++ Pinky/Pinky/PNKAvailability.h
@@ -3,11 +3,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-#if PNK_USE_MPS
-
 /// Determines whether the Metal Performance Shaders framework supports a Metal device.
 BOOL PNKSupportsMTLDevice(id<MTLDevice> device);
-
-#endif
 
 NS_ASSUME_NONNULL_END
