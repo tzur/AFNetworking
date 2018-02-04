d57dc4c3e0d11cfa154abb07ff07d7b899a1cae8
Dekel Avrahami <dekel@lightricks.com>
2018-02-04 10:01:14 +0200

    WFVideoViewTests: Fail tests on error from player.
    Flakiness of tests that practice playback in WFVideoViewTests in CI has
    been observed. It is probably because at some conditions an error is
    reported from the player. This (temporary) commit is in order to get the
    details of this error from CI and understand what causes it and how to
    fix this flakiness.

--- Wireframes/WireframesTests/WFFakeVideoViewDelegate.mm
+++ Wireframes/WireframesTests/WFFakeVideoViewDelegate.mm
@@ -17,6 +17,15 @@ NS_ASSUME_NONNULL_BEGIN
     didPlayVideoAtTime:(__unused NSTimeInterval)time {
 }
 
+- (void)videoView:(WFVideoView __unused *)videoView
+    didEncounterVideoError:(NSError *)error {
+  // Fail the test. Flakiness of playback tests in CI has been observed. It is probably because at
+  // some conditions an error is reported from the player. This (temporary) assertion is in order to
+  // get the details of this error from CI and understand what causes it and how to fix this
+  // flakiness.
+  LTAssert(NO, @"Video view did encounter video error: %@", error);
+}
+
 @end
 
 NS_ASSUME_NONNULL_END
