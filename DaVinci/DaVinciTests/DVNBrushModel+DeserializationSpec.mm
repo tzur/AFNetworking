fe108158c8a00e73d269a5896cb4c2cb5529d1b0
Rouven Strauss <rouven@lightricks.com>
2018-02-12 13:45:30 +0200

    DVNBrushModel+Deserialization: Implement shared examples.

--- DaVinci/DaVinciTests/DVNBrushModel+DeserializationSpec.mm
+++ DaVinci/DaVinciTests/DVNBrushModel+DeserializationSpec.mm
@@ -4,47 +4,70 @@
 #import "DVNBrushModel+Deserialization.h"
 
 #import "DVNBrushModelErrorCode.h"
-#import "DVNBrushModelV1.h"
+#import "DVNBrushModelVersion+TestBrushModel.h"
+
+static NSString * const kDVNBrushModelVersionDeserializationExamples =
+    @"DVNBrushModelVersionDeserializationExamples";
+static NSString * const kDVNBrushModelVersionDeserializationExamplesJSONDictionary =
+    @"DVNBrushRenderConfigurationProviderExamplesModel";
+static NSString * const kDVNBrushModelVersionDeserializationExamplesClass =
+    @"DVNBrushModelVersionDeserializationExamplesClass";
+
+SharedExamplesBegin(DVNBrushModelVersionDeserializationExamples)
+
+sharedExamplesFor(kDVNBrushModelVersionDeserializationExamples, ^(NSDictionary *data) {
+  __block NSDictionary *jsonDictionary;
+  __block Class expectedClass;
+
+  beforeEach(^{
+    jsonDictionary = data[kDVNBrushModelVersionDeserializationExamplesJSONDictionary];
+    expectedClass = data[kDVNBrushModelVersionDeserializationExamplesClass];
+  });
+
+  context(@"model creation from JSON dictionary", ^{
+    it(@"should correctly construct brush model without error", ^{
+      NSError * _Nullable error;
+      DVNBrushModel * _Nullable model = [DVNBrushModel modelFromJSONDictionary:jsonDictionary
+                                                                         error:&error];
+      expect(model).toNot.beNil();
+      expect(model).to.beMemberOf(expectedClass);
+      expect(error).to.beNil();
+    });
+
+    it(@"should return nil and populate given error if dictionary does not specify version", ^{
+      NSError * _Nullable error;
+      DVNBrushModel * _Nullable model = [DVNBrushModel modelFromJSONDictionary:@{} error:&error];
+      expect(model).to.beNil();
+      expect(error).toNot.beNil();
+      expect(error.code).to.equal($(DVNBrushModelErrorCodeNoSerializedVersion).value);
+    });
+
+    it(@"should return nil and populate given error if dictionary specifies invalid version", ^{
+      NSError * _Nullable error;
+      DVNBrushModel * _Nullable model =
+          [DVNBrushModel modelFromJSONDictionary:@{@"version": @"invalidVersion"}
+                                           error:&error];
+      expect(model).to.beNil();
+      expect(error).toNot.beNil();
+      expect(error.code).to.equal($(DVNBrushModelErrorCodeNoValidVersion).value);
+    });
+  });
+});
+
+SharedExamplesEnd
 
 SpecBegin(DVNBrushModelVersion_Deserialization)
 
-__block NSDictionary *jsonDictionary;
-
-beforeEach(^{
-  NSString *filePath = [[NSBundle bundleForClass:[self class]]
-                        pathForResource:@"DVNTestBrushModelV1" ofType:@"json"];
-  NSData *data = [NSData dataWithContentsOfFile:filePath];
-  jsonDictionary = [NSJSONSerialization JSONObjectWithData:data options:(NSJSONReadingOptions)0
-                                                     error:nil];
-});
-
-context(@"model creation from JSON dictionary", ^{
-  it(@"should correctly construct brush model of version 1 without error", ^{
-    NSError * _Nullable error;
-    DVNBrushModel * _Nullable model = [DVNBrushModel modelFromJSONDictionary:jsonDictionary
-                                                                       error:&error];
-    expect(model).toNot.beNil();
-    expect(model).to.beMemberOf([DVNBrushModelV1 class]);
-    expect(error).to.beNil();
-  });
-
-  it(@"should return nil and populate given error if dictionary does not specify version", ^{
-    NSError * _Nullable error;
-    DVNBrushModel * _Nullable model = [DVNBrushModel modelFromJSONDictionary:@{} error:&error];
-    expect(model).to.beNil();
-    expect(error).toNot.beNil();
-    expect(error.code).to.equal($(DVNBrushModelErrorCodeNoSerializedVersion).value);
-  });
-
-  it(@"should return nil and populate given error if dictionary does not specify valid version", ^{
-    NSError * _Nullable error;
-    DVNBrushModel * _Nullable model =
-        [DVNBrushModel modelFromJSONDictionary:@{@"version": @"invalidVersion"}
-                                         error:&error];
-    expect(model).to.beNil();
-    expect(error).toNot.beNil();
-    expect(error.code).to.equal($(DVNBrushModelErrorCodeNoValidVersion).value);
-  });
+context(@"deserialization", ^{
+  for (DVNBrushModelVersion *version in [DVNBrushModelVersion fields]) {
+    itShouldBehaveLike(kDVNBrushModelVersionDeserializationExamples, ^{
+      return @{
+        kDVNBrushModelVersionDeserializationExamplesJSONDictionary:
+            [version JSONDictionaryOfTestBrushModel],
+        kDVNBrushModelVersionDeserializationExamplesClass: [version classOfBrushModel]
+      };
+    });
+  }
 });
 
 SpecEnd
---
+++ DaVinci/DaVinciTests/DVNBrushModelVersion+TestBrushModel.mm
@@ -0,0 +1,39 @@
+// Copyright (c) 2018 Lightricks. All rights reserved.
+// Created by Rouven Strauss.
+
+#import "DVNBrushModelVersion+TestBrushModel.h"
+
+#import "DVNBrushModel+Deserialization.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@implementation DVNBrushModelVersion (TestBrushModel)
+
+#pragma mark -
+#pragma mark Public API
+#pragma mark -
+
+- (NSDictionary *)JSONDictionaryOfTestBrushModel {
+  return [self brushModelJSONDictionaryFromFileWithName:[self testFileName]];
+}
+
+#pragma mark -
+#pragma mark Auxiliary Class Methods
+#pragma mark -
+
+- (NSString *)testFileName {
+  switch (self.value) {
+    case DVNBrushModelVersionV1:
+      return @"DVNTestBrushModelV1";
+  }
+}
+
+- (NSDictionary *)brushModelJSONDictionaryFromFileWithName:(NSString *)name {
+  NSString *filePath = [[NSBundle bundleForClass:[self class]] pathForResource:name ofType:@"json"];
+  NSData *data = [NSData dataWithContentsOfFile:filePath];
+  return [NSJSONSerialization JSONObjectWithData:data options:(NSJSONReadingOptions)0 error:nil];
+}
+
+@end
+
+NS_ASSUME_NONNULL_END
