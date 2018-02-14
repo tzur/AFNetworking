79d0cf4688b92bdeeef49b24dc649a891f1019ca
Ofir Bibi <ofir@lightricks.com>
2018-02-14 10:11:52 +0200

    PNKStyleTransferProcessor: initial commit.

---
+++ Pinky/Pinky/PNKStyleTransferState.h
@@ -0,0 +1,18 @@
+// Copyright (c) 2018 Lightricks. All rights reserved.
+// Created by Ofir Bibi.
+
+NS_ASSUME_NONNULL_BEGIN
+
+/// Opaque class used to cache the state of \c PNKStyleTransferProcessor for an input image that was
+/// processed when this state was created. Using this state with the processor allows removing
+/// processing stages and shortening the processing time.
+///
+/// @note This class may hold large objects and textures mapped to GPU memory.
+API_AVAILABLE(ios(10.0))
+@interface PNKStyleTransferState : NSObject
+
+- (instancetype)init NS_UNAVAILABLE;
+
+@end
+
+NS_ASSUME_NONNULL_END
