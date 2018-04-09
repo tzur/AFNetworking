fa15739d8a54b3f870fc053b67defd37fa67e556
Gershon Hochman <gershon@lightricks.com>
2018-04-09 16:08:55 +0300

    PNKFillWithZeroesKernel: initial commit.

---
+++ Pinky/PinkyDeviceTests/PNKFillWithZeroesKernel.h
@@ -0,0 +1,26 @@
+// Copyright (c) 2018 Lightricks. All rights reserved.
+// Created by Gershon Hochman.
+
+NS_ASSUME_NONNULL_BEGIN
+
+#if PNK_USE_MPS
+
+/// Kernel filling the output image with zeroes. Can be used on images in private GPU memory such as
+/// \c MPSTemporaryImage that cannot be accessed by CPU.
+@interface PNKFillWithZeroesKernel : NSObject
+
+- (instancetype)init NS_UNAVAILABLE;
+
+/// Initializes a new kernel that runs on \c device and fills the output image with zeroes.
+- (instancetype)initWithDevice:(id<MTLDevice>)device NS_DESIGNATED_INITIALIZER;
+
+/// Encodes the operation performed by the kernel to \c commandBuffer. On kernel completion
+/// \c outputImage will be filled with zeroes.
+- (void)encodeToCommandBuffer:(id<MTLCommandBuffer>)commandBuffer
+                  outputImage:(MPSImage *)outputImage;
+
+@end
+
+#endif
+
+NS_ASSUME_NONNULL_END
---
+++ Pinky/PinkyDeviceTests/PNKFillWithZeroesKernel.mm
@@ -0,0 +1,64 @@
+// Copyright (c) 2018 Lightricks. All rights reserved.
+// Created by Gershon Hochman.
+
+#import "PNKFillWithZeroesKernel.h"
+
+#import "PNKTestComputeState.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+#if PNK_USE_MPS
+
+@interface PNKFillWithZeroesKernel ()
+
+/// Device to encode this kernel operation.
+@property (readonly, nonatomic) id<MTLDevice> device;
+
+/// Kernel state to encode operation on a single texture.
+@property (readonly, nonatomic) id<MTLComputePipelineState> stateSingle;
+
+/// Kernel state to encode operation on a texture array.
+@property (readonly, nonatomic) id<MTLComputePipelineState> stateArray;
+
+@end
+
+@implementation PNKFillWithZeroesKernel
+
+/// Name of kernel function for performing operation on a single texture.
+static NSString * const kKernelFunctionSingle = @"fillWithZeroesSingle";
+
+/// Name of kernel function for performing operation on a texture array.
+static NSString * const kKernelFunctionArray = @"fillWithZeroesArray";
+
+/// Family name of the kernel functions for debug purposes.
+static NSString * const kDebugGroupName = @"fillWithZeroes";
+
+- (instancetype)initWithDevice:(id<MTLDevice>)device {
+  if (self = [super init]) {
+    _device = device;
+    [self createStates];
+  }
+  return self;
+}
+
+- (void)createStates {
+  _stateSingle = PNKCreateTestComputeState(self.device, kKernelFunctionSingle);
+  _stateArray = PNKCreateTestComputeState(self.device, kKernelFunctionArray);
+}
+
+- (void)encodeToCommandBuffer:(id<MTLCommandBuffer>)commandBuffer
+                  outputImage:(MPSImage *)outputImage {
+  MTLSize workingSpaceSize = outputImage.pnk_textureArraySize;
+
+  bool isSingle = outputImage.pnk_isSingleTexture;
+  auto state = isSingle ? self.stateSingle : self.stateArray;
+
+  PNKComputeDispatchWithDefaultThreads(state, commandBuffer, @[], @[outputImage], kDebugGroupName,
+                                       workingSpaceSize);
+}
+
+@end
+
+#endif
+
+NS_ASSUME_NONNULL_END
