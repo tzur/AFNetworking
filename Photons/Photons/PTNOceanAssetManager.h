97764791d0793045fef3b5c1ca97d61dfc65c774
Barak Weiss <bweiss@lightricks.com>
2018-05-06 10:05:12 +0300

    PTNOceanAssetManager: Use PTNOceanClient.

--- Photons/Photons/PTNOceanAssetManager.h
+++ Photons/Photons/PTNOceanAssetManager.h
@@ -5,12 +5,13 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@class PTNDateProvider, FBRHTTPClient;
+@class PTNDateProvider, PTNOceanClient;
 
 /// Asset manager for fetching Ocean based assets. Supported URLs for album and descriptor fetching
 /// should have <tt>[NSURL ptn_oceanScheme]</tt> scheme, \c album or \c asset host and \c phrase or
-/// \c id query parameters, respectively. Fetching images data and audiovisual assets is
-/// unsupported. Albums returned by this manager will be set with maximum age for caching of five
+/// \c id query parameters, respectively.
+///
+/// Albums returned by this manager will be set with maximum age for caching of five
 /// minutes, album descriptors with maximum possible age and asset related ones will be set with
 /// maximum age of a single day.
 @interface PTNOceanAssetManager : NSObject <PTNCacheAwareAssetManager>
@@ -20,7 +21,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 /// Initializes with the given \c client and \c dateProvider. The given \c dateProvider is used for
 /// providing initial time reference for the maximum ages of the cached objects.
-- (instancetype)initWithClient:(FBRHTTPClient *)client
+- (instancetype)initWithClient:(PTNOceanClient *)client
                   dateProvider:(PTNDateProvider *)dateProvider NS_DESIGNATED_INITIALIZER;
 
 @end
