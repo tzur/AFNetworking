69eb18c7b3440acfb3192e704d6a4f4cfa9f9c81
Gershon Hochman <gershon@lightricks.com>
2018-05-22 09:47:20 +0300

    PNKImageMotionStaticLayer: synthesise property, use utils.

--- Pinky/Pinky/PNKImageMotionStaticLayer.mm
+++ Pinky/Pinky/PNKImageMotionStaticLayer.mm
@@ -3,16 +3,13 @@
 
 #import "PNKImageMotionStaticLayer.h"
 
+#import "PNKImageMotionLayerUtils.h"
+
 NS_ASSUME_NONNULL_BEGIN
 
-@interface PNKImageMotionStaticLayer ()
-
-/// Size of the original image.
-@property (nonatomic) cv::Size imageSize;
-
-@end
-
 @implementation PNKImageMotionStaticLayer
+
+@synthesize imageSize = _imageSize;
 
 - (instancetype)initWithImageSize:(cv::Size)imageSize {
   if (self = [super init]) {
@@ -22,18 +19,8 @@ NS_ASSUME_NONNULL_BEGIN
 }
 
 - (void)displacements:(cv::Mat *)displacements forTime:(__unused NSTimeInterval)time {
-  [self validateDisplacementsMatrix:displacements];
+  PNKImageMotionValidateDisplacementsMatrix(*displacements, self.imageSize);
   *displacements = 0;
-}
-
-- (void)validateDisplacementsMatrix:(cv::Mat *)displacements {
-  LTParameterAssert(displacements->size() == self.imageSize, @"Displacements matrix should be of "
-                    "size (%d, %d), got (%d, %d)", self.imageSize.height, self.imageSize.width,
-                    displacements->rows, displacements->cols);
-  LTParameterAssert(displacements->channels() == 2, @"Displacements matrix should have 2 "
-                    "channels, got %d", displacements->channels());
-  LTParameterAssert(displacements->depth() == CV_16F, @"Displacements matrix should be of "
-                    "half-float type (%d), got %d", CV_16F, displacements->depth());
 }
 
 @end
