f25f5a7eadf400c6ba94d17bc9ec7f52e8b215e5
Gershon Hochman <gershon@lightricks.com>
2018-05-22 09:44:58 +0300

    PNKImageMotionLayerExamples: initial commit.

---
+++ Pinky/PinkyDeviceTests/PNKImageMotionLayerExamples.h
@@ -0,0 +1,15 @@
+// Copyright (c) 2018 Lightricks. All rights reserved.
+// Created by Gershon Hochman.
+
+/// Group name of shared tests for \c PNKImageMotionLayer implementations.
+extern NSString * const kPNKImageMotionLayerExamples;
+
+/// Dictionary key to the object whose implementation of the \c PNKImageMotionLayer protocol has to
+/// be tested.
+extern NSString * const kPNKImageMotionLayerExamplesLayer;
+
+/// Dictionary key stating the width of the motion map to produce.
+extern NSString * const kPNKImageMotionLayerExamplesImageWidth;
+
+/// Dictionary key stating the height of the motion map to produce.
+extern NSString * const kPNKImageMotionLayerExamplesImageHeight;
---
+++ Pinky/PinkyDeviceTests/PNKImageMotionLayerExamples.mm
@@ -0,0 +1,72 @@
+// Copyright (c) 2018 Lightricks. All rights reserved.
+// Created by Gershon Hochman.
+
+#import "PNKImageMotionLayerExamples.h"
+
+#import "PNKImageMotionLayer.h"
+
+NSString * const kPNKImageMotionLayerExamples = @"PNKImageMotionLayerExamples";
+NSString * const kPNKImageMotionLayerExamplesLayer = @"PNKImageMotionLayerExamplesLayer";
+NSString * const kPNKImageMotionLayerExamplesImageWidth = @"PNKImageMotionLayerExamplesImageWidth";
+NSString * const kPNKImageMotionLayerExamplesImageHeight =
+    @"PNKImageMotionLayerExamplesImageHeight";
+
+SharedExamplesBegin(PNKImageMotionLayerExamples)
+
+sharedExamplesFor(kPNKImageMotionLayerExamples, ^(NSDictionary *data) {
+  context(@"PNKImageMotionLayer protocol", ^{
+    __block id<PNKImageMotionLayer> layer;
+    __block int imageWidth;
+    __block int imageHeight;
+
+    beforeEach(^{
+      layer = data[kPNKImageMotionLayerExamplesLayer];
+      imageWidth = [data[kPNKImageMotionLayerExamplesImageWidth] intValue];
+      imageHeight = [data[kPNKImageMotionLayerExamplesImageHeight] intValue];
+    });
+
+    it(@"should raise when displacements matrix has 1 channel", ^{
+      __block cv::Mat1hf displacements(imageHeight, imageWidth);
+      expect(^{
+        [layer displacements:&displacements forTime:0];
+      }).to.raise(NSInvalidArgumentException);
+    });
+
+    it(@"should raise when displacements matrix is not of half-float type", ^{
+      __block cv::Mat2f displacements(imageHeight, imageWidth);
+      expect(^{
+        [layer displacements:&displacements forTime:0];
+      }).to.raise(NSInvalidArgumentException);
+    });
+
+    it(@"should raise when displacements matrix has wrong height", ^{
+      __block cv::Mat2hf displacements(imageHeight + 1, imageWidth);
+      expect(^{
+        [layer displacements:&displacements forTime:0];
+      }).to.raise(NSInvalidArgumentException);
+    });
+
+    it(@"should raise when displacements matrix has wrong width", ^{
+      __block cv::Mat2hf displacements(imageHeight, imageWidth + 1);
+      expect(^{
+        [layer displacements:&displacements forTime:0];
+      }).to.raise(NSInvalidArgumentException);
+    });
+
+    it(@"should return the correct image size", ^{
+      auto imageSize = layer.imageSize;
+      expect(imageSize.width).to.equal(imageWidth);
+      expect(imageSize.height).to.equal(imageHeight);
+    });
+
+    it(@"should return zero displacements for zero time", ^{
+      cv::Mat2hf displacements(imageHeight, imageWidth);
+      [layer displacements:&displacements forTime:0];
+
+      auto expectedDisplacements = cv::Mat2hf::zeros(imageHeight, imageWidth);
+      expect($(displacements)).to.equalMat($(expectedDisplacements));
+    });
+  });
+});
+
+SharedExamplesEnd
