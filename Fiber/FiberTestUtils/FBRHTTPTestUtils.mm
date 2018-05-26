53f0f437bb6b9f8f3a421e910d8d7ef166a18868
Daniel Lahyani <dlahyani@lightricks.com>
2018-05-26 12:48:09 +0300

    FBRHTTPTestUtils: Add function to generate fake response with JSON.

--- Fiber/FiberTestUtils/FBRHTTPTestUtils.mm
+++ Fiber/FiberTestUtils/FBRHTTPTestUtils.mm
@@ -4,6 +4,7 @@
 #import "FBRHTTPTestUtils.h"
 
 #import <Fiber/FBRHTTPResponse.h>
+#import <Mantle/Mantle.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
@@ -14,6 +15,29 @@ FBRHTTPResponse *FBRFakeHTTPResponse(NSString *requestURL, NSUInteger statusCode
                                               statusCode:statusCode HTTPVersion:@"1.1"
                                             headerFields:headers];
   return [[FBRHTTPResponse alloc] initWithMetadata:metadata content:content];
+}
+
+FBRHTTPResponse *FBRFakeHTTPJSONResponse(NSString *requestURL, id JSONObject,
+                                         NSUInteger statusCode,
+                                         FBRHTTPRequestHeaders * _Nullable headers) {
+  if ([JSONObject conformsToProtocol:@protocol(MTLJSONSerializing)]) {
+    JSONObject = [MTLJSONAdapter JSONDictionaryFromModel:JSONObject];
+  }
+
+  NSData *content = [NSJSONSerialization dataWithJSONObject:JSONObject options:0 error:nil];
+  LTAssert(content, @"Invalid JSONObject provided. Expected JSON NSArray or NSDictionary or JSON "
+           "serializable model, got %@ of kind %@", JSONObject, [JSONObject class]);
+
+  FBRHTTPRequestHeaders *actualHeaders = @{
+    @"Content-Type": @"application/json",
+    @"Content-Length": [NSString stringWithFormat:@"%lu", (unsigned long)content.length]
+  };
+
+  if (headers) {
+    actualHeaders = [actualHeaders mtl_dictionaryByAddingEntriesFromDictionary:headers];
+  }
+
+  return FBRFakeHTTPResponse(requestURL, statusCode, actualHeaders, content);
 }
 
 NS_ASSUME_NONNULL_END
