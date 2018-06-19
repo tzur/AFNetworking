f1365fbae4ee107cb2614f140275e0402ed3910f
Gershon Hochman <gershon@lightricks.com>
2018-06-19 15:17:39 +0300

    PNKImageMotionTreeLayer: moving segmentation from initialiser to setter.

--- Pinky/Pinky/PNKImageMotionTreeLayer.h
+++ Pinky/Pinky/PNKImageMotionTreeLayer.h
@@ -7,23 +7,26 @@ NS_ASSUME_NONNULL_BEGIN
 
 /// Class for calculating displacements of a tree layer. The tree layer is separated into groups of
 /// trees such that each group receives its own motion vector.
-@interface PNKImageMotionTreeLayer : NSObject<PNKImageMotionLayer>
+@interface PNKImageMotionTreeLayer : NSObject<PNKImageMotionLayer,
+    PNKImageMotionSegmentationAwareLayer>;
 
 - (instancetype)init NS_UNAVAILABLE;
 
-/// Initializes with the segmentation map, number of samples and motion amplitude.
+/// Initializes with the image size, number of samples and motion amplitude.
 ///
-/// @param segmentation Segmentation map. Pixels with value \c pnk::LayerTypeTrees are assumed to
-/// belong to trees.
+/// @param imageSize Size of the displacement map that will be created.
 ///
 /// @param numberOfSamples Number of samples in time used to simulate the movement of a tree tip.
 /// Each sample corresponds to a single frame. The movement is periodical with period of
 /// \c numberOfSamples samples (frames). \c numberOfSamples must be a power of 2.
 ///
 /// @param amplitude Amplitude of tree tip oscillation.
-- (instancetype)initWithSegmentation:(const cv::Mat &)segmentation
-                     numberOfSamples:(NSUInteger)numberOfSamples
-                           amplitude:(CGFloat)amplitude NS_DESIGNATED_INITIALIZER;
+- (instancetype)initWithImageSize:(cv::Size)imageSize numberOfSamples:(NSUInteger)numberOfSamples
+                        amplitude:(CGFloat)amplitude NS_DESIGNATED_INITIALIZER;
+
+/// Segmentation map. The map's size must match \c imageSize. The segmentation map must be set
+/// before calling the \c displacements:forTime: method.
+@property (nonatomic) cv::Mat1b segmentation;
 
 @end
 
--- Pinky/PinkyDeviceTests/PNKImageMotionTreeLayerSpec.mm
+++ Pinky/PinkyDeviceTests/PNKImageMotionTreeLayerSpec.mm
@@ -8,20 +8,29 @@
 
 SpecBegin(PNKImageMotionTreeLayer)
 
-itShouldBehaveLike(kPNKImageMotionLayerExamples, ^{
-  // Image width.
-  static const int kImageWidth = 256;
-  // Image height.
-  static const int kImageHeight = 128;
+/// Image width.
+static const int kImageWidth = 256;
 
+/// Image height.
+static const int kImageHeight = 128;
+
+/// Number of samples.
+static const NSUInteger kNumberOfSamples = 32;
+
+/// Amplitude.
+static const CGFloat kAmplitude = 1;
+
+itShouldBehaveLike(kPNKImageMotionLayerExamples, ^{
   cv::Mat1b segmentation(kImageHeight, kImageWidth, (uchar)0);
   segmentation(cv::Rect(kImageWidth / 8, 0, kImageWidth / 4, kImageHeight)) =
       pnk::ImageMotionLayerTypeTrees;
   segmentation(cv::Rect(kImageWidth * 5 / 8, 0, kImageWidth / 4, kImageHeight)) =
       pnk::ImageMotionLayerTypeTrees;
 
-  auto treeLayer = [[PNKImageMotionTreeLayer alloc] initWithSegmentation:segmentation
-                                                         numberOfSamples:32 amplitude:1];
+  auto treeLayer = [[PNKImageMotionTreeLayer alloc]
+                    initWithImageSize:cv::Size(kImageWidth, kImageHeight)
+                    numberOfSamples:kNumberOfSamples amplitude:kAmplitude];
+  treeLayer.segmentation = segmentation;
 
   return @{
     kPNKImageMotionLayerExamplesLayer: treeLayer,
@@ -33,9 +42,40 @@ itShouldBehaveLike(kPNKImageMotionLayerExamples, ^{
 it(@"should raise when number of samples is not a power of 2", ^{
   expect(^{
     auto __unused treeLayer = [[PNKImageMotionTreeLayer alloc]
-                               initWithSegmentation:cv::Mat1b(64, 64, (uchar)0) numberOfSamples:31
-                               amplitude:1];
+                               initWithImageSize:cv::Size(kImageWidth, kImageHeight)
+                               numberOfSamples:kNumberOfSamples - 1 amplitude:kAmplitude];
   }).to.raise(NSInvalidArgumentException);
+});
+
+it(@"should raise when segmentation width differs from image width", ^{
+  auto treeLayer = [[PNKImageMotionTreeLayer alloc]
+                    initWithImageSize:cv::Size(kImageWidth, kImageHeight)
+                    numberOfSamples:kNumberOfSamples amplitude:kAmplitude];
+  cv::Mat1b segmentation(kImageHeight, kImageWidth + 1, (uchar)0);
+  expect(^{
+    treeLayer.segmentation = segmentation;
+  }).to.raise(NSInvalidArgumentException);
+});
+
+it(@"should raise when segmentation height differs from image height", ^{
+  auto treeLayer = [[PNKImageMotionTreeLayer alloc]
+                    initWithImageSize:cv::Size(kImageWidth, kImageHeight)
+                    numberOfSamples:kNumberOfSamples amplitude:kAmplitude];
+  cv::Mat1b segmentation(kImageHeight + 1, kImageWidth, (uchar)0);
+  expect(^{
+    treeLayer.segmentation = segmentation;
+  }).to.raise(NSInvalidArgumentException);
+});
+
+it(@"should raise when trying to get displacements without setting segmentation", ^{
+  auto treeLayer = [[PNKImageMotionTreeLayer alloc]
+                    initWithImageSize:cv::Size(kImageWidth, kImageHeight)
+                    numberOfSamples:kNumberOfSamples amplitude:kAmplitude];
+
+  __block cv::Mat1hf displacements(kImageHeight, kImageWidth);
+  expect(^{
+    [treeLayer displacements:&displacements forTime:0];
+  }).to.raise(NSInternalInconsistencyException);
 });
 
 SpecEnd
