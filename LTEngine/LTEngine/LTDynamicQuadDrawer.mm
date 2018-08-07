e6b686035100eff403388e2a008e3398284bf68c
Rouven Strauss <rouven@lightricks.com>
2018-08-07 10:30:48 +0200

    LTDynamicQuadDrawer: Cache attribute data for canonical square.
    This commit optimizes LTDynamicQuadDrawer to cache the attribute data
    for the very frequently occurring case in which the values for both the
    position and the texcoord equal the canonical square quad. This yields
    a performance boost of 30%, averaged over 2000 runs.

--- LTEngine/LTEngine/LTDynamicQuadDrawer.mm
+++ LTEngine/LTEngine/LTDynamicQuadDrawer.mm
@@ -92,8 +92,16 @@ auxiliaryTextures:(NSDictionary<NSString *, LTTexture *> *)uniformsToAuxiliaryTe
     return;
   }
 
-  attributeData = [attributeData arrayByAddingObject:[self attributeDataForQuads:quads
-                                                                 textureMapQuads:textureMapQuads]];
+  // Since draw calls with a single canonical quad used for both the rendered geometry and texture
+  // coordinates are common, a corresponding cached \c LTAttributeData singleton is used for
+  // optimization purposes.
+  LTAttributeData *attributeDataForInternalGPUStruct =
+      quads == kCanonicalQuads && textureMapQuads == kCanonicalQuads ?
+      [[self class] canonicalSquareAttributeData] :
+      [[self class] attributeDataForQuads:quads textureMapQuads:textureMapQuads
+                                gpuStruct:self.gpuStruct];
+
+  attributeData = [attributeData arrayByAddingObject:attributeDataForInternalGPUStruct];
 
   NSDictionary<NSString *, LTTexture *> *uniformsToTextures =
       [self mappingAugmentedWithTexture:texture fromMapping:uniformsToAuxiliaryTextures];
@@ -104,52 +112,81 @@ auxiliaryTextures:(NSDictionary<NSString *, LTTexture *> *)uniformsToAuxiliaryTe
                             uniforms:uniforms];
 }
 
-- (LTAttributeData *)attributeDataForQuads:(const std::vector<lt::Quad> &)quads
-                           textureMapQuads:(const std::vector<lt::Quad> &)textureMapQuads {
+/// Collection containing a single canonical square quad.
+static const std::vector<lt::Quad> kCanonicalQuads = {lt::Quad::canonicalSquare()};
+
++ (LTAttributeData *)canonicalSquareAttributeData {
+  static LTAttributeData *attributeData;
+
+  static dispatch_once_t onceToken;
+  dispatch_once(&onceToken, ^{
+    LTGPUStruct *gpuStruct =
+        [[LTGPUStructRegistry sharedInstance] structForName:kLTQuadDrawerGPUStructName];
+    attributeData = [self attributeDataForQuads:kCanonicalQuads textureMapQuads:kCanonicalQuads
+                                      gpuStruct:gpuStruct];
+  });
+
+  return attributeData;
+}
+
++ (LTAttributeData *)attributeDataForQuads:(const std::vector<lt::Quad> &)quads
+                           textureMapQuads:(const std::vector<lt::Quad> &)textureMapQuads
+                                 gpuStruct:(LTGPUStruct *)gpuStruct {
+  static const NSUInteger kNumberOfTriangleCorners = 3;
+  static const NSUInteger kNumberOfTrianglesPerQuad = 2;
+  static const NSUInteger kNumberOfTriangleCornersPerQuad =
+      kNumberOfTriangleCorners * kNumberOfTrianglesPerQuad;
+  static const NSUInteger kNumberOfQuadCorners = 4;
+
   NSUInteger numberOfQuads = quads.size();
 
   LTParameterAssert(numberOfQuads);
   LTParameterAssert(textureMapQuads.size() == numberOfQuads);
 
-  std::vector<LTDynamicQuadDrawerVertex> attributes;
-  attributes.reserve(numberOfQuads);
+  NSUInteger dataLengthInBytes =
+      numberOfQuads * kNumberOfTriangleCornersPerQuad * sizeof(LTDynamicQuadDrawerVertex);
+  NSMutableData *mutableData = [NSMutableData dataWithLength:dataLengthInBytes];
+  LTDynamicQuadDrawerVertex *entry = (LTDynamicQuadDrawerVertex *)mutableData.mutableBytes;
+
   CGFloat z = 0;
+  NSUInteger entryIndex = 0;
 
   for (NSUInteger i = 0; i < numberOfQuads; ++i) {
-    lt::Quad targetQuad = quads[i];
-    lt::Quad textureMapQuad = textureMapQuads[i];
+    std::array<GLKVector3, kNumberOfQuadCorners> vertices = [self verticesForQuad:quads[i]];
+    std::array<GLKVector3, kNumberOfQuadCorners> texcoords =
+        [self verticesForQuad:textureMapQuads[i]];
 
-    std::array<GLKVector3, 4> vertices = [self verticesForQuad:targetQuad];
-    std::array<GLKVector3, 4> texcoords = [self verticesForQuad:textureMapQuad];
-
-    attributes.push_back({.position = LTVector4(vertices[0].x, vertices[0].y, z * vertices[0].z,
-                                                vertices[0].z),
-                          .texcoord = LTVector3(texcoords[0].x, texcoords[0].y, texcoords[0].z)});
-    attributes.push_back({.position = LTVector4(vertices[1].x, vertices[1].y, z * vertices[1].z,
-                                                vertices[1].z),
-                          .texcoord = LTVector3(texcoords[1].x, texcoords[1].y, texcoords[1].z)});
-    attributes.push_back({.position = LTVector4(vertices[2].x, vertices[2].y, z * vertices[2].z,
-                                                vertices[2].z),
-                          .texcoord = LTVector3(texcoords[2].x, texcoords[2].y, texcoords[2].z)});
-    attributes.push_back({.position = LTVector4(vertices[0].x, vertices[0].y, z * vertices[0].z,
-                                                vertices[0].z),
-                          .texcoord = LTVector3(texcoords[0].x, texcoords[0].y, texcoords[0].z)});
-    attributes.push_back({.position = LTVector4(vertices[2].x, vertices[2].y, z * vertices[2].z,
-                                                vertices[2].z),
-                          .texcoord = LTVector3(texcoords[2].x, texcoords[2].y, texcoords[2].z)});
-    attributes.push_back({.position = LTVector4(vertices[3].x, vertices[3].y, z * vertices[3].z,
-                                                vertices[3].z),
-                          .texcoord = LTVector3(texcoords[3].x, texcoords[3].y, texcoords[3].z)});
+    entry[entryIndex++] = {
+      .position = LTVector4(vertices[0].x, vertices[0].y, z * vertices[0].z, vertices[0].z),
+      .texcoord = LTVector3(texcoords[0].x, texcoords[0].y, texcoords[0].z)
+    };
+    entry[entryIndex++] = {
+      .position = LTVector4(vertices[1].x, vertices[1].y, z * vertices[1].z, vertices[1].z),
+      .texcoord = LTVector3(texcoords[1].x, texcoords[1].y, texcoords[1].z)
+    };
+    entry[entryIndex++] = {
+      .position = LTVector4(vertices[2].x, vertices[2].y, z * vertices[2].z, vertices[2].z),
+      .texcoord = LTVector3(texcoords[2].x, texcoords[2].y, texcoords[2].z)
+    };
+    entry[entryIndex++] = {
+      .position = LTVector4(vertices[0].x, vertices[0].y, z * vertices[0].z, vertices[0].z),
+      .texcoord = LTVector3(texcoords[0].x, texcoords[0].y, texcoords[0].z)
+    };
+    entry[entryIndex++] = {
+      .position = LTVector4(vertices[2].x, vertices[2].y, z * vertices[2].z, vertices[2].z),
+      .texcoord = LTVector3(texcoords[2].x, texcoords[2].y, texcoords[2].z)
+    };
+    entry[entryIndex++] = {
+      .position = LTVector4(vertices[3].x, vertices[3].y, z * vertices[3].z, vertices[3].z),
+      .texcoord = LTVector3(texcoords[3].x, texcoords[3].y, texcoords[3].z)
+    };
 
     z += 1.0 / numberOfQuads;
   }
-
-  NSData *data = [NSData dataWithBytes:&attributes[0]
-                                length:attributes.size() * sizeof(attributes[0])];
-  return [[LTAttributeData alloc] initWithData:data inFormatOfGPUStruct:self.gpuStruct];
+  return [[LTAttributeData alloc] initWithData:mutableData inFormatOfGPUStruct:gpuStruct];
 }
 
-- (std::array<GLKVector3, 4>)verticesForQuad:(lt::Quad)quad {
++ (std::array<GLKVector3, 4>)verticesForQuad:(lt::Quad)quad {
   GLKMatrix3 transform = GLKMatrix3Transpose(quad.transform());
   return {{
     GLKMatrix3MultiplyVector3(transform, GLKVector3Make(0, 0, 1)),
