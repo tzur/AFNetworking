0786f7f2b8263ef69fae96c021c6be037edfb06a
Michael Kupchick <michael@lightricks.com>
2018-08-22 18:46:06 +0300

    PNKComputeState: Use MetalToolbox under the hood.

--- Pinky/Pinky/PNKComputeState.h
+++ Pinky/Pinky/PNKComputeState.h
@@ -1,30 +1,16 @@
 // Copyright (c) 2017 Lightricks. All rights reserved.
 // Created by Ofir Bibi.
 
+#import <MetalToolbox/MTBFunctionConstant.h>
+
 NS_ASSUME_NONNULL_BEGIN
 
-/// Creates a compute pipeline state for a given function.
-///
-/// @param functionName Name of the kernel to compile into the pipeline state.
-///
-/// @note This creates a non-specialized kernel function. Raises \c NSInternalInconsistencyException
-/// if the kernel with \c functionName has constants.
-id<MTLComputePipelineState> PNKCreateComputeState(id<MTLDevice> device, NSString *functionName)
-    API_AVAILABLE(ios(10.0));
-
-/// MTLFunctionConstantValues is not supported in simulator for Xcode 8. Solved in Xcode 9.
-#if PNK_USE_MPS
-
-/// Creates a compute pipeline state for a given function specialized for the given constants.
+/// Creates a compute pipeline state for a given function specialized for the given constants when
+/// provided.
 ///
 /// @param functionName Name of the kernel to compile into the pipeline state.
 /// @param constants Constant values to set in the kernel. Used to compile a specialized version of
-/// the kernel.
-///
-/// @note This creates a specialized kernel function.
-id<MTLComputePipelineState> PNKCreateComputeStateWithConstants(id<MTLDevice> device,
-    NSString *functionName, MTLFunctionConstantValues *constants) API_AVAILABLE(ios(10.0));
-
-#endif // PNK_USE_MPS
-
+/// the kernel when provided.
+id<MTLComputePipelineState> PNKCreateComputeState(id<MTLDevice> device,
+    NSString * const sfunctionName, NSArray<MTBFunctionConstant *> * _Nullable constants = nil);
 NS_ASSUME_NONNULL_END
