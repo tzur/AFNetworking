43f2bcf5f8c74a6a6539259dd674dbc5b20758e1
Nimrod Gruber <nimrod@lightricks.com>
2018-08-27 10:30:02 +0300

    EventGeneration.sh: Forward exit code from python scripts.
    Currently exit code in the internally called python scripts is ignored.
    This results in failures being considered as successful by Xcode when
    compiling event JSON objects. Only failing at a later time when trying
    to compile the autogenerated file that's missing.
    
    The fix forwards any non-zero error codes from the underlying python
    scripts.

--- BuildTools/EventGeneration/EventGeneration.sh
+++ BuildTools/EventGeneration/EventGeneration.sh
@@ -11,20 +11,20 @@ shared_events_folder="$2"
 
 if [[ "$INPUT_FILE_PATH" == *"AbstractEvents/"*Analytricks*.json ]]; then
   python "$build_tool_folder/EventGeneration/GenerateAnalytricksEvent.py" \
-  "$INPUT_FILE_PATH" "$shared_events_folder" "$DERIVED_FILE_DIR/$arch"
+  "$INPUT_FILE_PATH" "$shared_events_folder" "$DERIVED_FILE_DIR/$arch" || exit $?
 fi
 
 if [[ "$INPUT_FILE_PATH" == *"ConcreteEvents/"*Analytricks*.json ]]; then
   python "$build_tool_folder/../BuildTools/EventGeneration/GenerateAnalytricksDataProvider.py" \
-  "$INPUT_FILE_PATH" "$shared_events_folder" "$DERIVED_FILE_DIR/$arch"
+  "$INPUT_FILE_PATH" "$shared_events_folder" "$DERIVED_FILE_DIR/$arch" || exit $?
 fi
 
 if [[ "$INPUT_FILE_PATH" == *"Common/"*Analytricks*.json ]]; then
   python "$build_tool_folder/../BuildTools/EventGeneration/GenerateAnalytricksValueClass.py" \
-  "$INPUT_FILE_PATH" "$DERIVED_FILE_DIR/$arch"
+  "$INPUT_FILE_PATH" "$DERIVED_FILE_DIR/$arch" || exit $?
 fi
 
 if [[ "$INPUT_FILE_PATH" == *"Event.json"* ]]; then
   python "$build_tool_folder/../BuildTools/EventGeneration/GenerateValueClass.py" \
-  "$INPUT_FILE_PATH" "$DERIVED_FILE_DIR/$arch"
+  "$INPUT_FILE_PATH" "$DERIVED_FILE_DIR/$arch" || exit $?
 fi
