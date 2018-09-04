9e9b088ca9642a954e6d2fd6a9ddf6e176dece5f
Yaron Inger <yaron@lightricks.com>
2018-09-04 12:27:32 +0300

    NSBundle+PinkyBundle: use Pinky class instead of main bundle.
    Instead of using the main bundle to locate Pinky's bundle, use the
    bundle that contains Pinky's code as the base reference point to
    finding the bundle. This allows to use the bundle both in XCTest and in
    production.

--- Pinky/Pinky/NSBundle+PinkyBundle.mm
+++ Pinky/Pinky/NSBundle+PinkyBundle.mm
@@ -5,11 +5,19 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+/// Used to allow NSBundle to locate the bundle that contains Pinky.
+@interface PNKBundleLocator : NSObject
+@end
+
+@implementation PNKBundleLocator
+@end
+
 @implementation NSBundle (PinkyBundle)
 
 + (nullable NSBundle *)pnk_bundle {
-  return [NSBundle bundleWithPath:[[NSBundle mainBundle] pathForResource:@"Pinky"
-                                                                  ofType:@"bundle"]];
+  auto _Nullable executableBundle = [NSBundle bundleForClass:PNKBundleLocator.class];
+  auto _Nullable bundlePath = [executableBundle pathForResource:@"Pinky" ofType:@"bundle"];
+  return [NSBundle bundleWithPath:bundlePath];
 }
 
 @end
