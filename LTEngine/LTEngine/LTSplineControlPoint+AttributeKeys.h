33cd66adde5d6a4fbb1c3e59a8578d0d973838a4
Rouven Strauss <rouven@lightricks.com>
2018-09-07 19:12:31 +0200

    LTSplineControlPoint+AttributeKeys: Add method for key of screen speed.
    Augments LTSplineControlPoint with an additional category method
    returning the key representing the speed, in point units of the screen
    coordinate system, of the LTSplineControlPoint.

--- LTEngine/LTEngine/LTSplineControlPoint+AttributeKeys.h
+++ LTEngine/LTEngine/LTSplineControlPoint+AttributeKeys.h
@@ -9,13 +9,17 @@ NS_ASSUME_NONNULL_BEGIN
 /// commonly used as keys of the \c attributes dictionary.
 @interface LTSplineControlPoint (AttributeKeys)
 
-/// Returns the key used to describe a radius attribute associated with an
-// \c LTSplineControlPoint.
+/// Returns the key used to describe an attribute representing the radius, in floating-point pixel
+/// units of the content coordinate system, associated with the receiver.
 + (NSString *)keyForRadius;
 
-/// Returns the key used to describe a force attribute associated with an
-// \c LTSplineControlPoint.
+/// Returns the key used to describe an attribute representing the force associated with the
+/// receiver.
 + (NSString *)keyForForce;
+
+/// Returns the key used to describe an attribute representing the speed, in point units of the
+/// screen coordinate system, of the receiver.
++ (NSString *)keyForSpeedInScreenCoordinates;
 
 @end
 
