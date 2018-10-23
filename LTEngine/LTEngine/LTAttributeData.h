a9110d43e86e38cb1faa58d6e289b7ba43651478
Michael Kimyagarov <mkimi@lightricks.com>
2018-10-23 10:27:30 +0300

    LTAttributeData: mark the designated initializer.
    And disable the default initializer.

--- LTEngine/LTEngine/LTAttributeData.h
+++ LTEngine/LTEngine/LTAttributeData.h
@@ -9,10 +9,13 @@ NS_ASSUME_NONNULL_BEGIN
 /// to \c LTArrayBuffer objects.
 @interface LTAttributeData : NSObject <NSCopying>
 
+- (instancetype)init NS_UNAVAILABLE;
+
 /// Initializes with a copy of the given \c data in the format of the given \c gpuStruct. The number
 /// of bytes of the given \c data must be a multiple of the number of bytes of the given
 /// \c gpuStruct.
-- (instancetype)initWithData:(NSData *)data inFormatOfGPUStruct:(LTGPUStruct *)gpuStruct;
+- (instancetype)initWithData:(NSData *)data inFormatOfGPUStruct:(LTGPUStruct *)gpuStruct
+    NS_DESIGNATED_INITIALIZER;
 
 /// Binary data in the format of the \c gpuStruct of this instance.
 @property (readonly, nonatomic) NSData *data;
